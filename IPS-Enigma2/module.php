2ce9deb9a0e7363849faa6c009ff68cc0f55dba0
Joey-1970 <paeper@horburg.de>
2015-04-14 19:27:26 +0200

    Update module.php

--- IPS-Enigma2/module.php
+++ IPS-Enigma2/module.php
@@ -15,6 +15,8 @@ class Enigma2 extends IPSModule
   {
     //Never delete this line!
     parent::ApplyChanges();
+    $this->RegisterVariableBoolean("Status", "~Switch");
+		$this->RegisterEventCyclic("UpdateTimer", "Automatische Aktualisierung", 15);
   }
   
   private function CreateCategoryByIdent($id, $ident, $name)
