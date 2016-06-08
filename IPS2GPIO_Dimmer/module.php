6e76f69a13f327367d8a6d3067681fc5fa6f1666
Joey-1970 <paeper@horburg.de>
2016-06-08 22:26:21 +0200

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -1,3 +1,14 @@
+
+// Setzt einen Pin in einen bestimmten Modus
+public function Set_Mode($RPiIP, $RPiPort, $IPS_ID, $Command, $GPIO_Pin, $GPIO_Mode)
+{
+   list($result, $IPS_User, $IPS_Pass) = RemoteAccessData();
+	$result = "";
+   $result = exec('sudo python '.IPS_GetKernelDir().'modules/SymconModules/IPS2GPIO/ips2gpio.py '.$RPiIP.' '.$RPiPort.' '.$IPS_User.' '.$IPS_Pass.' '.$IPS_ID.' '.$Command.' '.$GPIO_Pin.' '.$GPIO_Mode);
+return $result;
+}
+
+// Ermittelt den User und das Passwort für den Fernzugriff (nur RPi)
 private function RemoteAccessData()
 {
    $result = true;
