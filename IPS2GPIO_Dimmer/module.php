cf8b0347728c31a7134c1086d7f67609f913c815
Joey-1970 <paeper@horburg.de>
2016-06-08 22:24:38 +0200

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -1,1 +1,22 @@
-
+private function RemoteAccessData()
+{
+   $result = true;
+   exec('sudo cat /root/.symcon', $ResultArray);
+   If (strpos($ResultArray[0], "Licensee=") === false)
+		{
+		$result = false;
+		}
+	else
+		{
+      $User = substr(strstr($ResultArray[0], "="),1);
+      }
+If (strpos($ResultArray[(count($ResultArray))-1], "Password=") === false)
+		{
+		$result = false;
+		}
+	else
+		{
+      $Pass = base64_decode(substr(strstr($ResultArray[(count($ResultArray))-1], "="),1));
+      }
+return array($result, $User, $Pass);
+}
