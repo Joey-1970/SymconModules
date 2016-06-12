aaa343b47f98c594ae259ea85372e2896c9454e4
Joey-1970 <paeper@horburg.de>
2016-06-12 14:30:50 +0200

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -18,7 +18,7 @@
             parent::Create();
            
             $this->RegisterPropertyInteger("Pin", 2);
- 	    //$this->ForceParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");	
+ 	    $this->ForceParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");	
         }
  
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
