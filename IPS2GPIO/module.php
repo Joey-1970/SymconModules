9c6cd3de086383af97c90a9ae6199955dcb18ea9
Michael Tröger <mt@neo-ami.de>
2016-06-12 18:46:23 +0200

    no message

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -3,23 +3,15 @@ class IPS2GPIO_IO extends IPSModule
 {
   // Der Konstruktor des Moduls
   // Überschreibt den Standard Kontruktor von IPS
-  public function __construct($InstanceID) 
-  {
-      // Diese Zeile nicht löschen
-      parent::__construct($InstanceID);
- 
-            // Selbsterstellter Code
-  }
-  
   public function Create() 
   {
     // Diese Zeile nicht entfernen
     parent::Create();
  
     // Modul-Eigenschaftserstellung
-    //$this->RegisterPropertyString("IPAddress", "127.0.0.1");
-    //$this->RegisterPropertyBoolean("Open", false);
-    //$this->RegisterPropertyInteger("Model", 0);
+    $this->RegisterPropertyString("IPAddress", "127.0.0.1");
+    $this->RegisterPropertyBoolean("Open", false);
+    $this->RegisterPropertyInteger("Model", 0);
   }
   
   public function ApplyChanges()
