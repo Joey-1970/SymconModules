b6054382977b01c9b2ca52bebfd981f7b7b30ab8
Joey-1970 <paeper@horburg.de>
2016-06-13 06:05:47 +0200

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -38,6 +38,41 @@
            $this->RegisterVariableInteger("Intensity", "Intensity", "~Intensity.255");
            $this->EnableAction("Intensity");
         }
+	// Beginn der Funktionen
+	
+	// Setzt einen Pin in einen bestimmten Modus
+	public function Set_Mode($RPiIP, $RPiPort, $IPS_ID, $Command, $GPIO_Pin, $GPIO_Mode)
+	{
+   		list($result, $IPS_User, $IPS_Pass) = RemoteAccessData();
+		$result = "";
+   		$result = exec('sudo python '.IPS_GetKernelDir().'modules/SymconModules/IPS2GPIO/ips2gpio.py '.$RPiIP.' '.$RPiPort.' '.$IPS_User.' '.$IPS_Pass.' '.$IPS_ID.' '.$Command.' '.$GPIO_Pin.' '.$GPIO_Mode);
+	return $result;
+	}
+	
+	
+		// Ermittelt den User und das Passwort für den Fernzugriff (nur RPi)
+	private function RemoteAccessData()
+	{
+	   	$result = true;
+	   	exec('sudo cat /root/.symcon', $ResultArray);
+	   	If (strpos($ResultArray[0], "Licensee=") === false)
+			{
+			$result = false;
+			}
+		else
+			{
+	      		$User = substr(strstr($ResultArray[0], "="),1);
+	      		}
+		If (strpos($ResultArray[(count($ResultArray))-1], "Password=") === false)
+			{
+			$result = false;
+			}
+		else
+			{
+	      		$Pass = base64_decode(substr(strstr($ResultArray[(count($ResultArray))-1], "="),1));
+	      		}
+	return array($result, $User, $Pass);
+	}
 
     }
 ?>
