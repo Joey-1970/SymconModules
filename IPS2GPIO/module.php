60ead00fb9367e9a0ff4596b7b6e142ae45276ad
Joey-1970 <paeper@horburg.de>
2016-06-14 20:59:09 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -29,5 +29,37 @@ class IPS2GPIO_IO extends IPSModule
     //$this->RegisterVariableBoolean("Status", "Status", "~Switch");
     //$this->RegisterEventCyclic("UpdateTimer", "Automatische Aktualisierung", 15);
   }
-}
+  
+  private function ConnectionTest()
+  {
+      $IP = "paule-25.selfhost.bz";
+      $port = 8888;
+      $AktuellerPCStatus = False;
+
+      If (GetValueBoolean(18029 /*[Fritz!Box\DSL\DSL\Status]*/ )== true)
+	    {
+	        $AktuellerPCStatus = Sys_Ping($IP, 2000);
+
+	        If ($AktuellerPCStatus == true)
+		      {
+		          $status = @fsockopen($IP, $port, $errno, $errstr, 10);
+
+		          if (!$status)
+   		        {
+      	          //SetValueBoolean(45369 /*[LAN\TV Server\TV Server Status\HD+ Server]*/ , false);
+			            //echo "Fehler";
+   		        }
+   		        else
+   		        {
+   		            fclose($status);
+      	          //SetValueBoolean(45369 /*[LAN\TV Server\TV Server Status\HD+ Server]*/ , true);
+			            //echo "Ok";
+   		        }
+		      }
+		      else
+		      {
+		            //SetValueBoolean(45369 /*[LAN\TV Server\TV Server Status\HD+ Server]*/ , false);
+		      }
+      }
+  }
 ?>
