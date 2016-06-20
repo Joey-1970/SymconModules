71a5a4e7f10e21a11a60790ad2f87c7a1272e586
Joey-1970 <paeper@horburg.de>
2016-06-20 10:18:30 +0200

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -54,7 +54,7 @@
 	// Setzt den gewaehlten Pin in den Output-Modus
 	private function Set_Mode()
 	{
-   		$this->SendDataToParent(json_encode(Array("DataID" => "{B87AC955-F258-468B-92FE-F4E0866A9E18}", "Buffer" => $Text)));
+   		$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Buffer" => $Text)));
    		$IPSID = $this->InstanceID;
    		//list($result, $IPSUser, $IPSPass) = $this->RemoteAccessData();
    		$IPSUser = IPS_GetProperty((IPS_GetInstance($this->InstanceID)['ConnectionID']), "User");
