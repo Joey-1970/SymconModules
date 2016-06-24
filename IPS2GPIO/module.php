8842a9755817719215a780dac5de6596af716e85
Joey-1970 <paeper@horburg.de>
2016-06-24 18:32:17 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -48,7 +48,7 @@ class IPS2GPIO_IO extends IPSModule
            	$Typ[0] = array(0, 1, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 21, 22, 23, 24, 25);	
            	$Typ[1] = array(2, 3, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 22, 23, 24, 25, 27);
            	$Typ[2] = array(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27);
-           	SetValueString($this->GetIDForIdent("PinPossible"), serialize ($Typ($this->ReadPropertyInteger('Model'))));
+           	SetValueString($this->GetIDForIdent("PinPossible"), serialize ($Typ[$this->ReadPropertyInteger('Model')]));
 	  }
   	  
   	  public function ReceiveData($JSONString) 
