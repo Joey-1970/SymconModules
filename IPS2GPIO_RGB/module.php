cdd07f343dd790e4730389d09cf9d1a7876fdfe1
Joey-1970 <paeper@horburg.de>
2016-06-24 19:32:58 +0200

    Update module.php

--- IPS2GPIO_RGB/module.php
+++ IPS2GPIO_RGB/module.php
@@ -118,6 +118,8 @@
    			$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin_G"), "Value" => 0)));
    			$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin_B"), "Value" => 0)));	
 		}
+	
+	return;
 	}
 	
 	
