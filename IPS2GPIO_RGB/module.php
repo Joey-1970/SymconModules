28637cafc895a604b52a7913ab3d81332bf7648c
Joey-1970 <paeper@horburg.de>
2016-06-25 07:45:42 +0200

    Update module.php

--- IPS2GPIO_RGB/module.php
+++ IPS2GPIO_RGB/module.php
@@ -98,6 +98,11 @@
  			$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin_R"), "Value" => $R))); 
    			$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin_G"), "Value" => $G)));
    			$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin_B"), "Value" => $B)));
+ 		
+ 			//$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin_R"), "Value" => $R))); 
+
+ 			
+ 			
  		}
 	return;
 	}
