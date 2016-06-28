dcbefb30098ac13f9f171aa04f16638414c3926b
Joey-1970 <paeper@horburg.de>
2016-06-28 08:58:57 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -93,8 +93,7 @@ class IPS2GPIO_IO extends IPSModule
   
 	// Setzt den gewaehlten Pin in den geforderten Modus
 	private function Set_Mode($Pin, $Modus)
-	{
-		//$result = CSCK_SendText(IPS_GetInstance($this->InstanceID)['ConnectionID'], pack("LLLL", 0, $Pin, $Modus, 0));
+	{		
 		$this->ClientSocket(pack("LLLL", 0, $Pin, $Modus, 0));
 	return;
 	}
@@ -102,7 +101,6 @@ class IPS2GPIO_IO extends IPSModule
 	// Dimmt den gewaehlten Pin
 	private function Set_Intensity($Pin, $Value)
 	{
-		//$result = CSCK_SendText(IPS_GetInstance($this->InstanceID)['ConnectionID'], pack("LLLL", 5, $Pin, $Value, 0));
 		$this->ClientSocket(pack("LLLL", 5, $Pin, $Value, 0));
 	return;
 	}
@@ -110,15 +108,13 @@ class IPS2GPIO_IO extends IPSModule
 	// Setzt die Farbe der RGB-LED
 	private function Set_Intensity_RGB($Pin_R, $Value_R, $Pin_G, $Value_G, $Pin_B, $Value_B)
 	{
-		//$result = CSCK_SendText(IPS_GetInstance($this->InstanceID)['ConnectionID'], pack("LLLL", 5, $Pin_R, $Value_R, 0).pack("LLLL", 5, $Pin_G, $Value_G, 0).pack("LLLL", 5, $Pin_B, $Value_B, 0));
-		$this->ClientSocket(pack("LLLL", 0, $Pin_R, $Value_R, 0).pack("LLLL", 0, $Pin_G, $Value_G, 0).pack("LLLL", 0, $Pin_B, $Value_B, 0));
+		$this->ClientSocket(pack("LLLL", 5, $Pin_R, $Value_R, 0).pack("LLLL", 5, $Pin_G, $Value_G, 0).pack("LLLL", 5, $Pin_B, $Value_B, 0));
 	return;
 	}
 			
 	// Schaltet den gewaehlten Pin
 	private function Set_Status($Pin, $Value)
 	{
-		//$result = CSCK_SendText(IPS_GetInstance($this->InstanceID)['ConnectionID'], pack("LLLL", 5, $Pin, $Value, 0));
 		$this->ClientSocket(pack("LLLL", 5, $Pin, $Value, 0));
 	return;
 	}
@@ -139,48 +135,36 @@ class IPS2GPIO_IO extends IPSModule
 	
 	private function ClientSocket($message)
 	{
-		if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0))) 
-		{ 
-		    $errorcode = socket_last_error(); 
-		    $errormsg = socket_strerror($errorcode); 
-		
-		    die("Couldn't create socket: [$errorcode] $errormsg \n"); 
+		// create Socket 
+		if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0))) { 
+		    	$errorcode = socket_last_error(); 
+		    	$errormsg = socket_strerror($errorcode); 
+		    	die("Couldn't create socket: [$errorcode] $errormsg \n"); 
 		} 
-		
-		//echo "Socket created"; 
-		
-		if(!socket_connect($sock , $this->ReadPropertyString("IPAddress") , 8888)) 
-		{ 
-		    $errorcode = socket_last_error(); 
-		    $errormsg = socket_strerror($errorcode); 
-		
-		    die("Could not connect: [$errorcode] $errormsg \n"); 
+				
+		// establish Connection
+		if(!socket_connect($sock , $this->ReadPropertyString("IPAddress") , 8888)) { 
+		    	$errorcode = socket_last_error(); 
+		    	$errormsg = socket_strerror($errorcode); 
+			die("Could not connect: [$errorcode] $errormsg \n"); 
 		} 
-		
-		//echo "Connection established \n"; 
-		
+			
 		//Send the message to the server 
-		if( ! socket_send ( $sock , $message , strlen($message) , 0)) 
-		{ 
-		    $errorcode = socket_last_error(); 
-		    $errormsg = socket_strerror($errorcode); 
-		
-		    die("Could not send data: [$errorcode] $errormsg \n"); 
+		if( ! socket_send ( $sock , $message , strlen($message) , 0)) { 
+		    	$errorcode = socket_last_error(); 
+		    	$errormsg = socket_strerror($errorcode); 
+			die("Could not send data: [$errorcode] $errormsg \n"); 
 		} 
-		
-		//echo "Message send successfully \n"; 
 		
 		//Now receive reply from server 
-		if(socket_recv ( $sock , $buf , 16, MSG_WAITALL ) === FALSE) 
-		{ 
-		    $errorcode = socket_last_error(); 
-		    $errormsg = socket_strerror($errorcode); 
-		
-		    die("Could not receive data: [$errorcode] $errormsg \n"); 
+		if(socket_recv ( $sock , $buf , 16, MSG_WAITALL ) === FALSE) { 
+		    	$errorcode = socket_last_error(); 
+		    	$errormsg = socket_strerror($errorcode);  
+		    	die("Could not receive data: [$errorcode] $errormsg \n"); 
 		} 
 		
 		//print the received message 
-		//echo $buf; 
+		
 		//print_r (unpack("L*", $buf));  
 
 	return;	
