1f6269b0d99b847360afc3bcd513e4723ce0dbe2
Joey-1970 <paeper@horburg.de>
2016-07-09 22:36:58 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -247,7 +247,7 @@ class IPS2GPIO_IO extends IPSModule
 		elseif (strlen(utf8_decode($Message)) == 12) {
 			$response = unpack("S1L*", $Message);
 			IPS_LogMessage("GPIO Notify: ","Meldung");		
-			IPS_LogMessage("GPIO Notify: ","Meldung: ".$response[1]." ".$response[2]." ".$response[3]);
+			IPS_LogMessage("GPIO Notify: ","Meldung: ".count($response));
 		}
 		elseif (strlen(utf8_decode($Message)) > 16) {
 			$Message = substr($Message, 0, 12);
