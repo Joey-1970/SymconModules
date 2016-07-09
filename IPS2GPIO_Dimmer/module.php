c6a7f2b587dcdb282eddaeca3a782a4dbb944ee4
Joey-1970 <paeper@horburg.de>
2016-07-09 14:00:58 +0200

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -66,7 +66,7 @@
 	{
     	// Empfangene Daten vom Gateway/Splitter
     	$data = json_decode($JSONString);
-    	IPS_LogMessage("ReceiveData_Dimmer", utf8_decode($data->Buffer));
+    	//IPS_LogMessage("ReceiveData_Dimmer", utf8_decode($data->Buffer));
  	switch ($data->Function) {
 		    case "pin_possible":
 		        If ($data->InstanzID == $this->$InstanceID) {
