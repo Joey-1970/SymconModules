bb82221f7dd9292afde4c04977046f3e1d1c4d02
Joey-1970 <paeper@horburg.de>
2016-07-11 11:36:58 +0200

    Update module.php

--- IPS2GPIO_RGB/module.php
+++ IPS2GPIO_RGB/module.php
@@ -109,15 +109,8 @@
 		SetValue($this->GetIDForIdent("Intensity_B"), $B);
  		 
  		If (GetValueBoolean($this->GetIDForIdent("Status")) == true) { 
- 			//$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin_R"), "Value" => $R))); 
-   			//$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin_G"), "Value" => $G)));
-   			//$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin_B"), "Value" => $B)));
- 		
  			$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle_RGB", "Pin_R" => $this->ReadPropertyInteger("Pin_R"), "Value_R" => $R, "Pin_G" => $this->ReadPropertyInteger("Pin_G"), "Value_G" => $G, "Pin_B" => $this->ReadPropertyInteger("Pin_B"), "Value_B" => $B))); 
-
- 			
- 			
- 		}
+		}
 	return;
 	}
 	
