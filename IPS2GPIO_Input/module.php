bbf8a6dba90a96283f145bbde1e6083d35cc7b15
Joey-1970 <paeper@horburg.de>
2016-07-11 21:29:43 +0200

    Update module.php

--- IPS2GPIO_Input/module.php
+++ IPS2GPIO_Input/module.php
@@ -43,6 +43,8 @@
            
             $this->Set_Mode();
             $this->Set_GlitchFilter();
+            $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
+
         }
 	
 	
