801b33c03a320f524a9a6bea953eb3b871f0c8ef
Joey-1970 <paeper@horburg.de>
2016-07-11 08:47:14 +0200

    Update module.php

--- IPS2GPIO_Input/module.php
+++ IPS2GPIO_Input/module.php
@@ -78,6 +78,9 @@
 		   case "get_notifypin":
 		   	$this->SendDataToParent(json_encode(Array("DataID"=> "{EBFBEE2B-0F1A-470D-8B5B-97F977D605CA}", "Function" => "set_notifypin", "Pin" => $this->ReadPropertyInteger("Pin"))));
 		   	break;
+		   case "get_usedpin":
+		   	$this->SendDataToParent(json_encode(Array("DataID"=> "{EBFBEE2B-0F1A-470D-8B5B-97F977D605CA}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin"))));
+		   	break;
  	}
     	// Datenverarbeitung und schreiben der Werte in die Statusvariablen
     	//SetValue($this->GetIDForIdent("Value"), $data->Buffer);
