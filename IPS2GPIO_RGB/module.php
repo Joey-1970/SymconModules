caadd3de8cabdc9e266b1e537d85668c8f443fc1
Joey-1970 <paeper@horburg.de>
2016-07-11 22:13:22 +0200

    Update module.php

--- IPS2GPIO_RGB/module.php
+++ IPS2GPIO_RGB/module.php
@@ -107,9 +107,9 @@
 	// Dimmt den gewaehlten Pin
 	public function Set_RGB($R, $G, $B)
 	{
- 		SetValue($this->GetIDForIdent("Intensity_R"), $R);
-		SetValue($this->GetIDForIdent("Intensity_G"), $G);
-		SetValue($this->GetIDForIdent("Intensity_B"), $B);
+ 		SetValueInteger($this->GetIDForIdent("Intensity_R"), $R);
+		SetValueInteger($this->GetIDForIdent("Intensity_G"), $G);
+		SetValueInteger($this->GetIDForIdent("Intensity_B"), $B);
  		 
  		If (GetValueBoolean($this->GetIDForIdent("Status")) == true) { 
  			$this->SendDataToParent(json_encode(Array("DataID" => "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle_RGB", "Pin_R" => $this->ReadPropertyInteger("Pin_R"), "Value_R" => $R, "Pin_G" => $this->ReadPropertyInteger("Pin_G"), "Value_G" => $G, "Pin_B" => $this->ReadPropertyInteger("Pin_B"), "Value_B" => $B))); 
