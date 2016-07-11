e05c7346e03e5c8de30905ca449a67e723b05689
Joey-1970 <paeper@horburg.de>
2016-07-11 21:28:50 +0200

    Update module.php

--- IPS2GPIO_RGB/module.php
+++ IPS2GPIO_RGB/module.php
@@ -46,6 +46,9 @@
            	$this->RegisterVariableInteger("Color", "Farbe", "~HexColor");
            	$this->EnableAction("Color");
            	$this->Set_Mode_RGB();
+           	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
+
+           	
         }
 	
 	public function RequestAction($Ident, $Value) 
