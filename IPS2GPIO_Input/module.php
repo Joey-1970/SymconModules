a4bd9985ad40f8bcff9f424460872e097d631e52
Joey-1970 <paeper@horburg.de>
2016-07-13 21:47:50 +0200

    Update module.php

--- IPS2GPIO_Input/module.php
+++ IPS2GPIO_Input/module.php
@@ -2,18 +2,7 @@
     // Klassendefinition
     class IPS2GPIO_Input extends IPSModule 
     {
- 
-        // Der Konstruktor des Moduls
-        // Überschreibt den Standard Kontruktor von IPS
-        public function __construct($InstanceID) 
-        {
-            // Diese Zeile nicht löschen
-            parent::__construct($InstanceID);
- 
-            // Selbsterstellter Code
-        }
- 
-        // Überschreibt die interne IPS_Create($id) Funktion
+	// Überschreibt die interne IPS_Create($id) Funktion
         public function Create() 
         {
             // Diese Zeile nicht löschen.
