4ad8dc85096b1fde843317b2b74714c423dba4c5
Joey-1970 <paeper@horburg.de>
2016-07-16 13:56:30 +0200

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -12,10 +12,8 @@
         {
             // Diese Zeile nicht löschen.
             parent::Create();
-            $this->SetStatus(101);
             $this->RegisterPropertyInteger("Pin", -1);
  	    $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
- 	    $this->SetStatus(104);
         }
  
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
@@ -23,7 +21,7 @@
         {
             // Diese Zeile nicht löschen
             parent::ApplyChanges();
-            
+            $this->SetStatus(101);
             //Connect to available splitter or create a new one
 	   $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
 	   
