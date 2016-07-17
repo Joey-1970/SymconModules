5c977c6593ca09afa1d283b877ad4836eac08287
Joey-1970 <paeper@horburg.de>
2016-07-17 18:32:02 +0200

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -30,7 +30,7 @@
            $this->RegisterVariableInteger("Intensity", "Intensity", "~Intensity.255");
            $this->EnableAction("Intensity");
            If ($this->ReadPropertyInteger("Pin") >= 0) {
-           	$this->Set_Mode();
+           	//$this->Set_Mode();
            	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
            }	
         }
@@ -61,7 +61,7 @@
     	//IPS_LogMessage("ReceiveData_Dimmer", utf8_decode($data->Buffer));
  	switch ($data->Function) {
 		case "get_usedpin":
-		   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin"))));
+		   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin"), "Modus" => "W")));
 		   	break;
 		case "status":
 			If ($data->Pin == $this->ReadPropertyInteger("Pin")) {
