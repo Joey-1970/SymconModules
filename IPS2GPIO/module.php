a8f02d8e300788f9725491eb5a7ff54cc70bc1af
Joey-1970 <paeper@horburg.de>
2016-07-19 08:19:13 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -23,12 +23,16 @@ class IPS2GPIO_IO extends IPSModule
 		parent::ApplyChanges();
 		$this->RegisterVariableString("PinPossible", "PinPossible");
 		$this->DisableAction("PinPossible");
+		IPS_SetHidden(GetIDForIdent("PinPossible"), true);
 		$this->RegisterVariableString("PinUsed", "PinUsed");
 		$this->DisableAction("PinUsed");
+		IPS_SetHidden(GetIDForIdent("PinUsed"), true);
 		$this->RegisterVariableString("PinNotify", "PinNotify");
 		$this->DisableAction("PinNotify");
+		IPS_SetHidden(GetIDForIdent("PinNotify"), true);
 		$this->RegisterVariableInteger("Handle", "Handle");
 		$this->DisableAction("Handle");
+		IPS_SetHidden(GetIDForIdent("Handle"), true);
 		
 		$ParentID = $this->GetParentID();
 		If ($ParentID > 0) {
