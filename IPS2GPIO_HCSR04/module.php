cb1091d9401813a85bd7e90be05aa505f5fd405f
Joey-1970 <paeper@horburg.de>
2016-07-19 20:58:48 +0200

    Update module.php

--- IPS2GPIO_HCSR04/module.php
+++ IPS2GPIO_HCSR04/module.php
@@ -48,12 +48,12 @@
 	    	$data = json_decode($JSONString);
 	 	switch ($data->Function) {
 			   case "notify":
-			   	If (($data->Pin == $this->ReadPropertyInteger("Pin_I")) AND ($data->Value == false)) {
+			   	If (($data->Pin == $this->ReadPropertyInteger("Pin_I")) AND ($data->Value == true)) {
 			   		$TimeDiff = $data->Timestamp - GetValueInteger($this->GetIDForIdent("TimestampTrigger"));
    					SetValueFloat($this->GetIDForIdent("Distanz"), ($TimeDiff * 34300 / 2000));
    					IPS_LogMessage("HCS04: ","Stop: ".$data->Timestamp." Start: ".GetValueInteger($this->GetIDForIdent("TimestampTrigger")));
 			   	}
-			   	elseif (($data->Pin == $this->ReadPropertyInteger("Pin_I")) AND ($data->Value == true)) {
+			   	elseif (($data->Pin == $this->ReadPropertyInteger("Pin_I")) AND ($data->Value == false)) {
 			   		SetValueInteger($this->GetIDForIdent("TimestampTrigger"), $data->Timestamp);	
 			   	}
 			   	break;
