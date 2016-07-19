9b115b473da3f1ea85c191a4ca1b29bde977499f
Joey-1970 <paeper@horburg.de>
2016-07-19 08:43:36 +0200

    Update module.php

--- IPS2GPIO_HCSR04/module.php
+++ IPS2GPIO_HCSR04/module.php
@@ -31,7 +31,7 @@
             $this->RegisterVariableInteger("TimestampTrigger", "TimestampTrigger", "", 0);
             $this->DisableAction("TimestampTrigger");
             IPS_SetHidden(GetIDForIdent("TimestampTrigger"), true);
-            $this->RegisterTimer("Messzyklus", 0, 'I2GSR_Measurement($_IPS["TARGET"]);');
+            $this->RegisterTimer("Messzyklus", 0, 'I2GSR4_Measurement($_IPS["TARGET"]);');
             
             If (($this->ReadPropertyInteger("Pin_I") >= 0) AND ($this->ReadPropertyInteger("Pin_O")) >= 0) {
             	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
