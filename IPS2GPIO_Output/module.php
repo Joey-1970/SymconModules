f5b500f57ed5ba56958b61ccfa874ce1e3d95833
Joey-1970 <paeper@horburg.de>
2016-07-20 07:48:41 +0200

    Update module.php

--- IPS2GPIO_Output/module.php
+++ IPS2GPIO_Output/module.php
@@ -31,6 +31,20 @@
             	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
             }
         }
+
+	public function RequestAction($Ident, $Value) 
+	{
+  		switch($Ident) {
+	        case "Status":
+	            $this->Set_Status($Value);
+	            //Neuen Wert in die Statusvariable schreiben
+	            SetValue($this->GetIDForIdent($Ident), $Value);
+	            break;
+	        default:
+	            throw new Exception("Invalid Ident");
+	    }
+	}
+
 	
 	public function ReceiveData($JSONString) 
 	{
