5300129db286ee204a7cbdb6054d31a325feaf65
Joey-1970 <paeper@horburg.de>
2016-07-20 17:31:05 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -12,8 +12,6 @@
         {
             // Diese Zeile nicht löschen.
             parent::Create();
-            $this->RegisterPropertyInteger("Pin_D", -1);
-            $this->RegisterPropertyInteger("Pin_C", -1);
  	    $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
         }
  
@@ -44,12 +42,13 @@
 			   	//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_notifypin", "Pin" => $this->ReadPropertyInteger("Pin"), "GlitchFilter" => $this->ReadPropertyInteger("GlitchFilter"))));
 			   	break;
 			   case "get_usedpin":
-			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin_D"), "Modus" => "R")));
-			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin_C"), "Modus" => "R")));
+			   	// Ist bei allen bisherigen Raspberry Pi GPIO 2&3, außer beim Modell 1 Revision 1, muss noch angepasst werden!
+			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => 2, "Modus" => "R")));
+			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => 3, "Modus" => "R")));
 
 			   	break;
 			   case "status":
-			   	If ($data->Pin == $this->ReadPropertyInteger("Pin")) {
+			   	If (($data->Pin == 2) OR ($data->Pin == 3)){
 			   		$this->SetStatus($data->Status);
 			   	}
 			   	break;
