cfb7ebdb5c8061440080a8e132c16f67098d4f47
Joey-1970 <paeper@horburg.de>
2016-07-20 07:46:03 +0200

    Create module.php

---
+++ IPS2GPIO_Output/module.php
@@ -0,0 +1,58 @@
+<?
+    // Klassendefinition
+    class IPS2GPIO_Output extends IPSModule 
+    {
+	public function __construct($InstanceID) {
+            // Diese Zeile nicht löschen
+            parent::__construct($InstanceID);
+        }
+
+	// Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() 
+        {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+            $this->RegisterPropertyInteger("Pin", -1);
+ 	    $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
+        }
+ 
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() 
+        {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+            //Connect to available splitter or create a new one
+	    $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
+	   
+	    //Status-Variablen anlegen
+	    $this->RegisterVariableBoolean("Status", "Status", "~Switch", 1);
+            $this->EnableAction("Status");
+            If ($this->ReadPropertyInteger("Pin") >= 0) {
+            	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
+            }
+        }
+	
+	public function ReceiveData($JSONString) 
+	{
+	    	// Empfangene Daten vom Gateway/Splitter
+	    	$data = json_decode($JSONString);
+	 	switch ($data->Function) {
+			   case "get_usedpin":
+			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin"), "Modus" => "W")));
+			   	break;
+			   case "status":
+			   	If ($data->Pin == $this->ReadPropertyInteger("Pin")) {
+			   		$this->SetStatus($data->Status);
+			   	}
+			   	break;
+			   case "freepin":
+			   	// Funktion zum erstellen dynamischer Pulldown-Menüs
+			   	break;
+	 	}
+	return;
+ 	}
+	// Beginn der Funktionen
+	
+
+}
+?>
