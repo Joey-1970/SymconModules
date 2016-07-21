75d60beaa84f2a3a76a49c773bcc4fcfba0d0039
Joey-1970 <paeper@horburg.de>
2016-07-21 08:25:32 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -41,8 +41,10 @@
 			   case "notify":
 			   // leer
 			   	break;
-			   case "get_notifypin":
-			   	//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_notifypin", "Pin" => $this->ReadPropertyInteger("Pin"), "GlitchFilter" => $this->ReadPropertyInteger("GlitchFilter"))));
+			   case "set_i2c-handle":
+			   	If ($data->Address == $this->ReadPropertyString("DeviceAddress")) {
+			   		SetValueInteger($this->GetIDForIdent("Handle_I2C"), $data->Handle);
+			   	}
 			   	break;
 			   case "get_used_i2c":
 			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "Value" => true)));
