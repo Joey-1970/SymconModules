b9ddf228266b5da141e42ecaa086297ce7d33833
Joey-1970 <paeper@horburg.de>
2016-07-22 09:00:43 +0200

    Update module.php

--- IPS2GPIO_HCSR04/module.php
+++ IPS2GPIO_HCSR04/module.php
@@ -79,7 +79,7 @@
  	}
 	// Beginn der Funktionen
 	
-	// Toggelt den Status
+	// Führt eine Messung aus
 	public function Measurement()
 	{
 		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_trigger", "Pin" => $this->ReadPropertyInteger("Pin_O"), "Time" => 10)));
