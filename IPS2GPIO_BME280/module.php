e81c1c31d71e48dc7c532efe828bda62751b0479
Joey-1970 <paeper@horburg.de>
2016-07-22 09:11:03 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -28,9 +28,9 @@
 	    	//Status-Variablen anlegen
 	    	$this->RegisterVariableInteger("HardwareRev", "HardwareRev");
           	
-          	$this->RegisterVariableInteger("Handle_I2C", "Handle_I2C");
-		$this->DisableAction("Handle_I2C");
-		IPS_SetHidden($this->GetIDForIdent("Handle_I2C"), true);
+          	$this->RegisterVariableInteger("Handle", "Handle");
+		$this->DisableAction("Handle");
+		IPS_SetHidden($this->GetIDForIdent("Handle"), true);
              	// den Handle für dieses Gerät ermitteln
              	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_i2c")));
            	
@@ -47,7 +47,7 @@
 			   	break;
 			   case "set_i2c-handle":
 			   	If ($data->Address == $this->ReadPropertyString("DeviceAddress")) {
-			   		SetValueInteger($this->GetIDForIdent("Handle_I2C"), $data->Handle);
+			   		SetValueInteger($this->GetIDForIdent("Handle"), $data->Handle);
 			   	}
 			   	break;
 			   case "get_used_i2c":
@@ -67,7 +67,8 @@
 	// Führt eine Messung aus
 	public function Measurement()
 	{
-		//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_trigger", "Pin" => $this->ReadPropertyInteger("Pin_O"), "Time" => 10)));
+		// Temperatur
+		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_Read_byte", "Handle" => $this->ReadPropertyInteger("Handle"), "Register" => "0xFA")));
 	return;
 	}	
 	
