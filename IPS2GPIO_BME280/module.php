e5d6483cba1f156a854bf7d4507cdd0c463d773c
Joey-1970 <paeper@horburg.de>
2016-07-24 13:40:41 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -38,6 +38,7 @@
              		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_i2c", "DeviceAddress" => $this->ReadPropertyString("DeviceAddress"))));
              	}
             	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
+            	$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
         }
 	
 	public function ReceiveData($JSONString) 
