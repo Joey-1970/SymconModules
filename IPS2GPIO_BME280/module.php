1afed229f520678033f22eb14aa7f4aa0648c35c
Joey-1970 <paeper@horburg.de>
2016-07-24 13:47:28 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -79,7 +79,7 @@
 	public function Measurement()
 	{
 		// Temperatur
-		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_byte", "Handle" => $this->ReadPropertyInteger("Handle"), "Register" => "0xFA")));
+		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => "0xFA")));
 	return;
 	}	
 	
