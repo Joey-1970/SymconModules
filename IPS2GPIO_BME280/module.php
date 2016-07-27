d58abe5095c8202708707798575658223e2be9c0
Joey-1970 <paeper@horburg.de>
2016-07-27 16:22:25 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -49,7 +49,7 @@
             	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
             	$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
             	$this->Setup();
-            	$tihs->Get_CalibrateData();
+            	$this->Get_CalibrateData();
         }
 	
 	public function ReceiveData($JSONString) 
