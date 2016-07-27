9f50afd746fab17cbb4662ca1632ed0ead4e148d
Joey-1970 <paeper@horburg.de>
2016-07-27 17:44:19 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -177,7 +177,7 @@ class IPS2GPIO_IO extends IPSModule
 		   	break;
 		   case "i2c_exchange_word":
 		   	$this->CommandClientSocket(pack("LLLLL", 69, $data->Handle, $data->Register, 4, $data->Value), 16);
-			IPS_LogMessage("I2C Exchange Word Parameter : ",$data->Handle." , ".$data->Register);  	
+			IPS_LogMessage("I2C Exchange Word Parameter : ",$data->Handle." , ".$data->Register." , ".$data->Value);  	
 		   	break;
 		   case "i2c_exchange_byte":
 		   	$this->CommandClientSocket(pack("LLLLL", 70, $data->Handle, $data->Register, 2, $data->Value), 16);
