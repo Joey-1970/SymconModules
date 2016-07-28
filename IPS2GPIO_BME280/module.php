d8262c1922c54a1bea10dac9d654efaf5637f002
Joey-1970 <paeper@horburg.de>
2016-07-28 12:33:10 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -183,6 +183,11 @@
 		SetValueFloat($this->GetIDForIdent("Pressure"), $Pressure);
 		
 		// Luftfeuchtigkeit
+		$Hum = $FineCalibrate - 76800;
+		If ($Hum <> 0) {
+			$Hum = ($Hum_raw - ($Dig_H4 * 64 + $Dig_H5 / 16384 * $Hum)) * ($Dig_H2  / 65536 * (1 + $Dig_H6 / 67108864 * $Hum *))
+		}
+		
 		SetValueFloat($this->GetIDForIdent("Humidity"), $Hum_raw);
 		
 		
