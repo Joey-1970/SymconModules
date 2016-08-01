053ba753376d2bc0f37017fe4d91eecf18827da3
Joey-1970 <paeper@horburg.de>
2016-08-01 12:15:31 +0200

    Update module.php

--- IPS2GPIO_Display/module.php
+++ IPS2GPIO_Display/module.php
@@ -70,6 +70,13 @@
  	}
 	// Beginn der Funktionen
 	
+	public function Brightness($Value)
+	{
+		$Message = "dims=".$Value."\xFF\xFF\xFF"; 
+		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "write_bytes_serial", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Message" => $Message)));
+
+	return;
+	}
 
 }
 ?>
