89756ff3ce92c4b30350a12d057ef639313f6280
Joey-1970 <paeper@horburg.de>
2016-08-01 13:08:32 +0200

    Update module.php

--- IPS2GPIO_HCSR04/module.php
+++ IPS2GPIO_HCSR04/module.php
@@ -38,6 +38,8 @@
 	            	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_value", "Pin" => $this->ReadPropertyInteger("Pin_O"), "Value" => 0)));
 	            }
 	            $this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
+	            // Erste Messung durchführen
+	            $this->Measurement();
         }
 	
 	public function ReceiveData($JSONString) 
