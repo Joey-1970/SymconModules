4b6f560184c57274813fb576b08dce2d1552d715
Joey-1970 <paeper@horburg.de>
2016-08-01 06:15:21 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -64,8 +64,12 @@
 
             	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
             	$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
+            	// Parameterdaten zum Baustein senden
             	$this->Setup();
+            	// Kalibrirungsdaten einlesen
             	$this->ReadCalibrateData();
+            	// Erste Messdaten einlesen
+            	$this->Measurement();
             	$this->SetStatus(102);
         }
 	
