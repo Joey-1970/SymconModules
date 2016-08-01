aa2302cdc5772f8efec7eac089811594b8535ec0
Joey-1970 <paeper@horburg.de>
2016-08-01 17:38:17 +0200

    Update module.php

--- IPS2GPIO_Display/module.php
+++ IPS2GPIO_Display/module.php
@@ -32,7 +32,7 @@
             	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_serial")));
 
 
-            }
+  
         }
 
 	public function RequestAction($Ident, $Value) 
