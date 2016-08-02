901451cd2409350f6d229f2661c77200519cef0a
Joey-1970 <paeper@horburg.de>
2016-08-02 15:20:20 +0200

    Update module.php

--- IPS2GPIO_Display/module.php
+++ IPS2GPIO_Display/module.php
@@ -27,11 +27,11 @@
 
              	If (GetValueInteger($this->GetIDForIdent("Handle")) >= 0) {
              		// Handle löschen
-             		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "close_handle_serial", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")))));
+             		//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "close_handle_serial", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")))));
              		SetValueInteger($this->GetIDForIdent("Handle"), -1);
              	}
             	// den Handle für dieses Gerät ermitteln
-            	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_serial")));
+            	//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_serial")));
 
 
   
