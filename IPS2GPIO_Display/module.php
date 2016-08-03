71e524b91e60996c53b042a79ee2ff0b545c2c2f
Joey-1970 <paeper@horburg.de>
2016-08-03 08:47:34 +0200

    Update module.php

--- IPS2GPIO_Display/module.php
+++ IPS2GPIO_Display/module.php
@@ -78,7 +78,7 @@
 	public function Brightness($Value)
 	{
 		$Value = min(100, max(0, $Value));
-		$Message = utf8_encode("dims=".$Value)."\xFF\xFF\xFF"; 
+		$Message = utf8_encode("dims=".$Value).chr(255).chr(255).chr(255); 
 		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "write_bytes_serial", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Command" => $Message)));
 
 	return;
