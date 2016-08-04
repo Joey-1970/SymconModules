bbeb1957d38948296c7f1397c459845f6a5dfbfb
Joey-1970 <paeper@horburg.de>
2016-08-04 11:31:36 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -20,6 +20,9 @@
             	parent::ApplyChanges();
             	//Connect to available splitter or create a new one
 	    	$this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
+	    	If (($this->ReadPropertyInteger("DeviceAddress") < 0) OR ($this->ReadPropertyInteger("DeviceAddress") > 128)) {
+	    		IPS_LogMessage("GPIO : ","I2C-Device Adresse in einem nicht definierten Bereich!");  
+	    	}
 	    	//Status-Variablen anlegen
 	    	$this->RegisterVariableInteger("HardwareRev", "HardwareRev", "", 100);
           	$this->DisableAction("HardwareRev");
