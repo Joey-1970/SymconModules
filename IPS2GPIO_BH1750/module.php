6756fe3c59f15b8d29e4190f405522cc4a43d72d
Joey-1970 <paeper@horburg.de>
2016-08-04 16:44:33 +0200

    Update module.php

--- IPS2GPIO_BH1750/module.php
+++ IPS2GPIO_BH1750/module.php
@@ -103,7 +103,7 @@
 	{
 		// Messwerte aktualisieren
 		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Value" => hexdec("10"))));
-		IPS_Sleep(120);
+		IPS_Sleep(150);
 		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => $this->ReadPropertyInteger("DeviceAddress"))));
 	return;
 	}	
