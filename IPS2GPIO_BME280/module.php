a3a6ca69b0d2e30c3971640c83b4c7e169af0983
Joey-1970 <paeper@horburg.de>
2016-08-05 06:09:19 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -54,7 +54,7 @@
              	
              	If (GetValueInteger($this->GetIDForIdent("Handle")) >= 0) {
              		// Handle löschen
-             		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "close_handle_i2c", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")))));
+             		//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "close_handle_i2c", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")))));
              		SetValueInteger($this->GetIDForIdent("Handle"), -1);
              	}
             	// den Handle für dieses Gerät ermitteln
