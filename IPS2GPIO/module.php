9474913b8efd684d12ed5830634025b52c517391
Joey-1970 <paeper@horburg.de>
2016-08-06 23:14:16 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -187,20 +187,25 @@ class IPS2GPIO_IO extends IPSModule
 		   	break;
 		   case "i2c_read_byte":
 		   	IPS_LogMessage("I2C Read Byte Parameter : ",$data->Handle." , ".$data->Register); 
-		   	$this->CommandClientSocket(pack("LLLL", 61, $data->Handle, $data->Register, 0), 16);
+		   	$this->CommandClientSocket(pack("L*", 61, $data->Handle, $data->Register, 0), 16);
 		   	break;
 		   case "i2c_read_word":
 		   	IPS_LogMessage("I2C Read Word Parameter : ",$data->Handle." , ".$data->Register); 
-		   	$this->CommandClientSocket(pack("LLLL", 63, $data->Handle, $data->Register, 0), 16);
+		   	$this->CommandClientSocket(pack("L*", 63, $data->Handle, $data->Register, 0), 16);
 		   	break; 
 		   case "i2c_read_block_byte":
 		   	//IPS_LogMessage("I2C Read Block Byte Parameter : ",$data->Handle." , ".$data->Register." , ".$data->Count);  	
-		   	$this->CommandClientSocket(pack("LLLLL", 67, $data->Handle, $data->Register, 4, $data->Count), 16 + ($data->Count));
+		   	$this->CommandClientSocket(pack("L*", 67, $data->Handle, $data->Register, 4, $data->Count), 16 + ($data->Count));
 			break;
 		   case "i2c_write_byte":
 		   	IPS_LogMessage("I2C Write Byte Parameter : ",$data->Handle." , ".$data->Register." , ".$data->Value);  	
-		   	$this->CommandClientSocket(pack("LLLLL", 62, $data->Handle, $data->Register, 4, $data->Value), 16);
+		   	$this->CommandClientSocket(pack("L*", 62, $data->Handle, $data->Register, 4, $data->Value), 16);
 		   	break;
+		   case "i2c_write_byte_onhandle":
+		   	IPS_LogMessage("I2C Write Byte Handle Parameter : ",$data->Handle." , ".$data->Value);  	
+		   	$this->CommandClientSocket(pack("L*", 60, $data->Handle, $data->Value, 0), 16);
+		   	break;	
+		   	
 		   
 		   // Serielle Kommunikation
 		   case "close_handle_serial":
@@ -430,6 +435,10 @@ class IPS2GPIO_IO extends IPSModule
 		        	break;
 		        case "55":
            			IPS_LogMessage("GPIO I2C Close Handle: ","Handle: ".$response[2]." Value: ".$response[4]);
+		            	break;
+		        case "60":
+           			IPS_LogMessage("GPIO I2C Write Byte Handle: ","Handle: ".$response[2]." Value: ".$response[4]);
+		            	//$this->SendDataToChildren(json_encode(Array("DataID" => "{8D44CA24-3B35-4918-9CBD-85A28C0C8917}", "Function"=>"set_i2c_data", "Handle" => $response[2], "Register" => $response[3], "Value" => $response[4])));
 		            	break;
 		        case "61":
            			IPS_LogMessage("GPIO I2C Read Byte: ","Handle: ".$response[2]." Register: ".$response[3]." Value: ".$response[4]);
