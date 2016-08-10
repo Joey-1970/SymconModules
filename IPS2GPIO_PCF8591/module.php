c33c1dec72f6ec83fed3624a051dda3e566d6509
Joey-1970 <paeper@horburg.de>
2016-08-10 19:13:36 +0200

    Update module.php

--- IPS2GPIO_PCF8591/module.php
+++ IPS2GPIO_PCF8591/module.php
@@ -173,7 +173,7 @@
 				//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => hexdec("40")|($i & 3) )));
 			}
 			SetValueBoolean($this->GetIDForIdent("WriteProtection"), false);
-			IPS_Sleep(50);
+			IPS_Sleep(100);
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_block_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => hexdec("40"), "Count" => 4)));
 
 		}
