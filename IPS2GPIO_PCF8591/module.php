2833b894e88e8545ca56ab9055f01c69a372f4d8
Joey-1970 <paeper@horburg.de>
2016-08-10 17:54:26 +0200

    Update module.php

--- IPS2GPIO_PCF8591/module.php
+++ IPS2GPIO_PCF8591/module.php
@@ -158,10 +158,10 @@
 		If (GetValueInteger($this->GetIDForIdent("Handle")) >= 0) {
 			// Aktualisierung der Messerte anfordern
 			SetValueBoolean($this->GetIDForIdent("WriteProtection"), true);
-			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_block_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => hexdec("40")|(3), "Count" => 4)));
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_block_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => hexdec("40"), "Count" => 4)));
 			SetValueBoolean($this->GetIDForIdent("WriteProtection"), false);
 			// Messwerte einlesen
-			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_block_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => hexdec("40")|(3), "Count" => 4)));
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_block_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => hexdec("40"), "Count" => 4)));
 
 /*
 			for ($i = 0; $i <= 3; $i++) {
