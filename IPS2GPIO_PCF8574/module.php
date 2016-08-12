59ad63eb3052aed3dad27092d4ede5a210cf386e
Joey-1970 <paeper@horburg.de>
2016-08-12 21:07:12 +0200

    Update module.php

--- IPS2GPIO_PCF8574/module.php
+++ IPS2GPIO_PCF8574/module.php
@@ -154,7 +154,7 @@
 	public function Read_Status()
 	{
 		If (GetValueInteger($this->GetIDForIdent("Handle")) >= 0) {
-			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => GetValueInteger($this->GetIDForIdent("DeviceAddress")) )));
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => $this->ReadPropertyInteger("DeviceAddress")) )));
 		}
 	return;
 	}
@@ -176,7 +176,7 @@
 			}
 			// Setzen der Ausgänge
 			$NewBitmask = $Bitmask | pow(2, $Pin);
-			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => GetValueInteger($this->GetIDForIdent("DeviceAddress")), "Value" => $NewBitmask)));
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte", "Handle" => GetValueInteger($this->GetIDForIdent("Handle")), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Value" => $NewBitmask)));
 		}
 	return;
 	}
