82793513702f35f5ea63c20217598c707f4aabdc
Joey-1970 <paeper@horburg.de>
2016-08-21 10:00:18 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -337,8 +337,8 @@ class IPS2GPIO_IO extends IPSModule
 		elseif (($this->ReadPropertyBoolean("I2C_Used") == true) AND (GetValueInteger($this->GetIDForIdent("HardwareRev"))) > 3) {
 			$PinUsed[] = 2; 
 			$PinUsed[] = 3;
-			//$this->CommandClientSocket(pack("LLLL", 0, 2, "0", 0), 16);
-			//$this->CommandClientSocket(pack("LLLL", 0, 3, "0", 0), 16);
+			$this->CommandClientSocket(pack("LLLL", 0, 2, "W", 0), 16);
+			$this->CommandClientSocket(pack("LLLL", 0, 3, "W", 0), 16);
 		}
 		elseif ($this->ReadPropertyBoolean("Serial_Used") == true)  {
 			$PinUsed[] = 14; 
