0b41d5246512bd6c7aaba9b73c231e1ee192829d
Joey-1970 <paeper@horburg.de>
2016-08-25 20:52:57 +0200

    Update module.php

--- IPS2GPIO_PCF8574/module.php
+++ IPS2GPIO_PCF8574/module.php
@@ -73,6 +73,7 @@
 
            	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
 		$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
+	        $this->Setup();
 	        // Erste Messdaten einlesen
 	        $this->Read_Status();
 	        $this->SetStatus(102);
@@ -152,6 +153,12 @@
 	{
 		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_byte_onhandle", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"))));
 	return;
+	}
+	
+	private function Setup()
+	{
+		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte_onhandle", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "Value" => 0)));
+	return;	
 	}
 	
 	public function Set_Pin_Output($Pin, $Value)
