9ea5a652c958b4eab71fc919e038408990706c7e
Joey-1970 <paeper@horburg.de>
2016-08-25 20:07:29 +0200

    Update module.php

--- IPS2GPIO_PCF8574/module.php
+++ IPS2GPIO_PCF8574/module.php
@@ -151,7 +151,7 @@
 	// Führt eine Messung aus
 	public function Read_Status()
 	{
-		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_byte", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "Register" => $this->ReadPropertyInteger("DeviceAddress") )));
+		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_byte_onhandle", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"))));
 	return;
 	}
 	
