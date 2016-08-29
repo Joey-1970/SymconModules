3b739609453af36e3b0f5432818920b988dd5474
Joey-1970 <paeper@horburg.de>
2016-08-29 14:07:50 +0200

    Update module.php

--- IPS2GPIO_Display/module.php
+++ IPS2GPIO_Display/module.php
@@ -37,7 +37,7 @@
             	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_serial", "Baud" => 9600, "Device" => $this->ReadPropertyString('ConnectionString') )));
 		
 		$this->Setup();
-
+		$this->SetStatus(102);
   
         }
 
