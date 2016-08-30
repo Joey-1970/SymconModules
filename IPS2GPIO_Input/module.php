4b56e70cdd2dea9dbaecdce63ec50b82dca63930
Joey-1970 <paeper@horburg.de>
2016-08-30 14:07:44 +0200

    Update module.php

--- IPS2GPIO_Input/module.php
+++ IPS2GPIO_Input/module.php
@@ -25,21 +25,27 @@
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() 
         {
-            // Diese Zeile nicht löschen
-            parent::ApplyChanges();
-            //Connect to available splitter or create a new one
-	    $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
+                // Diese Zeile nicht löschen
+                parent::ApplyChanges();
+                //Connect to available splitter or create a new one
+	        $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
 	   
-	    //Status-Variablen anlegen
-	    $this->RegisterVariableBoolean("Status", "Status", "~Switch", 10);
-            $this->DisableAction("Status");
-            $this->RegisterVariableBoolean("Toggle", "Toggle", "~Switch", 20);
-            $this->DisableAction("Toggle");
-            $this->RegisterVariableBoolean("Trigger", "Trigger", "~Switch", 30);
-            $this->DisableAction("Trigger");
-            If ($this->ReadPropertyInteger("Pin") >= 0) {
-            	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
-            }
+	        //Status-Variablen anlegen
+	        $this->RegisterVariableBoolean("Status", "Status", "~Switch", 10);
+                $this->DisableAction("Status");
+                $this->RegisterVariableBoolean("Toggle", "Toggle", "~Switch", 20);
+                $this->DisableAction("Toggle");
+                $this->RegisterVariableBoolean("Trigger", "Trigger", "~Switch", 30);
+                $this->DisableAction("Trigger");
+            
+                //ReceiveData-Filter setzen
+		$Filter = '.*"Pin":'.$this->ReadPropertyInteger("Pin").'.*|.*"Function":status.*|.*"Function":get_usedpin.*|.*"Function":get_notifypin.*';
+		//$this->SetReceiveDataFilter($Filter);
+
+            
+                If ($this->ReadPropertyInteger("Pin") >= 0) {
+            		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
+                }
         }
 	
 	public function ReceiveData($JSONString) 
