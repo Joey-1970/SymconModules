0bc9f731ad54c7762aba9f6ec196d7ad9d14d9b1
Joey-1970 <paeper@horburg.de>
2016-08-30 14:10:28 +0200

    Update module.php

--- IPS2GPIO_Output/module.php
+++ IPS2GPIO_Output/module.php
@@ -21,17 +21,23 @@
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() 
         {
-            // Diese Zeile nicht löschen
-            parent::ApplyChanges();
-            //Connect to available splitter or create a new one
-	    $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
+                 // Diese Zeile nicht löschen
+                 parent::ApplyChanges();
+                 //Connect to available splitter or create a new one
+	         $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
 	   
-	    //Status-Variablen anlegen
-	    $this->RegisterVariableBoolean("Status", "Status", "~Switch", 10);
-            $this->EnableAction("Status");
-            If ($this->ReadPropertyInteger("Pin") >= 0) {
-            	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
-            }
+	         //Status-Variablen anlegen
+	         $this->RegisterVariableBoolean("Status", "Status", "~Switch", 10);
+                 $this->EnableAction("Status");
+            
+                //ReceiveData-Filter setzen
+		$Filter = '.*"Pin":'.$this->ReadPropertyInteger("Pin").'.*|.*"Function":get_usedpin.*';
+		//$this->SetReceiveDataFilter($Filter);
+
+            
+                If ($this->ReadPropertyInteger("Pin") >= 0) {
+            		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
+                }
         }
 
 	public function RequestAction($Ident, $Value) 
