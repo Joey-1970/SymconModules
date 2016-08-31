ea680682aa0b2ec979d5425d441d2fd78697118c
Joey-1970 <paeper@horburg.de>
2016-08-31 08:18:37 +0200

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -33,8 +33,10 @@
 	        $this->EnableAction("Intensity");
            
            	//ReceiveData-Filter setzen
-		$Filter = '.*"Pin":'.$this->ReadPropertyInteger("Pin").'.*|.*"Function":get_usedpin.*';
-		//$this->SetReceiveDataFilter($Filter);
+		$Filter = '.*"Function":"get_usedpin".*|.*"Pin":'.$this->ReadPropertyInteger("Pin").'.*';
+
+		//$Filter = '.*"Pin":'.$this->ReadPropertyInteger("Pin").'.*|.*"Function":get_usedpin.*';
+		$this->SetReceiveDataFilter($Filter);
 		
 	        If ($this->ReadPropertyInteger("Pin") >= 0) {
 	        $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
