6e28d1c0d25b7c9b2fbc454eb717ebdd1dfd047c
Joey-1970 <paeper@horburg.de>
2016-08-31 08:02:54 +0200

    Update module.php

--- IPS2GPIO_BH1750/module.php
+++ IPS2GPIO_BH1750/module.php
@@ -43,10 +43,8 @@
 		IPS_ApplyChanges(IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}")[0]);
 		
 		//ReceiveData-Filter setzen
-		//$Filter = '.*"DeviceAddress":'.$this->ReadPropertyInteger("DeviceAddress").'.*|.*"Function":"status".*|.*"Function":"get_used_i2c".*';
-		//$Filter = '.*"'.$this->ReadPropertyInteger("DeviceAddress").'".*|.*"status".*|.*"get_used_i2c".*';
 		$Filter = '.*"Function":"get_used_i2c".*|.*"DeviceAddress":'.$this->ReadPropertyInteger("DeviceAddress").'.*|.*"Function":"status".*';
-		$this->SendDebug("IPS2GPIO", $Filter, 0);
+		//$this->SendDebug("IPS2GPIO", $Filter, 0);
 		$this->SetReceiveDataFilter($Filter);
 
            	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
