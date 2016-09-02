425b9acb8f1e220795cbb6a6246eacde8e132ed3
Joey-1970 <paeper@horburg.de>
2016-09-02 21:36:17 +0200

    Update module.php

--- IPS2GPIO_Output/module.php
+++ IPS2GPIO_Output/module.php
@@ -35,7 +35,8 @@
 		$this->SetReceiveDataFilter($Filter);
 
                 If ($this->ReadPropertyInteger("Pin") >= 0) {
-            		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
+                	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
+           		//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_pinupdate")));
                 }
         }
 
