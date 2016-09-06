9174ee6a641db07f3cc4ef6c49fa1433a706370c
Joey-1970 <paeper@horburg.de>
2016-09-06 12:17:33 +0200

    Update module.php

--- IPS2GPIO_Display/module.php
+++ IPS2GPIO_Display/module.php
@@ -34,7 +34,11 @@
 		$this->DisableAction("Baud");
 		IPS_SetHidden($this->GetIDForIdent("Baud"), true);
 		
-         	// den Handle für dieses Gerät ermitteln
+		//ReceiveData-Filter setzen 		    
+		$Filter = '((.*"Function":"get_serial".*|.*"Pin":14.*)|.*"Pin":15.*))'; 
+ 		$this->SetReceiveDataFilter($Filter); 
+ 
+        	// den Handle für dieses Gerät ermitteln
             	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_serial", "Baud" => 9600, "Device" => $this->ReadPropertyString('ConnectionString') )));
 		
 		$this->Setup();
