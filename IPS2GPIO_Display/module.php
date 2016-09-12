9e751cd2fa57bf9bc6ddaa03b574e652ae69e361
Joey-1970 <paeper@horburg.de>
2016-09-12 12:03:40 +0200

    Update module.php

--- IPS2GPIO_Display/module.php
+++ IPS2GPIO_Display/module.php
@@ -34,6 +34,22 @@
 		$this->DisableAction("Baud");
 		IPS_SetHidden($this->GetIDForIdent("Baud"), true);
 		
+		$this->RegisterVariableInteger("PageNumber", "PageNumber", "", 110);
+		$this->EnableAction("PageNumber");
+		IPS_SetHidden($this->GetIDForIdent("PageNumber"), false);
+		
+		$this->RegisterVariableInteger("Coordinate_X", "Coordinate_X", "", 110);
+		$this->DisableAction("Coordinate_X");
+		IPS_SetHidden($this->GetIDForIdent("Coordinate_X"), false);
+		
+		$this->RegisterVariableInteger("Coordinate_Y", "Coordinate_Y", "", 110);
+		$this->DisableAction("Coordinate_Y");
+		IPS_SetHidden($this->GetIDForIdent("Coordinate_Y"), false);
+		
+		$this->RegisterVariableBoolean("SleepMode", "SleepMode", "", 110);
+		$this->EnableAction("SleepMode");
+		IPS_SetHidden($this->GetIDForIdent("SleepMode"), false);
+		
 		$this->RegisterVariableString("Response", "Response", "", 120);
 		$this->DisableAction("Response");
 		IPS_SetHidden($this->GetIDForIdent("Response"), false);
@@ -142,16 +158,23 @@
 				IPS_LogMessage("IPS2GPIO Display","Fehler: I/O-Operation fehlgeschlagen");
 			break;
 			case "65": // Touch event return data 
-			
+				SetValueInteger($this->GetIDForIdent("PageNumber"), hexdec(substr($Message, 2, 2)));
+				
 			break;
 			case "66": // Current page ID number returns 
-			
+				SetValueInteger($this->GetIDForIdent("PageNumber"), hexdec(substr($Message, 2, 2)));
 			break;
 			case "67": // Touch coordinate data returns 
-			
+				$Coordinate_X = hexdec(substr($Message, 2, 2) + hexdec(substr($Message, 4, 2);
+				SetValueInteger($this->GetIDForIdent("Coordinate_X"), $Coordinate_X);
+				$Coordinate_Y = hexdec(substr($Message, 6, 2) + hexdec(substr($Message, 8, 2);
+				SetValueInteger($this->GetIDForIdent("Coordinate_Y"), $Coordinate_Y);
 			break;
 			case "68": // Touch Event in sleep mode 
-			
+				$Coordinate_X = hexdec(substr($Message, 2, 2) + hexdec(substr($Message, 4, 2);
+				SetValueInteger($this->GetIDForIdent("Coordinate_X"), $Coordinate_X);
+				$Coordinate_Y = hexdec(substr($Message, 6, 2) + hexdec(substr($Message, 8, 2);
+				SetValueInteger($this->GetIDForIdent("Coordinate_Y"), $Coordinate_Y);
 			break;
 			case "70": // String variable data returns  
 			
@@ -160,16 +183,16 @@
 			
 			break;
 			case "86": // Device automatically enters into sleep mode 
-			
+				SetValueBoolean($this->GetIDForIdent("SleepMode"), true);
 			break;
 			case "87": // Device automatically wake up 
-			
+				SetValueBoolean($this->GetIDForIdent("SleepMode"), false);
 			break;
 			case "88": // System successful start up 
-			
+				IPS_LogMessage("IPS2GPIO Display","Erfolgreicher Systemstart");
 			break;
 			case "89": // Start SD card upgrade 
-			
+				IPS_LogMessage("IPS2GPIO Display","SD-Card-Upgrade gestartet");
 			break;
 			case "fe": // Data transparent transmit ready 
 			
