88220c8b9f27903b84f9e77814a685d67864cff6
Joey-1970 <paeper@horburg.de>
2016-10-09 14:25:03 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -75,6 +75,7 @@
 	 	switch ($data->Function) {
 			   case "get_used_i2c":
 			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "InstanceID" => $this->InstanceID)));
+				$this->ApplyChanges();
 			   	break;
 			   case "status":
 			   	If ($data->HardwareRev <= 3) {
