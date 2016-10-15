6ae86c2bdf2ea54f46ec75120d322e140dad65a0
Joey-1970 <paeper@horburg.de>
2016-10-15 19:23:28 +0200

    Update module.php

--- IPS2GPIO_PCF8591/module.php
+++ IPS2GPIO_PCF8591/module.php
@@ -65,11 +65,13 @@
 		$Filter = '((.*"Function":"get_used_i2c".*|.*"DeviceAddress":'.$this->ReadPropertyInteger("DeviceAddress").'.*)|.*"Function":"status".*)';
 		$this->SetReceiveDataFilter($Filter);
 		
-		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "InstanceID" => $this->InstanceID)));
-            	$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
-            	// Erste Messdaten einlesen
-            	$this->Measurement();
-            	$this->SetStatus(102);
+		If (IPS_GetKernelRunlevel() == 10103) {
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "InstanceID" => $this->InstanceID)));
+			$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
+			// Erste Messdaten einlesen
+			$this->Measurement();
+			$this->SetStatus(102);
+		}
         }
 	
 	public function RequestAction($Ident, $Value) 
