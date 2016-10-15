1790b2ef9ee1b1a429fbaefe5045ea69665c7e3d
Joey-1970 <paeper@horburg.de>
2016-10-15 19:16:28 +0200

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -57,15 +57,17 @@
 		$Filter = '((.*"Function":"get_used_i2c".*|.*"DeviceAddress":'.$this->ReadPropertyInteger("DeviceAddress").'.*)|.*"Function":"status".*)';
 		$this->SetReceiveDataFilter($Filter);
 		
-		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "InstanceID" => $this->InstanceID)));
-           	$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
-            	// Parameterdaten zum Baustein senden
-            	$this->Setup();
-            	// Kalibrierungsdaten einlesen
-            	$this->ReadCalibrateData();
-            	// Erste Messdaten einlesen
-            	$this->Measurement();
-            	$this->SetStatus(102);
+		If (IPS_GetKernelRunlevel() == 10103) {
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "InstanceID" => $this->InstanceID)));
+			$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
+			// Parameterdaten zum Baustein senden
+			$this->Setup();
+			// Kalibrierungsdaten einlesen
+			$this->ReadCalibrateData();
+			// Erste Messdaten einlesen
+			$this->Measurement();
+			$this->SetStatus(102);
+		}
         }
 	
 	public function ReceiveData($JSONString) 
