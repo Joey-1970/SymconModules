1a9efd2c3c40010bf4eb4d2bb8f590fbe7795317
Joey-1970 <paeper@horburg.de>
2016-10-15 19:15:31 +0200

    Update module.php

--- IPS2GPIO_BH1750/module.php
+++ IPS2GPIO_BH1750/module.php
@@ -40,13 +40,15 @@
 		//$this->SendDebug("IPS2GPIO", $Filter, 0);
 		$this->SetReceiveDataFilter($Filter);
 		
-		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "InstanceID" => $this->InstanceID)));
-            	$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
-            	// Setup
-            	$this->Setup();
-            	// Erste Messdaten einlesen
-            	$this->Measurement();
-            	$this->SetStatus(102);
+		If (IPS_GetKernelRunlevel() == 10103) {
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "InstanceID" => $this->InstanceID)));
+			$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
+			// Setup
+			$this->Setup();
+			// Erste Messdaten einlesen
+			$this->Measurement();
+			$this->SetStatus(102);
+		}
         }
 	
 	public function ReceiveData($JSONString) 
