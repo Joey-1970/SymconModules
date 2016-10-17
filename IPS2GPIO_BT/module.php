a35a81cd764806866d4a4cdec348fbc2408430d1
Joey-1970 <paeper@horburg.de>
2016-10-17 20:06:54 +0200

    Update module.php

--- IPS2GPIO_BT/module.php
+++ IPS2GPIO_BT/module.php
@@ -97,7 +97,9 @@
 	// Führt eine Messung aus
 	public function Measurement()
 	{
-		
+		If ($this->ReadPropertyString("MAC".$i) <> "") {
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_BT_connect", "MAC" => $this->ReadPropertyString("MAC".$i) )));
+		}
 	}
 	
 }
