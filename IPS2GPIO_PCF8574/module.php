2d328413ef02e13ba25714a6301a9b4f266e4091
Joey-1970 <paeper@horburg.de>
2016-10-21 17:11:45 +0200

    Update module.php

--- IPS2GPIO_PCF8574/module.php
+++ IPS2GPIO_PCF8574/module.php
@@ -182,7 +182,7 @@
 	return;	
 	}
 	
-	public function SetPinOutput(Integer $Pin, $Value)
+	public function SetPinOutput(Int $Pin, Bool $Value)
 	{
 		// Setzt einen bestimmten Pin auf den vorgegebenen Wert
 		$Pin = min(7, max(0, $Pin));
@@ -203,7 +203,7 @@
 	return;
 	}
 	
-	public function SetOutput(Integer $Value)
+	public function SetOutput(Int $Value)
 	{
 		// Setzt alle Ausgänge
 		$Value = min(255, max(0, $Value));
