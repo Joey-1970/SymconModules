3350ef4eba48fba10da62781dcb922a916853417
Joey-1970 <paeper@horburg.de>
2016-10-28 20:25:00 +0200

    Update module.php

--- IPS2GPIO_iAQ/module.php
+++ IPS2GPIO_iAQ/module.php
@@ -108,6 +108,10 @@
 	// Führt eine Messung aus
 	public function Measurement()
 	{
+		// Daten anfordern
+		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_byte", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "Register" => hexdec("5B") )));
+
+		//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "Register" => hexdec("5B"), "Value" => $Value)));
 		//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_word", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "Register" => $this->ReadPropertyInteger("DeviceAddress"))));
 	return;
 	}	
