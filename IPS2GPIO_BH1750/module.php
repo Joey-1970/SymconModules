0a86c52bed43dd4a3b7b704eb7acc5b4bba45a94
Joey-1970 <paeper@horburg.de>
2016-11-04 09:29:17 +0100

    Update module.php

--- IPS2GPIO_BH1750/module.php
+++ IPS2GPIO_BH1750/module.php
@@ -48,9 +48,9 @@
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
 			$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
 			// Setup
-			$this->Setup();
+			//$this->Setup();
 			// Erste Messdaten einlesen
-			$this->Measurement();
+			//$this->Measurement();
 			$this->SetStatus(102);
 		}
         }
