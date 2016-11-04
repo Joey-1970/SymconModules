99a4d59862900c7d88afc8a8225dc031391775d6
Joey-1970 <paeper@horburg.de>
2016-11-04 20:15:26 +0100

    Update module.php

--- IPS2GPIO_BME280/module.php
+++ IPS2GPIO_BME280/module.php
@@ -341,7 +341,9 @@
 	private function PressureTrend(int $interval)
 	{
 		$LoggingArray = AC_GetLoggedValues(IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}")[0], $this->GetIDForIdent("Pressure"), time()- (3600 * $interval), time(), 0); 
-		$Result = round($LoggingArray[0]["Value"] - array_pop($LoggingArray), 1); 
+		$Aktuell = $LoggingArray[0]['Value'];
+		$Historisch = array_pop($LoggingArray);
+		$Result = round($Aktuell - $Historisch, 1); 
 	return $Result;
 	}
 	    
