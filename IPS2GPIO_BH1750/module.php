98638bbcb5ccf282f20c0f953161e089315f28f2
Joey-1970 <paeper@horburg.de>
2016-11-06 18:27:21 +0100

    Update module.php

--- IPS2GPIO_BH1750/module.php
+++ IPS2GPIO_BH1750/module.php
@@ -12,6 +12,8 @@
 		$this->RegisterPropertyInteger("DeviceBus", 1);
  	    	$this->RegisterPropertyInteger("Messzyklus", 60);
  	    	$this->RegisterPropertyBoolean("Logging", false);
+		$this->RegisterPropertyInteger("HysteresisOn", 0);
+		$this->RegisterPropertyInteger("HysteresisOff", 0);
             	$this->RegisterTimer("Messzyklus", 0, 'I2GBH_Measurement($_IPS["TARGET"]);');
         }
  
@@ -32,6 +34,10 @@
              	$this->RegisterVariableInteger("Illuminance", "Illuminance", "illuminance.lx", 10);
 		$this->DisableAction("Illuminance");
 		IPS_SetHidden($this->GetIDForIdent("Illuminance"), false);
+		
+		$this->RegisterVariableBoolean("Hysteresis", "Hysteresis", "~Switch", 20);
+		$this->DisableAction("Hysteresis");
+		IPS_SetHidden($this->GetIDForIdent("Hysteresis"), false);
 		
 		If (IPS_GetKernelRunlevel() == 10103) {
 			// Logging setzen
@@ -82,6 +88,9 @@
 			  			$Lux = (($data->Value & 0xff00)>>8) | (($data->Value & 0x00ff)<<8);
 			  			$Lux = max(0, $Lux);
 			  			SetValueInteger($this->GetIDForIdent("Illuminance"), $Lux);
+						If ($Lux >= $this->ReadPropertyInteger("HysteresisOn")) {
+							SetValueBoolean($this->GetIDForIdent("Hysteresis"), true);
+						}
 			  		}
 			  		
 			  	}
