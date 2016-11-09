03ffd605c60885bc304d808a9630ddd276d29a9e
Joey-1970 <paeper@horburg.de>
2016-11-09 12:21:07 +0100

    Update module.php

--- IPS2GPIO_RPi/module.php
+++ IPS2GPIO_RPi/module.php
@@ -243,6 +243,13 @@
 		
 	}    
 	
+	public function PiShutdown()
+	{
+		$Command = "sudo shutdown –h";
+		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_RPi_connect", "InstanceID" => $this->InstanceID,  "Command" => $Command, "CommandNumber" => 3, "IsArray" => false )));
+		
+	}       
+	    
 	public function SetDisplayPower(bool $Value)
 	{
 		If ($Value == true) {
