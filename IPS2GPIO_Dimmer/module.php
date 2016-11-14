be84f5c6abf7b4522590a35b81d1607d38ef446b
Joey-1970 <paeper@horburg.de>
2016-11-14 20:49:04 +0100

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -21,9 +21,9 @@
 		   
 		//Status-Variablen anlegen
 		$this->RegisterVariableBoolean("Status", "Status", "~Switch", 10);
-	        $this->DisableAction("Status");
+	        $this->EnableAction("Status");
 	        $this->RegisterVariableInteger("Intensity", "Intensity", "~Intensity.255", 20);
-	        $this->DisableAction("Intensity");
+	        $this->EnableAction("Intensity");
            
            	//ReceiveData-Filter setzen
 		$Filter = '(.*"Function":"get_usedpin".*|.*"Pin":'.$this->ReadPropertyInteger("Pin").'.*)';
