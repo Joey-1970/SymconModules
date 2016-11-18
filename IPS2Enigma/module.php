3b519c5852ef1cc11a11ad4b2446f0d33f76f7c7
Joey-1970 <paeper@horburg.de>
2016-11-18 22:52:35 +0100

    Update module.php

--- IPS2Enigma/module.php
+++ IPS2Enigma/module.php
@@ -1,38 +1,26 @@
 <?
     // Klassendefinition
-    class IPS2GPIO_Output extends IPSModule 
+    class IPS2Enigma extends IPSModule 
     {
 	// Überschreibt die interne IPS_Create($id) Funktion
         public function Create() 
         {
             	// Diese Zeile nicht löschen.
             	parent::Create();
-            	$this->RegisterPropertyInteger("Pin", -1);
-		$this->RegisterPropertyBoolean("Logging", false);
- 	    	$this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
+           	$this->RegisterPropertyBoolean("Open", 0);
+	    	$this->RegisterPropertyString("IPAddress", "127.0.0.1");
         }
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() 
         {
 		// Diese Zeile nicht löschen
 		parent::ApplyChanges();
-		//Connect to available splitter or create a new one
-		$this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
 		//Status-Variablen anlegen
 		$this->RegisterVariableBoolean("Status", "Status", "~Switch", 10);
 		$this->EnableAction("Status");
             	
-		// Logging setzen
-		AC_SetLoggingStatus(IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}")[0], $this->GetIDForIdent("Status"), $this->ReadPropertyBoolean("Logging"));
-		IPS_ApplyChanges(IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}")[0]);
-             	//ReceiveData-Filter setzen
-                $Filter = '(.*"Function":"get_usedpin".*|.*"Pin":'.$this->ReadPropertyInteger("Pin").'.*)';
-		$this->SetReceiveDataFilter($Filter);
-		If (IPS_GetKernelRunlevel() == 10103) {
-			If ($this->ReadPropertyInteger("Pin") >= 0) {
-				$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
-			}
-		}
+
+
         }
 	public function RequestAction($Ident, $Value) 
 	{
@@ -47,42 +35,20 @@
 	    }
 	}
 	
-	public function ReceiveData($JSONString) 
-	{
-	    	// Empfangene Daten vom Gateway/Splitter
-	    	$data = json_decode($JSONString);
-	 	switch ($data->Function) {
-			   case "get_usedpin":
-			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
-			   	break;
-			   case "status":
-			   	If ($data->Pin == $this->ReadPropertyInteger("Pin")) {
-			   		$this->SetStatus($data->Status);
-			   	}
-			   	break;
-			  case "result":
-				If ($data->Pin == $this->ReadPropertyInteger("Pin")) {
-			   		SetValueBoolean($this->GetIDForIdent("Status"), $data->Value);
-				}
-			break;
-			   case "freepin":
-			   	// Funktion zum erstellen dynamischer Pulldown-Menüs
-			   	break;
-	 	}
-	return;
- 	}
+
 	// Beginn der Funktionen
 	
 	// Schaltet den gewaehlten Pin
 	public function Set_Status(Bool $Value)
 	{
-		$this->SendDataToParent(json_encode(Array("DataID"=>"{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_value", "Pin" => $this->ReadPropertyInteger("Pin"), "Value" => $Value)));
+	
+	return;
 	}
 	
 	// Toggelt den Status
 	public function Toggle_Status()
 	{
-		$this->Set_Status(!GetValueBoolean($this->GetIDForIdent("Status")));
+	
 	return;
 	}
 }
