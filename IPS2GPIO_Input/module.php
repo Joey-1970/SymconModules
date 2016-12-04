512b545c1fdb9368f08e3ef8aff41c1fa647c7ac
Joey-1970 <paeper@horburg.de>
2016-12-04 18:34:14 +0100

    Update module.php

--- IPS2GPIO_Input/module.php
+++ IPS2GPIO_Input/module.php
@@ -5,15 +5,17 @@
 	// Überschreibt die interne IPS_Create($id) Funktion
         public function Create() 
         {
-            // Diese Zeile nicht löschen.
-            parent::Create();
-            $this->RegisterPropertyInteger("Pin", -1);
-            $this->RegisterPropertyBoolean("ActionValue", true);
-            $this->RegisterPropertyInteger("GlitchFilter", 10);
-	    $this->RegisterPropertyString("PUL", "o");
-            $this->RegisterPropertyInteger("TriggerScript", 0);
-            $this->RegisterPropertyInteger("ToggleScript", 0);
- 	    $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
+            	// Diese Zeile nicht löschen.
+            	parent::Create();
+            	$this->RegisterPropertyBoolean("Open", false);
+		$this->RegisterPropertyInteger("Pin", -1);
+            	$this->RegisterPropertyBoolean("ActionValue", true);
+            	$this->RegisterPropertyInteger("GlitchFilter", 10);
+	    	$this->RegisterPropertyString("PUL", "o");
+            	$this->RegisterPropertyInteger("TriggerScript", 0);
+            	$this->RegisterPropertyInteger("ToggleScript", 0);
+ 	    	$this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
+	return;
         }
 
        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
@@ -37,12 +39,17 @@
 		$this->SetReceiveDataFilter($Filter);
 		
 		If (IPS_GetKernelRunlevel() == 10103) {
-			If ($this->ReadPropertyInteger("Pin") >= 0) {
+			If (($this->ReadPropertyInteger("Pin") >= 0) AND ($this->ReadPropertyBoolean("Open") == true)) {
 				$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", 
 									  "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 0, "Notify" => true, "GlitchFilter" => $this->ReadPropertyInteger("GlitchFilter"), "Resistance" => $this->ReadPropertyString("PUL"))));
+				$this->SetStatus(102);
+			}
+			else {
+				$this->SetStatus(104);
 			}
 		}
-        }
+        return;
+	}
 	
 	public function ReceiveData($JSONString) 
 	{
