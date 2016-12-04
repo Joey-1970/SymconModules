3c380e0b1a4ab22bc1ead78010ec3a62862d553e
Joey-1970 <paeper@horburg.de>
2016-12-04 09:32:50 +0100

    Update module.php

--- IPS2GPIO_Display/module.php
+++ IPS2GPIO_Display/module.php
@@ -7,7 +7,8 @@
         {
             // Diese Zeile nicht löschen.
             parent::Create();
-            $this->RegisterPropertyInteger("Baud", 3);
+            $this->RegisterPropertyBoolean("Open", false);
+	    $this->RegisterPropertyInteger("Baud", 3);
             $this->RegisterPropertyString("ConnectionString", "/dev/ttyAMA0");
             $this->RegisterPropertyBoolean("DateTime", true);
             $this->RegisterPropertyInteger("Brightness", 100);
@@ -110,9 +111,14 @@
         	If (IPS_GetKernelRunlevel() == 10103) {
 			// den Handle für dieses Gerät ermitteln
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_serial", "Baud" => 9600, "Device" => $this->ReadPropertyString('ConnectionString'), "InstanceID" => $this->InstanceID )));
-
-			$this->Setup();
-			$this->SetStatus(102);
+			
+			If ($this->ReadPropertyBoolean("Open") == true) {
+				$this->Setup();
+				$this->SetStatus(102);
+			}
+			else {
+				$this->SetStatus(104);
+			}
 		}
         }
 
