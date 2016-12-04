80286906eb5a146b5a6c809d5acd1f0280638590
Joey-1970 <paeper@horburg.de>
2016-12-04 19:20:48 +0100

    Update module.php

--- IPS2GPIO_Dimmer/module.php
+++ IPS2GPIO_Dimmer/module.php
@@ -94,13 +94,15 @@
 	// Dimmt den gewaehlten Pin
 	public function Set_Intensity(Int $value)
 	{
-		$value = min(255, max(0, $value));
-		If (GetValueBoolean($this->GetIDForIdent("Status")) == true) {
-			$this->SendDataToParent(json_encode(Array("DataID"=>"{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin"), "Value" => $value)));
- 		}
- 		else {
- 			SetValueInteger($this->GetIDForIdent("Intensity"), $value);
- 		}
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$value = min(255, max(0, $value));
+			If (GetValueBoolean($this->GetIDForIdent("Status")) == true) {
+				$this->SendDataToParent(json_encode(Array("DataID"=>"{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin"), "Value" => $value)));
+			}
+			else {
+				SetValueInteger($this->GetIDForIdent("Intensity"), $value);
+			}
+		}
 	return;
 	}
 	
