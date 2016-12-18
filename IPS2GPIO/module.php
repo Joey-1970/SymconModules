15a99d5de77f46d78391035c749218806700c0e9
Joey-1970 <paeper@horburg.de>
2016-12-18 13:29:20 +0100

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -957,8 +957,9 @@ class IPS2GPIO_IO extends IPSModule
 			$login = @$ssh->login($this->ReadPropertyString("User"), $this->ReadPropertyString("Password"));
 			if ($login == false)
 			{
-			    IPS_LogMessage("IPS2GPIO SSH-Connect","Angegebene IP ".$this->ReadPropertyString("IPAddress")." reagiert nicht!");
-			    return false;
+			    	IPS_LogMessage("IPS2GPIO SSH-Connect","Angegebene IP ".$this->ReadPropertyString("IPAddress")." reagiert nicht!");
+			    	$result = "";
+				return false;
 			}
 			$ResultArray = Array();
 			$CommandArray = unserialize($Command);
