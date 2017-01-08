5424c48287125f35ef14ccc11e53fbc72369429a
Joey-1970 <paeper@horburg.de>
2017-01-08 20:09:35 +0100

    Update module.php

--- IPS2GPIO_1Wire/module.php
+++ IPS2GPIO_1Wire/module.php
@@ -8,12 +8,6 @@
             	// Diese Zeile nicht löschen.
             	parent::Create();
             	$this->RegisterPropertyBoolean("Open", false);
-		$this->RegisterPropertyInteger("Pin", -1);
-            	$this->RegisterPropertyBoolean("ActionValue", true);
-            	$this->RegisterPropertyInteger("GlitchFilter", 10);
-	    	$this->RegisterPropertyString("PUL", "o");
-            	$this->RegisterPropertyInteger("TriggerScript", 0);
-            	$this->RegisterPropertyInteger("ToggleScript", 0);
  	    	$this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
         }
        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
@@ -25,12 +19,7 @@
 	        $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
 	   
 	        //Status-Variablen anlegen
-	        $this->RegisterVariableBoolean("Status", "Status", "~Switch", 10);
-                $this->DisableAction("Status");
-                $this->RegisterVariableBoolean("Toggle", "Toggle", "~Switch", 20);
-                $this->DisableAction("Toggle");
-                $this->RegisterVariableBoolean("Trigger", "Trigger", "~Switch", 30);
-                $this->DisableAction("Trigger");
+	        
             
                 //ReceiveData-Filter setzen
 		$Filter = '(.*"Function":"get_usedpin".*|.*"Pin":'.$this->ReadPropertyInteger("Pin").'.*)';
@@ -53,27 +42,7 @@
 	    	// Empfangene Daten vom Gateway/Splitter
 	    	$data = json_decode($JSONString);
 	 	switch ($data->Function) {
-			   case "notify":
-			   	If ($data->Pin == $this->ReadPropertyInteger("Pin")) {
-			   		// Trigger kurzzeitig setzen
-			   		If ($data->Value == $this->ReadPropertyBoolean("ActionValue") ) {
-			   			SetValueBoolean($this->GetIDForIdent("Trigger"), true);
-			   			If ($this->ReadPropertyInteger("TriggerScript") > 0) {
-			   				IPS_RunScript($this->ReadPropertyInteger("TriggerScript"));
-			   			}
-			   			SetValueBoolean($this->GetIDForIdent("Trigger"), false);
-			   		}
-			   		// Toggle-Variable
-			   		If ((GetValueBoolean($this->GetIDForIdent("Status")) == false) and ($data->Value == true)) {
-			   			SetValueBoolean($this->GetIDForIdent("Toggle"), !GetValueBoolean($this->GetIDForIdent("Toggle")));
-			   			If ($this->ReadPropertyInteger("ToggleScript") > 0) {
-			   				IPS_RunScript($this->ReadPropertyInteger("ToggleScript"));
-			   			}
-			   		}
-			   		// Status setzen
-			   		SetValueBoolean($this->GetIDForIdent("Status"), $data->Value);
-			   	}
-			   	break;
+			  
 			   case "get_usedpin":
 			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", 
 									  "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 0, "Notify" => true, "GlitchFilter" => $this->ReadPropertyInteger("GlitchFilter"), "Resistance" => $this->ReadPropertyString("PUL"))));
