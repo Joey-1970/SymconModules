5356be77c98d1d31cac8eb6e7d82c64964faaa5b
Joey-1970 <paeper@horburg.de>
2017-01-09 07:27:30 +0100

    Update module.php

--- IPS2GPIO_Input/module.php
+++ IPS2GPIO_Input/module.php
@@ -76,9 +76,11 @@
 			   	}
 			   	break;
 			   case "get_usedpin":
-			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", 
+			   	If ($this->ReadPropertyBoolean("Open") == true) {
+					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", 
 									  "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 0, "Notify" => true, "GlitchFilter" => $this->ReadPropertyInteger("GlitchFilter"), "Resistance" => $this->ReadPropertyString("PUL"))));
-			   	break;
+				}
+				break;
 			   case "status":
 			   	If ($data->Pin == $this->ReadPropertyInteger("Pin")) {
 			   		$this->SetStatus($data->Status);
