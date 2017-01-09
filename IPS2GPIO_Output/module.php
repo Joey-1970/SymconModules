38f4d2cf34cefdde754d5108551e1e5d599d2fe5
Joey-1970 <paeper@horburg.de>
2017-01-09 07:27:57 +0100

    Update module.php

--- IPS2GPIO_Output/module.php
+++ IPS2GPIO_Output/module.php
@@ -62,8 +62,10 @@
 	    	$data = json_decode($JSONString);
 	 	switch ($data->Function) {
 			   case "get_usedpin":
-			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
-			   	break;
+			   	If ($this->ReadPropertyBoolean("Open") == true) {
+					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
+				}
+				break;
 			   case "status":
 			   	If ($data->Pin == $this->ReadPropertyInteger("Pin")) {
 			   		$this->SetStatus($data->Status);
