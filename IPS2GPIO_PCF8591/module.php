818161d2911ce067be8043d593688265f3ee3aa8
Joey-1970 <paeper@horburg.de>
2017-01-09 07:28:59 +0100

    Update module.php

--- IPS2GPIO_PCF8591/module.php
+++ IPS2GPIO_PCF8591/module.php
@@ -101,8 +101,10 @@
 	    	$data = json_decode($JSONString);
 	 	switch ($data->Function) {
 			   case "get_used_i2c":
-			   	$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
-			   	$this->ApplyChanges();
+			   	If ($this->ReadPropertyBoolean("Open") == true) {
+					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
+					$this->ApplyChanges();
+				}
 				break;
 			   case "status":
 			   	If ($data->HardwareRev <= 3) {
