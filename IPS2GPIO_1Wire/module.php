156897323a6660ef8927c24129b3c7f9545c4a62
Joey-1970 <paeper@horburg.de>
2017-01-17 14:08:15 +0100

    Update module.php

--- IPS2GPIO_1Wire/module.php
+++ IPS2GPIO_1Wire/module.php
@@ -81,9 +81,11 @@
 			case "set_1wire_data":
 			   	$ResultArray = unserialize(utf8_decode($data->Result));
 				$SensorArray = unserialize(GetValueString($this->GetIDForIdent("SensorArray")));
+				
 				If (count($ResultArray) > 0 ) {
 					for ($i = 0; $i < Count($ResultArray); $i++) {
 						$Ident = "Sensor_".str_replace("-", "", $SensorArray[$i]);
+						//$LinesArray = explode(" ", $ResultArray[$i]);
 
 						SetValueFloat($this->GetIDForIdent("$Ident"), (int)substr($ResultArray[$i], -6) / 1000);
 						//IPS_LogMessage("IPS2GPIO 1-Wire: ","Sensorantwort: ".$ResultArray[$i]);
