d5adebf920e84e9572d60fa04f0e9f158dd6fa00
Joey-1970 <paeper@horburg.de>
2017-01-17 08:46:35 +0100

    Update module.php

--- IPS2GPIO_1Wire/module.php
+++ IPS2GPIO_1Wire/module.php
@@ -28,6 +28,7 @@
 		If (IPS_GetKernelRunlevel() == 10103) {
 			If ($this->ReadPropertyBoolean("Open") == true) {
 				$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => 4, "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
+				$this->Setup();
 				$this->SetStatus(102);
 			}
 			else {
@@ -58,6 +59,11 @@
 	 	}
  	}
 	// Beginn der Funktionen
+	private function Setup()
+	{
+		
+	}
+	    
 	
 }
 ?>
