fc822f172be3d2c42eec1f63c14c4961aef95133
Joey-1970 <paeper@horburg.de>
2017-01-17 10:46:24 +0100

    Update module.php

--- IPS2GPIO_1Wire/module.php
+++ IPS2GPIO_1Wire/module.php
@@ -8,6 +8,8 @@
             	// Diese Zeile nicht löschen.
             	parent::Create();
             	$this->RegisterPropertyBoolean("Open", false);
+		$this->RegisterPropertyInteger("Messzyklus", 5);
+		$this->RegisterTimer("Messzyklus", 0, 'I2G1W_Measurement($_IPS["TARGET"]);');
  	    	$this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
         }
        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
@@ -19,7 +21,9 @@
 	        $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
 	   
 	        //Status-Variablen anlegen
-	        // da kommt einiges
+	        $this->RegisterVariableString("SensorArray", "SensorArray", "", 5);
+		$this->DisableAction("SensorArray");
+		IPS_SetHidden($this->GetIDForIdent("SensorArray"), true);
             
                 //ReceiveData-Filter setzen
 		$Filter = '((.*"Function":"get_usedpin".*|.*"Pin":"4".*)|.*"InstanceID":'.$this->InstanceID.'.*)';
@@ -30,6 +34,9 @@
 			If ($this->ReadPropertyBoolean("Open") == true) {
 				$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => 4, "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
 				$this->Setup();
+				$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
+				// Erste Messung durchführen
+				$this->Measurement();
 				$this->SetStatus(102);
 			}
 			else {
@@ -59,6 +66,7 @@
 			   	break;
 			case "set_1wire_devices":
 			   	$ResultArray = unserialize(utf8_decode($data->Result));
+				SetValueString($this->GetIDForIdent("SensorArray"), utf8_decode($data->Result));
 				for ($i = 0; $i < Count($ResultArray); $i++) {
 					//IPS_LogMessage("IPS2GPIO 1-Wire: ","Sensor ".$ResultArray[$i]);
 					$Ident = "Sensor_".str_replace("-", "", $ResultArray[$i]);
@@ -71,9 +79,22 @@
 	// Beginn der Funktionen
 	private function Setup()
 	{
+		// Ermittlung der angeschlossenen Sensoren
 		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_1wire_devices", "InstanceID" => $this->InstanceID )));
 	}
 	    
-	
+	public function Measurement()
+	{
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$CommandArray = Array();
+			// Zusammenstellung der Sensoren
+			$SensorArray = unserialize(SetValueString($this->GetIDForIdent("SensorArray")));
+			for ($i = 0; $i < Count($SensorArray); $i++) {
+				$CommandArray[$i] = "cat /sys/bus/w1/devices/'.$SensorArray[$i].'/w1_slave";
+				IPS_LogMessage("IPS2GPIO 1-Wire: ","Sensor ".$CommandArray[$i]);
+			}
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_1W_data", "InstanceID" => $this->InstanceID,  "Command" => serialize($CommandArray) )));
+		}
+	}
 }
 ?>
