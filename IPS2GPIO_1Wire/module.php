33c686b0ff9a426c7187d25b01cf32caee2887f4
Joey-1970 <paeper@horburg.de>
2017-01-17 14:56:50 +0100

    Update module.php

--- IPS2GPIO_1Wire/module.php
+++ IPS2GPIO_1Wire/module.php
@@ -86,7 +86,7 @@
 				If (count($ResultArray) > 0 ) {
 					for ($i = 0; $i < Count($ResultArray); $i++) {
 						$Ident = "Sensor_".str_replace("-", "", $SensorArray[$i]);
-						$LinesArray = explode("/n", $ResultArray[$i]);
+						$LinesArray = explode(chr(10), $ResultArray[$i]);
 						IPS_LogMessage("IPS2GPIO 1-Wire","Linie 1: ".$LinesArray[0]);
 						SetValueFloat($this->GetIDForIdent("$Ident"), (int)substr($ResultArray[$i], -6) / 1000);
 						//IPS_LogMessage("IPS2GPIO 1-Wire: ","Sensorantwort: ".$ResultArray[$i]);
