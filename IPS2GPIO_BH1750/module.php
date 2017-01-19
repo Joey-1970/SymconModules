7d2bf721e8e04681ec03063486ebfcea7397a613
Joey-1970 <paeper@horburg.de>
2017-01-19 17:15:41 +0100

    Update module.php

--- IPS2GPIO_BH1750/module.php
+++ IPS2GPIO_BH1750/module.php
@@ -51,10 +51,10 @@
 			//$this->SendDebug("IPS2GPIO", $Filter, 0);
 			$this->SetReceiveDataFilter($Filter);
 		
-			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
-			$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
 			
 			If ($this->ReadPropertyBoolean("Open") == true) {
+				$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
+				$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
 				// Setup
 				$this->Setup();
 				// Erste Messdaten einlesen
@@ -62,8 +62,12 @@
 				$this->SetStatus(102);
 			}
 			else {
+				$this->SetTimerInterval("Messzyklus", 0);
 				$this->SetStatus(104);
 			}	
+		}
+		else {
+			$this->SetTimerInterval("Messzyklus", 0);
 		}
 	}
 	
