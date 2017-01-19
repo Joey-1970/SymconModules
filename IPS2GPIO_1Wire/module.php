66fcc29e42a9f7f65ee6bbc6e863a4adb3700eac
Joey-1970 <paeper@horburg.de>
2017-01-19 12:29:30 +0100

    Update module.php

--- IPS2GPIO_1Wire/module.php
+++ IPS2GPIO_1Wire/module.php
@@ -54,6 +54,7 @@
 			case "get_usedpin":
 			   	If ($this->ReadPropertyBoolean("Open") == true) {
 					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => 4, "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
+					$this->ApplyChanges();
 				}
 				break;
 			case "status":
