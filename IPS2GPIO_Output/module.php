25767e1f971dd6cc8d802373e575901ca11e63fb
Joey-1970 <paeper@horburg.de>
2017-01-21 11:34:57 +0100

    Update module.php

--- IPS2GPIO_Output/module.php
+++ IPS2GPIO_Output/module.php
@@ -34,8 +34,13 @@
 		$this->SetReceiveDataFilter($Filter);
 
 		If (IPS_GetKernelRunlevel() == 10103) {
-			If ($this->ReadPropertyInteger("Pin") >= 0) {
-				$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
+			If (($this->ReadPropertyInteger("Pin") >= 0) AND ($this->ReadPropertyBoolean("Open") == true)) {
+				$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", 
+									  "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
+				$this->SetStatus(102);
+			}
+			else {
+				$this->SetStatus(104);
 			}
 		}
 	}
