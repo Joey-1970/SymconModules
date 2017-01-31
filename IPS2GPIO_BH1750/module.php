e34a67ac7c5f0587f61052274008549592de3e09
Joey-1970 <paeper@horburg.de>
2017-01-31 21:51:33 +0100

    Update module.php

--- IPS2GPIO_BH1750/module.php
+++ IPS2GPIO_BH1750/module.php
@@ -134,6 +134,9 @@
 	public function Measurement()
 	{
 		If ($this->ReadPropertyBoolean("Open") == true) {
+			// Messwerterfassung setzen
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte_onhandle", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Value" => $this->ReadPropertyInteger("Resulution") )));
+
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_word", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"))));
 		}
 	}	
