359daea113bc0ad0491149a7af801c228a08e0e5
Joey-1970 <paeper@horburg.de>
2017-02-07 19:33:42 +0100

    Update module.php

--- IPS2GPIO_MCP3424/module.php
+++ IPS2GPIO_MCP3424/module.php
@@ -178,9 +178,9 @@
 				// Test
 				$MeasurementData = unserialize($this->GetBuffer("MeasurementData"));
 				IPS_LogMessage("IPS2GPIO MCP", "Anzahl Daten: ".count($MeasurementData));
-				IPS_LogMessage("IPS2GPIO MCP", "Daten 1: ".$MeasurementData[0]);
-				IPS_LogMessage("IPS2GPIO MCP", "Daten 2: ".$MeasurementData[1]);
-				IPS_LogMessage("IPS2GPIO MCP", "Daten 3: ".$MeasurementData[2]);
+				IPS_LogMessage("IPS2GPIO MCP", "Daten 1: ".$MeasurementData[1]);
+				IPS_LogMessage("IPS2GPIO MCP", "Daten 2: ".$MeasurementData[2]);
+				IPS_LogMessage("IPS2GPIO MCP", "Daten 3: ".$MeasurementData[3]);
 			   	break;
 	 	}
  	}
@@ -210,10 +210,10 @@
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			for ($i = 0; $i <= 3; $i++) {
 				$Configuration = 128 | ($i << 5) | (1 << 4) | ($this->ReadPropertyInteger("Resolution_".$i) << 2) | $this->ReadPropertyInteger("Amplifier_".$i);
-				IPS_LogMessage("IPS2GPIO MCP", "Setup Methode 1");
+				//IPS_LogMessage("IPS2GPIO MCP", "Setup Methode 1");
 				$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte_onhandle", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Value" => $Configuration)));
-				IPS_LogMessage("IPS2GPIO MCP", "Setup Methode 2");
-				$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Value" => $Configuration)));
+				//IPS_LogMessage("IPS2GPIO MCP", "Setup Methode 2");
+				//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Value" => $Configuration)));
 			}
 		} 
 	}
