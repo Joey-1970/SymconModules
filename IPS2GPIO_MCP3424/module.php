f2f921dbc850e2d0ee2c6891d720556463b0c956
Joey-1970 <paeper@horburg.de>
2017-02-08 07:40:29 +0100

    Update module.php

--- IPS2GPIO_MCP3424/module.php
+++ IPS2GPIO_MCP3424/module.php
@@ -195,10 +195,10 @@
 			// Messwerterfassung setzen
 			for ($i = 0; $i <= 3; $i++) {
 				If ($this->ReadPropertyInteger("Resolution_".$i) <= 2) { 
-					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Count" => 3)));
+					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Count" => 4)));
 				}
 				elseif ($this->ReadPropertyInteger("Resolution_".$i) == 3) {
-					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Count" => 4)));
+					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Count" => 5)));
 				}
 			}
 		}
