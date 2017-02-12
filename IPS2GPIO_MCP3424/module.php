738904e9162ced0f368caff2a996b33ceffd6731
Joey-1970 <paeper@horburg.de>
2017-02-12 19:32:26 +0100

    Update module.php

--- IPS2GPIO_MCP3424/module.php
+++ IPS2GPIO_MCP3424/module.php
@@ -205,9 +205,10 @@
 								2) flips the bits (ie: 0011 becomes 1100)
 								3) add 1 (ie: 1100  + 0001 = 1101)
 								*/
-								$Value = (($MeasurementData[1] & 15) << 8) | $MeasurementData[2];
-								$Value = $Value - 1;
-								$Value = ~$Value;
+								//$Value = (($MeasurementData[1] & 15) << 8) | $MeasurementData[2];
+								$Value = (((~$MeasurementData[1] - 1) & 15) << 8) | (~$MeasurementData[2] - 1);
+								//$Value = $Value - 1;
+								//$Value = ~$Value;
 								 
 								$Value = $Value * 0.001;
 							}
