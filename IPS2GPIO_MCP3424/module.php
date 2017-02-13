fe03fa6c6b2571360dd26b5bb905b9fe9a014c37
Joey-1970 <paeper@horburg.de>
2017-02-13 18:57:34 +0100

    Update module.php

--- IPS2GPIO_MCP3424/module.php
+++ IPS2GPIO_MCP3424/module.php
@@ -267,7 +267,7 @@
 				If ($this->ReadPropertyBoolean("Active_".$i) == true) {
 					$Configuration = ($i << 5) | (1 << 4) | ($this->ReadPropertyInteger("Resolution_".$i) << 2) | $this->ReadPropertyInteger("Amplifier_".$i);
 					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte_onhandle", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Value" => $Configuration)));
-					IPS_Sleep(400);
+					IPS_Sleep(320);
 
 					If ($this->ReadPropertyInteger("Resolution_".$i) <= 2) { 
 						$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_read_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Count" => 3)));
