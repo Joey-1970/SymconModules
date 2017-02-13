d3734116495a073f8420bf8d6b92657f27a9f966
Joey-1970 <paeper@horburg.de>
2017-02-13 13:52:58 +0100

    Update module.php

--- IPS2GPIO_MCP3424/module.php
+++ IPS2GPIO_MCP3424/module.php
@@ -224,40 +224,43 @@
 						case 1:
 							//IPS_LogMessage("IPS2GPIO MCP", "Auflösung 14 Bit");
 							$SignBit = ($MeasurementData[1] & 32) >> 5;
+							$Value = (($MeasurementData[1] & 63) << 8) | $MeasurementData[2];
 							If ($SignBit == 0) {
-								$Value = (($MeasurementData[1] & 31) << 8) | $MeasurementData[2];
+								//$Value = (($MeasurementData[1] & 31) << 8) | $MeasurementData[2];
 								$Value = $Value * 0.00025;
 							}
 							else {
-								$Value = (($MeasurementData[1] & 63) << 8) | $MeasurementData[2];
-								$Value = $Value - 1;
-								$Value = ~$Value * 0.00025;
+								$Value = -(bitnot($Value)) * 0.00025;
+								//$Value = $Value - 1;
+								//$Value = ~$Value * 0.00025;
 							}
 							break;
 						case 2:	
 							//IPS_LogMessage("IPS2GPIO MCP", "Auflösung 16 Bit");
 							$SignBit = ($MeasurementData[1] & 128) >> 7;
+							$Value = (($MeasurementData[1] & 255) << 8) | $MeasurementData[2];
 							If ($SignBit == 0) {
-								$Value = (($MeasurementData[1] & 127) << 8) | $MeasurementData[2];
+								//$Value = (($MeasurementData[1] & 127) << 8) | $MeasurementData[2];
 								$Value = $Value * (6.25 * pow(10,-5));
 							}
 							else {
-								$Value = (($MeasurementData[1] & 255) << 8) | $MeasurementData[2];
-								$Value = $Value - 1;
-								$Value = ~$Value * (6.25 * pow(10,-5));
+								$Value = -(bitnot($Value)) * (6.25 * pow(10,-5));
+								//$Value = $Value - 1;
+								//$Value = ~$Value * (6.25 * pow(10,-5));
 							}
 							break;
 						case 3:
 							//IPS_LogMessage("IPS2GPIO MCP", "Auflösung 18 Bit");
 							$SignBit = ($MeasurementData[1] & 2) >> 1;
+							$Value = (($MeasurementData[1] & 2) << 16) | ($MeasurementData[2] << 8) | $MeasurementData[3];
 							If ($SignBit == 0) {
-								$Value = (($MeasurementData[1] & 1) << 16) | ($MeasurementData[2] << 8) | $MeasurementData[3];  
+								//$Value = (($MeasurementData[1] & 1) << 16) | ($MeasurementData[2] << 8) | $MeasurementData[3];  
 								$Value = $Value * (1.5625 * pow(10,-5));
 							}
 							else {
-								$Value = (($MeasurementData[1] & 2) << 16) | ($MeasurementData[2] << 8) | $MeasurementData[3];
-								$Value = $Value - 1;
-								$Value = ~$Value * (1.5625 * pow(10,-5));
+								$Value = -(bitnot($Value)) * (1.5625 * pow(10,-5));
+								//$Value = $Value - 1;
+								//$Value = ~$Value * (1.5625 * pow(10,-5));
 							}
 							break;	
 					}	
