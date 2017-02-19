48f59b69fe6da40cc5e4d12d7680ac521e9134c2
Joey-1970 <paeper@horburg.de>
2017-02-19 15:29:43 +0100

    Update module.php

--- IPS2GPIO_PCF8591/module.php
+++ IPS2GPIO_PCF8591/module.php
@@ -92,8 +92,6 @@
   		switch($Ident) {
 	        case "Output":
 	            $this->Set_Output($Value);
-	            //Neuen Wert in die Statusvariable schreiben
-	            SetValue($this->GetIDForIdent($Ident), $Value);
 	            break;
 	        default:
 	            throw new Exception("Invalid Ident");
@@ -176,6 +174,8 @@
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			$Value = min(255, max(0, $Value));
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("40"), "Value" => $Value)));
+			//Neuen Wert in die Statusvariable schreiben
+	            	SetValueInteger($this->GetIDForIdent("Output"), $Value);
 		}
 	}
 }
