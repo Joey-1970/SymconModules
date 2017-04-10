0619a6bc709f0b7dd792c4296b44d2e6f3f5321b
Joey-1970 <paeper@horburg.de>
2017-04-10 21:27:36 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -31,7 +31,7 @@ class IPS2GPIO_IO extends IPSModule
 		
 		// Nachrichten abonnieren
 		// Kernel
-	        $this->RegisterMessage($this->InstanceID, 10100); // Alle Kernelmessages (10103 muss im MessageSink ausgewertet werden.)
+	        $this->RegisterMessage(0, 10100); // Alle Kernelmessages (10103 muss im MessageSink ausgewertet werden.)
 		
 		If (IPS_GetKernelRunlevel() == 10103) {
 			$this->RegisterVariableInteger("Handle", "Handle", "", 100);
