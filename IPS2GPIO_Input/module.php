4cbd927fc4a2fa4960ff8aac4a80ec3a9bc3b10c
Joey-1970 <paeper@horburg.de>
2017-08-10 12:33:29 +0200

    Update module.php

--- IPS2GPIO_Input/module.php
+++ IPS2GPIO_Input/module.php
@@ -16,7 +16,52 @@
             	$this->RegisterPropertyInteger("ToggleScript", 0);
  	    	$this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
         }
+	
+	public function GetConfigurationForm() 
+	{ 
+		$arrayStatus = array(); 
+		$arrayStatus[] = array("code" => 101, "icon" => "inactive", "caption" => "Instanz wird erstellt"); 
+		$arrayStatus[] = array("code" => 102, "icon" => "active", "caption" => "Instanz ist aktiv");
+		$arrayStatus[] = array("code" => 104, "icon" => "inactive", "caption" => "Instanz ist inaktiv");
+		$arrayStatus[] = array("code" => 200, "icon" => "error", "caption" => "Pin wird doppelt genutzt!");
+		$arrayStatus[] = array("code" => 201, "icon" => "error", "caption" => "Pin ist an diesem Raspberry Pi Modell nicht vorhanden!"); 
+		
+		$arrayElements = array(); 
+		$arrayElements[] = array("type" => "CheckBox", "name" => "Open", "caption" => "Aktiv");
+ 		$arrayElements[] = array("type" => "Label", "label" => "Angabe der GPIO-Nummer (Broadcom-Number)"); 
+  		
+		$arrayOptions = array();
+		$arrayOptions[] = array("label" => "ungesetzt", "value" => -1);
+		for ($i = 0; $i <= 27; $i++) {
+			$arrayOptions[] = array("label" => $i, "value" => $i);
+		}
+		$arrayElements[] = array("type" => "Select", "name" => "Pin", "caption" => "GPIO-Nr.", "options" => $arrayOptions );
+		$arrayElements[] = array("type" => "Label", "label" => "Der Trigger soll reagieren auf (aktiviert => True):");
+		$arrayElements[] = array("type" => "CheckBox", "name" => "ActionValue", "caption" => "Aktions Wert");
+		$arrayElements[] = array("type" => "Label", "label" => "Zur Software-Entprellung angeschlossener Taster/Schalter (0-300000ms)");
+		$arrayElements[] = array("type" => "NumberSpinner", "name" => "GlitchFilter", "caption" => "Glitchfilter (ms)");	
+		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
+		$arrayElements[] = array("type" => "Label", "label" => "Setzen der internen Pull Up/Down Widerstände"); 
+		$arrayOptions = array();
+		$arrayOptions[] = array("label" => "Kein", "value" => 0);
+		$arrayOptions[] = array("label" => "Pull-Down", "value" => 1);
+		$arrayOptions[] = array("label" => "Pull-Up", "value" => 2);		
+		$arrayElements[] = array("type" => "Select", "name" => "PUL", "caption" => "Widerstand setzen", "options" => $arrayOptions );
+		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
+		$arrayElements[] = array("type" => "Label", "label" => "Skriptausführung als Reaktion auf:"); 
+		$arrayElements[] = array("type" => "SelectScript", "name" => "TriggerScript", "caption" => "Triggerimpuls");
+		$arrayElements[] = array("type" => "SelectScript", "name" => "ToggleScript", "caption" => "Toggle Status"); 
 
+		$arrayActions = array();
+		If (($this->ReadPropertyInteger("Pin") >= 0) AND ($this->ReadPropertyBoolean("Open") == true)) {
+					}
+		else {
+			$arrayActions[] = array("type" => "Label", "label" => "Diese Funktionen stehen erst nach Eingabe und Übernahme der erforderlichen Daten zur Verfügung!");
+		}
+		
+ 		return JSON_encode(array("status" => $arrayStatus, "elements" => $arrayElements, "actions" => $arrayActions)); 		 
+ 	}       
+	    
        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() 
         {
