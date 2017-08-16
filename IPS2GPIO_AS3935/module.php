04bf3002675441a3f2b3f2b487db4b864eba33a1
Joey-1970 <paeper@horburg.de>
2017-08-16 20:59:41 +0200

    Update module.php

--- IPS2GPIO_AS3935/module.php
+++ IPS2GPIO_AS3935/module.php
@@ -148,11 +148,11 @@
 		
 		//Status-Variablen anlegen
 		$this->RegisterVariableInteger("Distance", "Entfernung", "IPS2GPIO.km", 10);
-           	$this->EnableAction("Distance");
+           	$this->DisableAction("Distance");
 		IPS_SetHidden($this->GetIDForIdent("Distance"), false);
 		
 		$this->RegisterVariableInteger("Energy", "Energie", "", 20);
-           	$this->EnableAction("Energy");
+           	$this->DisableAction("Energy");
 		IPS_SetHidden($this->GetIDForIdent("Energy"), false);
 		
 		$this->RegisterVariableInteger("LastInterrupt", "Letzte Meldung", "~UnixTimestamp", 30);
@@ -209,7 +209,6 @@
 			
 			case "get_used_i2c":
 			   	If ($this->ReadPropertyBoolean("Open") == true) {
-					//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
 					$this->ApplyChanges();
 				}
 				break;
@@ -236,15 +235,10 @@
 			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_AS3935_read", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Count" => 9 )));
 			$Data = array();
 			$Data = unserialize($Result);
-			$this->SendDebug("Daten", $Result, 0);
-
+			/*
 			$PowerDown = $Data[1] & 1;
 			$this->SendDebug("PowerDown", $PowerDown, 0);
-
-			/*
-			Indoor = 18
-			Outdoor = 14
-			*/
+		
 			$AFEGainBoost = $Data[1] & 62;
 			$this->SendDebug("AFEGainBoost", $AFEGainBoost, 0);
 
@@ -252,15 +246,25 @@
 			$this->SendDebug("NoiseFloorLevel", $NoiseFloorLevel, 0);
 			$WatchdogThreshold = $Data[2] & 15;
 			$this->SendDebug("WatchdogThreshold", $WatchdogThreshold, 0);
-			/*
-			0 = 1 Blitze
-			1 = 5
-			2 = 9 
-			3 = 16
-			*/
+			
 			$MinNumLigh = $Data[3] & 48;
 			$this->SendDebug("MinNumLigh", $MinNumLigh, 0);
-
+			
+			$LcoFdiv = $Data[4] & 192;
+			$this->SendDebug("LcoFdiv", $LcoFdiv, 0);
+			$MaskDisturber = $Data[4] & 32;
+			$this->SendDebug("MaskDisturber", $MaskDisturber, 0);
+			
+			$LCO = $Data[9] & 128;
+			$this->SendDebug("LCO", $LCO, 0);
+			$SRCO = $Data[9] & 64;
+			$this->SendDebug("SRCO", $SRCO, 0);
+			$TRCO = $Data[9] & 32;
+			$this->SendDebug("TRCO", $TRCO, 0);
+			$Capacitor = $Data[9] & 15;
+			$this->SendDebug("Capacitor", $Capacitor, 0);
+			*/
+			
 			/*
 			1 Noise Level to high
 			4 Disturber Detected
@@ -281,32 +285,14 @@
 				SetValueInteger($this->GetIDForIdent("Interrupt"), 3);
 			}
 			
-
-			$LcoFdiv = $Data[4] & 192;
-			$this->SendDebug("LcoFdiv", $LcoFdiv, 0);
-			$MaskDisturber = $Data[4] & 32;
-			$this->SendDebug("MaskDisturber", $MaskDisturber, 0);
-
 			$Energy = (($Data[7] & 31) << 16) | ($Data[6] << 8) | $Data[5] ;
 			SetValueInteger($this->GetIDForIdent("Energy"), $Energy);
 			$this->SendDebug("Energy", $Energy, 0);
 
 			$Distance = $Data[8] & 63;
 			SetValueInteger($this->GetIDForIdent("Distance"), $Distance);
-			$this->SendDebug("Distance", $Distance, 0);
-
-
-			$LCO = $Data[9] & 128;
-			$this->SendDebug("LCO", $LCO, 0);
-			$SRCO = $Data[9] & 64;
-			$this->SendDebug("SRCO", $SRCO, 0);
-			$TRCO = $Data[9] & 32;
-			$this->SendDebug("TRCO", $TRCO, 0);
-			$Capacitor = $Data[9] & 15;
-			$this->SendDebug("Capacitor", $Capacitor, 0);
+			$this->SendDebug("Distance", $Distance, 0);			
 		}
-		
-		
 	}
 	    
 	private function Setup()
