1a417032ed05a39145a7aa151a029c3a948eb20f
Joey-1970 <paeper@horburg.de>
2017-08-26 14:11:48 +0200

    Update module.php

--- IPS2GPIO_MCP3424/module.php
+++ IPS2GPIO_MCP3424/module.php
@@ -53,16 +53,11 @@
 		
 		$arrayElements[] = array("type" => "Label", "label" => "I²C-Bus (Default ist 1)");
 		$arrayOptions = array();
-		$arrayOptions[] = array("label" => "I²C-Bus 0", "value" => 0);
-		$arrayOptions[] = array("label" => "I²C-Bus 1", "value" => 1);
-		$arrayOptions[] = array("label" => "MUX I²C-Bus 0", "value" => 3);
-		$arrayOptions[] = array("label" => "MUX I²C-Bus 1", "value" => 4);
-		$arrayOptions[] = array("label" => "MUX I²C-Bus 2", "value" => 5);
-		$arrayOptions[] = array("label" => "MUX I²C-Bus 3", "value" => 6);
-		$arrayOptions[] = array("label" => "MUX I²C-Bus 4", "value" => 7);
-		$arrayOptions[] = array("label" => "MUX I²C-Bus 5", "value" => 8);
-		$arrayOptions[] = array("label" => "MUX I²C-Bus 6", "value" => 9);
-		$arrayOptions[] = array("label" => "MUX I²C-Bus 7", "value" => 10);
+		$DevicePorts = array();
+		$DevicePorts = unserialize($this->Get_I2C_Ports());
+		foreach($DevicePorts AS $Value => $Label) {
+			$arrayOptions[] = array("label" => $Label, "value" => $Value);
+		}
 		$arrayElements[] = array("type" => "Select", "name" => "DeviceBus", "caption" => "Device Bus", "options" => $arrayOptions );
 		
 		$arrayElements[] = array("type" => "Label", "label" => "Wiederholungszyklus in Sekunden (0 -> aus, 15 sek -> Minimum)"); 
@@ -387,7 +382,24 @@
 	        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
 	        IPS_SetVariableProfileDigits($Name, $Digits);
 	}
-	    
+	
+	private function Get_I2C_Ports()
+	{
+		If ($this->HasActiveParent() == true) {
+			$I2C_Ports = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_get_ports")));
+		}
+		else {
+			$DevicePorts = array();
+			$DevicePorts[0] = "I²C-Bus 0";
+			$DevicePorts[1] = "I²C-Bus 1";
+			for ($i = 3; $i <= 10; $i++) {
+				$DevicePorts[$i] = "MUX I²C-Bus ".($i -3);
+			}
+			$I2C_Ports = serialize($DevicePorts);
+		}
+	return $I2C_Ports;
+	}    
+	
 	private function HasActiveParent()
     	{
 		$Instance = @IPS_GetInstance($this->InstanceID);
