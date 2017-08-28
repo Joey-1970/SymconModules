f08ed92082977d6616cd97c066609db3c323674d
Joey-1970 <paeper@horburg.de>
2017-08-28 20:41:24 +0200

    Update module.php

--- IPS2GPIO_AS3935/module.php
+++ IPS2GPIO_AS3935/module.php
@@ -56,17 +56,12 @@
 		$GPIO = array();
 		$GPIO = unserialize($this->Get_GPIO());
 		If ($this->ReadPropertyInteger("Pin") >= 0 ) {
-			$GPIO[$this->ReadPropertyInteger("Pin")] = "GPIO".(sprintf("%'.02d", $this->ReadPropertyInteger("Pin")));
+			$GPIO[] = "GPIO".(sprintf("%'.02d", $this->ReadPropertyInteger("Pin")));
 		}
 		foreach($GPIO AS $Value => $Label) {
 			$arrayOptions[] = array("label" => $Label, "value" => $Value);
 		}
-		/*
-		$arrayOptions[] = array("label" => "ungesetzt", "value" => -1);
-		for ($i = 0; $i <= 27; $i++) {
-			$arrayOptions[] = array("label" => $i, "value" => $i);
-		}
-		*/
+		
 		$arrayElements[] = array("type" => "Select", "name" => "Pin", "caption" => "GPIO-Nr.", "options" => $arrayOptions );
 		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________"); 
 		
