71147ec0291c63c4a1f11b53a2fe6dc0f11001ec
Joey-1970 <paeper@horburg.de>
2017-09-06 20:51:31 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -230,7 +230,7 @@ class IPS2GPIO_IO extends IPSModule
 
 				
 				//Skripte senden
-				$Result = $this->SendProc("tag 999 wait p0 mils p1 evt p2 jmp 999");
+				$Result = $this->SendProc("tag 100 wait p0 mils p1 evt p2 jmp 100");
 				If ($Result >= 0) {
 					$this->SetBuffer("SerialScriptID", $Result);
 					
