457c58ef7db61217bc1b320bb79cec90be711e3c
Joey-1970 <paeper@horburg.de>
2017-09-08 14:24:17 +0200

    Update module.php

--- IPS2GPIO_1Wire/module.php
+++ IPS2GPIO_1Wire/module.php
@@ -56,8 +56,9 @@
 		$this->SetReceiveDataFilter($Filter);
 		
 		If ((IPS_GetKernelRunlevel() == 10103) AND ($this->HasActiveParent() == true)) {
-			If ($this->ReadPropertyBoolean("Open") == true) {
-				$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => 4, "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
+			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", "Pin" => 4, "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
+			
+			If (($Result == true) AND ($this->ReadPropertyBoolean("Open") == true)) {
 				$this->Setup();
 				$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
 				// Erste Messung durchführen
