07e7489d5253b34cf5875d52ce65ab31447d2fae
Joey-1970 <paeper@horburg.de>
2017-09-08 14:22:17 +0200

    Update module.php

--- IPS2GPIO_Display/module.php
+++ IPS2GPIO_Display/module.php
@@ -163,9 +163,9 @@
  
         	If (IPS_GetKernelRunlevel() == 10103) {
 			// den Handle für dieses Gerät ermitteln
-			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_serial", "Baud" => 9600, "Device" => $this->ReadPropertyString('ConnectionString'), "InstanceID" => $this->InstanceID )));
-			
-			If ($this->ReadPropertyBoolean("Open") == true) {
+			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_serial", "Baud" => 9600, "Device" => $this->ReadPropertyString('ConnectionString'), "InstanceID" => $this->InstanceID )));
+	
+			If (($Result == true) AND ($this->ReadPropertyBoolean("Open") == true)) {
 				$this->Setup();
 				$this->SetStatus(102);
 			}
