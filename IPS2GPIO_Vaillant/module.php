000237d317779adb45a4bd8d50634a21e330a745
Joey-1970 <paeper@horburg.de>
2017-10-23 11:56:03 +0200

    Create module.php

---
+++ IPS2GPIO_Vaillant/module.php
@@ -0,0 +1,244 @@
+<?
+    // Klassendefinition
+    class IPS2GPIO_Vaillant extends IPSModule 
+    {
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() 
+        {
+		 // Diese Zeile nicht löschen.
+		 parent::Create();
+		 $this->RegisterPropertyBoolean("Open", false);
+		 $this->RegisterPropertyInteger("Pin", -1);
+		 $this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
+        }
+	
+	
+	public function GetConfigurationForm() 
+	{ 
+		$arrayStatus = array(); 
+		$arrayStatus[] = array("code" => 101, "icon" => "inactive", "caption" => "Instanz wird erstellt"); 
+		$arrayStatus[] = array("code" => 102, "icon" => "active", "caption" => "Instanz ist aktiv");
+		$arrayStatus[] = array("code" => 104, "icon" => "inactive", "caption" => "Instanz ist inaktiv");
+		$arrayStatus[] = array("code" => 200, "icon" => "error", "caption" => "Pin wird doppelt genutzt!");
+		$arrayStatus[] = array("code" => 201, "icon" => "error", "caption" => "Pin ist an diesem Raspberry Pi Modell nicht vorhanden!"); 
+		
+		$arrayElements = array(); 
+		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv"); 
+ 		$arrayElements[] = array("type" => "Label", "label" => "Angabe der GPIO-Nummer (Broadcom-Number)"); 
+  		
+		$arrayOptions = array();
+		$GPIO = array();
+		$GPIO = unserialize($this->Get_GPIO());
+		If ($this->ReadPropertyInteger("Pin") >= 0 ) {
+			$GPIO[$this->ReadPropertyInteger("Pin")] = "GPIO".(sprintf("%'.02d", $this->ReadPropertyInteger("Pin")));
+		}
+		ksort($GPIO);
+		foreach($GPIO AS $Value => $Label) {
+			$arrayOptions[] = array("label" => $Label, "value" => $Value);
+		}
+		
+		$arrayElements[] = array("type" => "Select", "name" => "Pin", "caption" => "GPIO-Nr.", "options" => $arrayOptions );
+		
+		$arrayActions = array();
+		If (($this->ReadPropertyInteger("Pin") >= 0) AND ($this->ReadPropertyBoolean("Open") == true)) {
+			$arrayActions[] = array("type" => "Button", "label" => "On", "onClick" => 'I2GDMR_Set_Status($id, true);');
+			$arrayActions[] = array("type" => "Button", "label" => "Off", "onClick" => 'I2GDMR_Set_Status($id, false);');
+			$arrayActions[] = array("type" => "Button", "label" => "Toggle", "onClick" => 'I2GDMR_Toggle_Status($id);');
+			$arrayActions[] = array("type" => "Label", "label" => "Dimmen");
+			$arrayActions[] = array("type" => "HorizontalSlider", "name" => "Slider", "minimum" => 0,  "maximum" => 255, "onChange" => 'I2GDMR_Set_Intensity($id, $Slider);');
+		}
+		else {
+			$arrayActions[] = array("type" => "Label", "label" => "Diese Funktionen stehen erst nach Eingabe und Übernahme der erforderlichen Daten zur Verfügung!");
+		}
+		
+ 		return JSON_encode(array("status" => $arrayStatus, "elements" => $arrayElements, "actions" => $arrayActions)); 		 
+ 	}    
+	
+	    
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() 
+        {
+	        // Diese Zeile nicht löschen
+	        parent::ApplyChanges();
+		//Status-Variablen anlegen
+		$this->RegisterVariableBoolean("Status", "Status", "~Switch", 10);
+	        $this->EnableAction("Status");
+	        $this->RegisterVariableInteger("Intensity", "Intensity", "~Intensity.255", 20);
+	        $this->EnableAction("Intensity");
+           
+           	//ReceiveData-Filter setzen
+		$Filter = '(.*"Function":"get_usedpin".*|.*"Pin":'.$this->ReadPropertyInteger("Pin").'.*)';
+		$this->SetReceiveDataFilter($Filter);
+		
+		If ((IPS_GetKernelRunlevel() == 10103) AND ($this->HasActiveParent() == true)) {
+			If (($this->ReadPropertyInteger("Pin") >= 0) AND ($this->ReadPropertyBoolean("Open") == true)) {
+				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_usedpin", 
+									  "Pin" => $this->ReadPropertyInteger("Pin"), "InstanceID" => $this->InstanceID, "Modus" => 1, "Notify" => false)));
+				If ($Result == true) {
+					$this->SetStatus(102);
+				}
+			}
+			else {
+				$this->SetStatus(104);
+			}
+		}
+ 	}
+	
+	public function RequestAction($Ident, $Value) 
+	{
+  		switch($Ident) {
+	        case "Status":
+	            $this->Set_Status($Value);
+	            break;
+	        case "Intensity":
+	            $this->Set_Intensity($Value);
+	            break;
+	        default:
+	            throw new Exception("Invalid Ident");
+	    }
+	}
+	
+	public function ReceiveData($JSONString) 
+	{
+		// Empfangene Daten vom Gateway/Splitter
+		$data = json_decode($JSONString);
+		switch ($data->Function) {
+			case "get_usedpin":
+				If ($this->ReadPropertyBoolean("Open") == true) {
+					$this->ApplyChanges();
+				}
+				break;
+			case "status":
+				If ($data->Pin == $this->ReadPropertyInteger("Pin")) {
+					$this->SetStatus($data->Status);
+				}
+				break;
+			case "freepin":
+				   // Funktion zum erstellen dynamischer Pulldown-Menüs
+				   break;
+		}
+ 	}
+	// Beginn der Funktionen
+	// Dimmt den gewaehlten Pin
+	public function Set_Intensity(Int $value)
+	{
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$this->SendDebug("Set_Intensity", "Ausfuehrung", 0);
+			$value = min(255, max(0, $value));
+			If (GetValueBoolean($this->GetIDForIdent("Status")) == true) {
+				$Result = $this->SendDataToParent(json_encode(Array("DataID"=>"{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin"), "Value" => $value)));
+				If (!$Result) {
+					$this->SendDebug("Set_Intensity", "Fehler beim Schreiben des Wertes!", 0);
+					return;
+				}
+				else {
+					SetValueInteger($this->GetIDForIdent("Intensity"), $value);
+				}
+			}
+			else {
+				SetValueInteger($this->GetIDForIdent("Intensity"), $value);
+			}
+		}
+	}
+	
+	public function Fade_Intensity(Int $Value, Int $Fadetime)
+	{
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$this->SendDebug("Fade_Intensity", "Ausfuehrung", 0);
+			$Value = min(255, max(0, $Value));
+			$ActualValue = GetValueInteger($this->GetIDForIdent("Intensity"));
+			$TargetValue = $Value;
+			
+			If (GetValueBoolean($this->GetIDForIdent("Status")) == true) {
+				$this->SendDataToParent(json_encode(Array("DataID"=>"{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin"), "Value" => $Value)));
+			}
+			else {
+				SetValueInteger($this->GetIDForIdent("Intensity"), $Value);
+			}
+		}
+	}
+	    
+	// Schaltet den gewaehlten Pin
+	public function Set_Status(Bool $value)
+	{
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$this->SendDebug("Set_Status", "Ausfuehrung", 0);
+			If ($value == true) {
+				$Result = $this->SendDataToParent(json_encode(Array("DataID"=>"{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin"), "Value" => GetValueInteger($this->GetIDForIdent("Intensity")))));
+				If (!$Result) {
+					$this->SendDebug("Set_Status", "Fehler beim Schreiben des Wertes!", 0);
+					return; 
+				}
+				else {
+					$this->Get_Status();
+					SetValueBoolean($this->GetIDForIdent("Status"), true);
+				}
+			}
+			else {
+				$Result = $this->SendDataToParent(json_encode(Array("DataID"=>"{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin"), "Value" => 0)));
+				If (!$Result) {
+					$this->SendDebug("Set_Status", "Fehler beim Schreiben des Wertes!", 0);
+					return;
+				}
+				else {
+					SetValueBoolean($this->GetIDForIdent("Status"), false);
+				}
+			}
+		}
+	}
+	
+	public function Get_Status()
+	{
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$this->SendDebug("Get_Status", "Ausfuehrung", 0);
+			
+			If (GetValueBoolean($this->GetIDForIdent("Status")) == true) {
+				$Result = $this->SendDataToParent(json_encode(Array("DataID"=>"{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_PWM_dutycycle", "Pin" => $this->ReadPropertyInteger("Pin") )));
+				If ($Result < 0) {
+					$this->SendDebug("Get_Status", "Fehler beim Lesen des Wertes!", 0);
+					return;
+				}
+				else {
+					SetValueInteger($this->GetIDForIdent("Intensity"), $Result);
+				}
+			}
+		}
+	}
+	// Toggelt den Status
+	public function Toggle_Status()
+	{
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$this->SendDebug("Toggle_Status", "Ausfuehrung", 0);
+			$this->Set_Status(!GetValueBoolean($this->GetIDForIdent("Status")));
+		}
+	}
+	
+	private function Get_GPIO()
+	{
+		If ($this->HasActiveParent() == true) {
+			$GPIO = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_GPIO")));
+		}
+		else {
+			$AllGPIO = array();
+			$AllGPIO[-1] = "undefiniert";
+			for ($i = 2; $i <= 27; $i++) {
+				$AllGPIO[$i] = "GPIO".(sprintf("%'.02d", $i));
+			}
+			$GPIO = serialize($AllGPIO);
+		}
+	return $GPIO;
+	}    
+	    
+	private function HasActiveParent()
+    	{
+		$Instance = @IPS_GetInstance($this->InstanceID);
+		if ($Instance['ConnectionID'] > 0)
+		{
+			$Parent = IPS_GetInstance($Instance['ConnectionID']);
+			if ($Parent['InstanceStatus'] == 102)
+			return true;
+		}
+        return false;
+    	}  
+    }
+?>
