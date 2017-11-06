782885d64c7097a73462129004bcab9c4d6250d7
Joey-1970 <paeper@horburg.de>
2017-11-06 10:40:12 +0100

    Update module.php

--- IPS2GPIO_BME680/module.php
+++ IPS2GPIO_BME680/module.php
@@ -225,8 +225,6 @@
 					$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
 					// Parameterdaten zum Baustein senden
 					$this->Setup();
-					// Kalibrierungsdaten einlesen
-					$this->ReadCalibrateData();
 					// Erste Messdaten einlesen
 					$this->Measurement();
 					$this->SetStatus(102);
@@ -273,167 +271,13 @@
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			$this->SendDebug("Measurement", "Ausfuehrung", 0);
 			// Messwerte aktualisieren
-			$CalibrateData = array();
-			$CalibrateData = unserialize($this->GetBuffer("CalibrateData"));
-			$this->SendDebug("Measurement", "CalibrateData: ".count($CalibrateData), 0);
-			If (count($CalibrateData) == 32)  {
-				$this->ReadData();
-				// Kalibrierungsdatan aufbereiten
-				$Dig_T[0] = (($CalibrateData[137] << 8) | $CalibrateData[136]);
-				$Dig_T[1] = (($CalibrateData[139] << 8) | $CalibrateData[138]);
-				$Dig_T[2] = (($CalibrateData[141] << 8) | $CalibrateData[140]);
-				$Dig_P[0] = (($CalibrateData[143] << 8) | $CalibrateData[142]);
-				$Dig_P[1] = (($CalibrateData[145] << 8) | $CalibrateData[144]);
-				$Dig_P[2] = (($CalibrateData[147] << 8) | $CalibrateData[146]);
-				$Dig_P[3] = (($CalibrateData[149] << 8) | $CalibrateData[148]);
-				$Dig_P[4] = (($CalibrateData[151] << 8) | $CalibrateData[150]);
-				$Dig_P[5] = (($CalibrateData[153] << 8) | $CalibrateData[152]);
-				$Dig_P[6] = (($CalibrateData[155] << 8) | $CalibrateData[154]);
-				$Dig_P[7] = (($CalibrateData[157] << 8) | $CalibrateData[156]);
-				$Dig_P[8] = (($CalibrateData[159] << 8) | $CalibrateData[158]);
-				$Dig_H[0] = $CalibrateData[161];
-				$Dig_H[1] = (($CalibrateData[226] << 8) | $CalibrateData[225]);
-				$Dig_H[2] = $CalibrateData[227];
-				$Dig_H[3] = (($CalibrateData[228] << 4) | (hexdec("0F") & $CalibrateData[229]));
-				$Dig_H[4] = (($CalibrateData[230] << 4) | (($CalibrateData[229] >> 4) & hexdec("0F")));
-				$Dig_H[5] = $CalibrateData[231];
-				for ($i = 1; $i <= 2; $i++) {
-					If ($Dig_T[$i] & hexdec("8000")) {
-						$Dig_T[$i] = (-$Dig_T[$i] ^ hexdec("FFFF")) + 1;
-					}
-				}
+			$this->ReadData();
+			$MeasurementData = array();
+			$MeasurementData = unserialize($this->GetBuffer("MeasurementData"));
+			$this->SendDebug("Measurement", "MeasurementData: ".count($MeasurementData), 0);
+			$this->SendDebug("Measurement", "MeasurementData: ".$this->GetBuffer("MeasurementData"), 0);
 			
-				for ($i = 1; $i <= 8; $i++) {
-					If ($Dig_P[$i] & hexdec("8000")) {
-						$Dig_P[$i] = (-$Dig_P[$i] ^ hexdec("FFFF")) + 1;
-					}
-				}
-				for ($i = 0; $i <= 5; $i++) {
-					If ($Dig_H[$i] & hexdec("8000")) {
-						$Dig_H[$i] = (-$Dig_H[$i] ^ hexdec("FFFF")) + 1;
-					}
-				}
-				// Messwerte aufbereiten
-				$MeasurementData = array();
-				$MeasurementData = unserialize($this->GetBuffer("MeasurementData"));
-				$this->SendDebug("Measurement", "MeasurementData: ".count($MeasurementData), 0);
-				If (count($MeasurementData) == 8) {
-					$Pres_raw = (($MeasurementData[1] << 12) | ($MeasurementData[2] << 4) | ($MeasurementData[3] >> 4));
-					$Temp_raw = (($MeasurementData[4] << 12) | ($MeasurementData[5] << 4) | ($MeasurementData[6] >> 4));
-					$Hum_raw =  (($MeasurementData[7] << 8) | $MeasurementData[8]);
-					$FineCalibrate = 0;
-					// Temperatur
-					$V1 = ($Temp_raw / 16384 - $Dig_T[0] / 1024) * $Dig_T[1];
-					$V2 = ($Temp_raw / 131072 - $Dig_T[0] / 8192) * ($Temp_raw / 131072 - $Dig_T[0] / 8192) * $Dig_T[2];
-					$FineCalibrate = $V1 + $V2;
-					$Temp = $FineCalibrate / 5120;
-					SetValueFloat($this->GetIDForIdent("Temperature"), round($Temp, 2));
-				
-					// Luftdruck
-					$Pressure = 0;
-					$V1 = ($FineCalibrate / 2) - 64000;
-					$V2 = ((($V1 / 4) * ($V1 / 4)) / 2048) * $Dig_P[5];
-					$V2 = $V2 + (($V1 * $Dig_P[4]) * 2);
-					$V2 = ($V2 / 4) + ($Dig_P[3] * 65536);
-					$V1 = ((($Dig_P[2] * ((($V1 / 4) * ($V1 / 4)) / 8192)) / 8) + (($Dig_P[1] * $V1) / 2)) / 262144;
-					$V1 = ((32768 + $V1) * $Dig_P[0]) / 32768;
-					If ($V1 == 0) {
-						SetValueFloat($this->GetIDForIdent("Pressure"), "0");
-					}
-					$Pressure = ((1048576 - $Pres_raw) - ($V2 / 4096)) * 3125;
-					If ($Pressure < hexdec("80000000")) {
-						$Pressure = ($Pressure * 2) / $V1;
-					}
-					else {
-						$Pressure = ($Pressure / $V1) * 2;
-					}
-					$V1 = ($Dig_P[8] * ((($Pressure / 8) * ($Pressure / 8)) / 8192)) / 4096;
-					$V2 = (($Pressure / 4) * $Dig_P[7]) / 8192;
-					$Pressure = $Pressure + (($V1 + $V2 + $Dig_P[6]) / 16);
-					SetValueFloat($this->GetIDForIdent("Pressure"), round($Pressure / 100, 2));
-				
-					// Luftfeuchtigkeit
-					$Hum = $FineCalibrate - 76800;
-					If ($Hum <> 0) {
-						$Hum = ($Hum_raw - ($Dig_H[3] * 64 + $Dig_H[4] / 16384 * $Hum)) * ($Dig_H[1]  / 65536 * (1 + $Dig_H[5] / 67108864 * $Hum * (1 + $Dig_H[2] / 67108864 * $Hum)));
-					}
-					else {
-						SetValueFloat($this->GetIDForIdent("Humidity"), 0);
-					}
-					$Hum = $Hum * (1 - $Dig_H[0] * $Hum / 524288);
-					If ($Hum > 100) {
-						$Hum = 100;
-					}
-					elseif ($Hum < 0) {
-						$Hum = 0;
-					}
-					SetValueFloat($this->GetIDForIdent("Humidity"), round($Hum, 2));
-					// Berechnung von Taupunkt und absoluter Luftfeuchtigkeit
-					if ($Temp < 0) {
-						$a = 7.6; 
-						$b = 240.7;
-					}  
-					elseif ($Temp >= 0) {
-						$a = 7.5;
-						$b = 237.3;
-					}
-					$sdd = 6.1078 * pow(10.0, (($a * $Temp) / ($b + $Temp)));
-					$dd = $Hum/100.0 * $sdd;
-					$v = log10($dd/6.1078);
-					$td = $b * $v / ($a - $v);
-					$af = pow(10,5) * 18.016 / 8314.3 * $dd / ($Temp + 273.15);
-				
-					// Taupunkttemperatur
-					SetValueFloat($this->GetIDForIdent("DewPointTemperature"), round($td, 2));
-					// Absolute Feuchtigkeit
-					SetValueFloat($this->GetIDForIdent("HumidityAbs"), round($af, 2));
-					
-					// Relativen Luftdruck
-					$Altitude = $this->ReadPropertyInteger("Altitude");
-					If ($this->ReadPropertyInteger("Temperature_ID") > 0) {
-						// Wert der Variablen zur Berechnung nutzen
-						$Temperature = GetValueInteger($this->ReadPropertyInteger("Temperature_ID"));
-					}
-					else {
-						// Wert dieses BME280 verwenden
-						$Temperature = $Temp;
-					}
-					If ($this->ReadPropertyInteger("Humidity_ID") > 0) {
-						// Wert der Variablen zur Berechnung nutzen
-						$Humidity = GetValueInteger($this->ReadPropertyInteger("Humidity_ID"));
-					}
-					else {
-						// Wert dieses BME280 verwenden
-						$Humidity = $Hum;
-					}
-					$g_n = 9.80665; // Erdbeschleunigung (m/s^2)
-					$gam = 0.0065; // Temperaturabnahme in K pro geopotentiellen Metern (K/gpm)
-					$R = 287.06; // Gaskonstante für trockene Luft (R = R_0 / M)
-					$M = 0.0289644; // Molare Masse trockener Luft (J/kgK)
-					$R_0 = 8.314472; // allgemeine Gaskonstante (J/molK)
-					$T_0 = 273.15; // Umrechnung von °C in K
-					$C = 0.11; // DWD-Beiwert für die Berücksichtigung der Luftfeuchte
-					$E_0 = 6.11213; // (hPa)
-					$f_rel = $Humidity / 100; // relative Luftfeuchte (0-1.0)
-					// momentaner Stationsdampfdruck (hPa)
-					$e_d = $f_rel * $E_0 * exp((17.5043 * $Temperature) / (241.2 + $Temperature));
-        				$PressureRel = $Pressure * exp(($g_n * $Altitude) / ($R * ($Temperature + $T_0 + $C * $e_d + (($gam * $Altitude) / 2))));
-					SetValueFloat($this->GetIDForIdent("PressureRel"), round($PressureRel / 100, 2));
-					
-					// Luftdruck Trends
-					If ($this->ReadPropertyBoolean("LoggingPres") == true) {
-						SetValueFloat($this->GetIDForIdent("PressureTrend1h"), $this->PressureTrend(1));
-						SetValueFloat($this->GetIDForIdent("PressureTrend3h"), $this->PressureTrend(3));
-						SetValueFloat($this->GetIDForIdent("PressureTrend12h"), $this->PressureTrend(12));
-						SetValueFloat($this->GetIDForIdent("PressureTrend24h"), $this->PressureTrend(24));
-					}
-				}
-				
-			}
-			else {
-				$this->Setup();
-				$this->ReadCalibrateData();
-			}
+			
 		}
 	}	
 	
@@ -441,6 +285,7 @@
 	{
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			$this->SendDebug("Setup", "Ausfuehrung", 0);
+			/*
 			$osrs_t = $this->ReadPropertyInteger("OSRS_T"); // Oversampling Measure temperature x1, x2, x4, x8, x16 (dec: 0 (off), 1, 2, 3, 4)
 			$osrs_p = $this->ReadPropertyInteger("OSRS_P"); // Oversampling Measure pressure x1, x2, x4, x8, x16 (dec: 0 (off), 1, 2, 3, 4)
 			$osrs_h = $this->ReadPropertyInteger("OSRS_H"); // Oversampling Measure humidity x1, x2, x4, x8, x16 (dec: 0 (off), 1, 2, 3, 4)
@@ -466,54 +311,17 @@
 				$this->SendDebug("Setup", "config_reg setzen fehlerhaft!", 0);
 				return;
 			}
+			*/
 		}
 	}
-	
-	private function ReadCalibrateData()
-	{
-		If ($this->ReadPropertyBoolean("Open") == true) {
-			// Kalibrierungsdaten neu einlesen
-			$this->SendDebug("ReadCalibrateData", "Ausfuehrung", 0);
-			$CalibrateData = array();
-			for ($i = hexdec("88"); $i < (hexdec("88") + 24); $i++) {
-				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME280_read", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $i)));
-				If ($Result < 0) {
-					$this->SendDebug("ReadCalibrateData", "Fehler beim Einlesen der Kalibrierungsdaten bei Byte ".$i, 0);
-					return;
-				}
-				else {
-					$CalibrateData[$i] = $Result;
-				}
-			}
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME280_read", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("A1"))));
-			If ($Result < 0) {
-				$this->SendDebug("ReadCalibrateData", "Fehler beim Einlesen der Kalibrierungsdaten bei Byte 161", 0);
-				return;
-			}
-			else {
-				$CalibrateData[161] = $Result;
-			}
-			for ($i = hexdec("E1"); $i < (hexdec("E1") + 7); $i++) {
-				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME280_read", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $i)));
-				If ($Result < 0) {
-					$this->SendDebug("ReadCalibrateData", "Fehler beim Einlesen der Kalibrierungsdaten bei Byte ".$i, 0);
-					return;
-				}
-				else {
-					$CalibrateData[$i] = $Result;
-				}
-			}
-			$this->SetBuffer("CalibrateData", serialize($CalibrateData));
-		}
-	}
-	
+		
 	private function ReadData()
 	{
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			// Liest die Messdaten ein
 			$this->SendDebug("ReadData", "Ausfuehrung", 0);
 			
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME280_read_block", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("F7"), "Count" => 8)));
+			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME680_read_block", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("1D"), "Count" => 14)));
 			If ($Result < 0) {
 				$MeasurementData = array();
 				$this->SetBuffer("MeasurementData", serialize($MeasurementData));
