815ec3530ec1453d0ac150d441a6ad13f84d5a23
Joey-1970 <paeper@horburg.de>
2017-11-13 22:18:23 +0100

    Update module.php

--- IPS2GPIO_BME680/module.php
+++ IPS2GPIO_BME680/module.php
@@ -295,288 +295,24 @@
 			$this->SendDebug("Measurement", "Ausfuehrung", 0);
 			$this->bme680_set_sensor_settings();
 			$this->bme680_get_sensor_data();
-			
-			return;
-			
-			// Messwerte aktualisieren
-			$CalibrateData = array();
-			If (is_array(unserialize($this->GetBuffer("CalibrateData"))) == false) {
-				$this->SendDebug("Measurement", "Kalibrirungsdaten nicht korrekt!", 0);
-				$this->ReadCalibrateData();
-			}
-			$CalibrateData = unserialize($this->GetBuffer("CalibrateData"));
-			$this->SendDebug("Measurement", "CalibrateData: ".count($CalibrateData), 0);
-			$this->SendDebug("Measurement", "CalibrateData: ".$this->GetBuffer("CalibrateData"), 0);
-			If (count($CalibrateData) == 46)  {
-				$this->ReadData();
-				// Kalibrierungsdatan aufbereiten
-				$par_t1 = (($CalibrateData[34] << 8) | $CalibrateData[33]);
-				$par_t2 = (($CalibrateData[2] << 8) | $CalibrateData[1]);
-				$par_t3 = $CalibrateData[3];
-
-				$par_p1 = (($CalibrateData[6] << 8) | $CalibrateData[5]);
-				$par_p2 = (($CalibrateData[8] << 8) | $CalibrateData[7]);
-				$par_p3 = $CalibrateData[9];
-				$par_p4 = (($CalibrateData[12] << 8) | $CalibrateData[11]);
-				$par_p5 = (($CalibrateData[14] << 8) | $CalibrateData[13]);
-				$par_p6 = $CalibrateData[16];
-				$par_p7 = ($CalibrateData[15]);
-				$par_p8 = (($CalibrateData[20] << 8) | $CalibrateData[19]);
-				$par_p9 = (($CalibrateData[22] << 8) | $CalibrateData[21]);
-				$par_p10 = $CalibrateData[23];
-
-				$par_h1 = (($CalibrateData[27] << 4) | $CalibrateData[26] & hexdec("0F"));
-				$par_h2 = (($CalibrateData[26] << 4) | $CalibrateData[25] >> 4);
-				$par_h3 = $CalibrateData[28];
-				$par_h4 = $CalibrateData[29];
-				$par_h5 = $CalibrateData[30];
-				$par_h6 = $CalibrateData[31];
-				$par_h7 = $CalibrateData[32];
-
-				$par_gh1 = $CalibrateData[37];
-				$par_gh2 = (($CalibrateData[36] << 8) | $CalibrateData[35]);
-				$par_gh3 = $CalibrateData[38];
-				
-				$res_heat_val = $CalibrateData[39];
-				
-				$res_heat_range = ($CalibrateData[41] >> 4) & hexdec("03");
-				
-				$range_switching_error = ($CalibrateData[43] & hexdec("F0")) >> 4;
-
-				// Messwerte aufbereiten
-				$MeasurementData = array();
-				$MeasurementData = unserialize($this->GetBuffer("MeasurementData"));
-				$this->SendDebug("Measurement", "MeasurementData: ".count($MeasurementData), 0);
-				$this->SendDebug("Measurement", "MeasurementData: ".$this->GetBuffer("MeasurementData"), 0);
-				If (count($MeasurementData) == 15) {
-					$status = $MeasurementData[1] & hexdec("80"); // Flag New_Data_0
-					$gas_status = $MeasurementData[1] & hexdec("0F");
-					$maes_index = $MeasurementData[2];
-					
-					$adc_pres = ($MeasurementData[3] * 4096) | ($MeasurementData[4] * 16) | ($MeasurementData[5] / 16);
-					$adc_temp = ($MeasurementData[6] * 4096) | ($MeasurementData[7] * 16) | ($MeasurementData[8] / 16);
-					$adc_hum = ($MeasurementData[9] * 256) | $MeasurementData[10];
-					$adc_gas_res = ($MeasurementData[14] * 4) | ($MeasurementData[15] / 64);
-					$gas_range = $MeasurementData[15] & hexdec("0F");
-					
-					$status = $status | ($MeasurementData[15] & hexdec("20")); // Flag GASM_VALID_R
-					$status = $status | ($MeasurementData[15] & hexdec("10")); // Flag HEAT_STAB_R
-					
-					If (!$status) {
-						$this->SendDebug("Measurement", "New Data Flag: ".($MeasurementData[1] & hexdec("80")), 0);
-						$this->SendDebug("Measurement", "Flag GASM_VALID_R:".($MeasurementData[15] & hexdec("20")), 0);
-						$this->SendDebug("Measurement", "Flag HEAT_STAB_R:".($MeasurementData[15] & hexdec("10")), 0);
-						$this->SendDebug("Measurement", "Keine auswertbaren Daten!", 0);
-						//return;
-					}
-					
-					// Temperatur
-					$var1 = ($adc_temp / 8) - ($par_t1 * 2);
-					$var2 = ($var1 * $par_t2) / 2048;
-					$var3 = (($var1 / 2) * ($var1 / 2)) / 4096;
-					$var3 = (($var3) * ($par_t3 * 16)) / 16384;
-					$t_fine = ($var2 + $var3);
-					$Temp = ((($t_fine * 5) + 128) / 256);
-					SetValueFloat($this->GetIDForIdent("Temperature"), round($Temp, 2));
-					
-					// Luftdruck
-					$var1 = (($t_fine) / 2) - 64000;
-					$var2 = (($var1 / 4) * ($var1 / 4)) / 2048;
-					$var2 = (($var2) * $par_p6) / 4;
-					$var2 = $var2 + (($var1 * $par_p5) * 2);
-					$var2 = ($var2 / 4) + ($par_p4 * 65536);
-					$var1 = (($var1 / 4) * ($var1 / 4)) / 8192;
-					$var1 = ((($var1) * ($par_p3 * 32)) / 8) + (($par_p2 * $var1) / 2);
-					$var1 = $var1 / 262144;
-					$var1 = ((32768 + $var1) * $par_p1) / 32768;
-					$Pressure = (1048576 - $adc_pres);
-					$Pressure = (($Pressure - ($var2 / 4096)) * (3125));
-					$Pressure = (($Pressure / $var1) * 2);
-					$var1 = ($par_p9 * ((($Pressure / 8) * ($Pressure / 8)) / 8192)) / 4096;
-					$var2 = (($Pressure / 4) * $par_p8) / 8192;
-					$var3 = (($Pressure / 256) * ($Pressure / 256) * ($Pressure / 256) * $par_p10) / 131072;
-					$Pressure = ($Pressure) + (($var1 + $var2 + $var3 + ($par_p7 * 128)) / 16);
-					SetValueFloat($this->GetIDForIdent("Pressure"), round($Pressure / 100, 2));
-					
-					// Luftfeuchtigkeit
-					$temp_scaled = (($t_fine * 5) + 128) / 256;
-					$var1 = ($adc_hum - (($par_h1 * 16))) - ((($temp_scaled * $par_h3) / (100)) / 2);
-					$var2 = ($par_h2 * ((($temp_scaled * $par_h4) / (100)) + ((($temp_scaled * (($temp_scaled * $par_h5) / (100))) / 64) / (100)) + (1 * 16384))) / 1024;
-					$var3 = $var1 * $var2;
-					$var4 = $par_h6 * 128;
-					$var4 = (($var4) + (($temp_scaled *$par_h7) / (100))) / 16;
-					$var5 = (($var3 / 16384) * ($var3 / 16384)) / 1024;
-					$var6 = ($var4 * $var5) / 2;
-					$Hum = ((($var3 + $var6) / 1024) * (1000)) / 4096;
-					If ($Hum > 100) {
-						$Hum = 100;
-					}
-					elseif ($Hum < 0) {
-						$Hum = 0;
-					}
-					SetValueFloat($this->GetIDForIdent("Humidity"), round($Hum, 2));
-
-					// Berechnung von Taupunkt und absoluter Luftfeuchtigkeit
-					if ($Temp < 0) {
-						$a = 7.6; 
-						$b = 240.7;
-					}  
-					elseif ($Temp >= 0) {
-						$a = 7.5;
-						$b = 237.3;
-					}
-					$sdd = 6.1078 * pow(10.0, (($a * $Temp) / ($b + $Temp)));
-					$dd = $Hum/100.0 * $sdd;
-					$v = log10($dd/6.1078);
-					$td = $b * $v / ($a - $v);
-					$af = pow(10,5) * 18.016 / 8314.3 * $dd / ($Temp + 273.15);
-				
-					// Taupunkttemperatur
-					SetValueFloat($this->GetIDForIdent("DewPointTemperature"), round($td, 2));
-					// Absolute Feuchtigkeit
-					SetValueFloat($this->GetIDForIdent("HumidityAbs"), round($af, 2));
-					
-					// Relativen Luftdruck
-					$Altitude = $this->ReadPropertyInteger("Altitude");
-					If ($this->ReadPropertyInteger("Temperature_ID") > 0) {
-						// Wert der Variablen zur Berechnung nutzen
-						$Temperature = GetValueInteger($this->ReadPropertyInteger("Temperature_ID"));
-					}
-					else {
-						// Wert dieses BME280 verwenden
-						$Temperature = $Temp;
-					}
-					If ($this->ReadPropertyInteger("Humidity_ID") > 0) {
-						// Wert der Variablen zur Berechnung nutzen
-						$Humidity = GetValueInteger($this->ReadPropertyInteger("Humidity_ID"));
-					}
-					else {
-						// Wert dieses BME280 verwenden
-						$Humidity = $Hum;
-					}
-					$g_n = 9.80665; // Erdbeschleunigung (m/s^2)
-					$gam = 0.0065; // Temperaturabnahme in K pro geopotentiellen Metern (K/gpm)
-					$R = 287.06; // Gaskonstante für trockene Luft (R = R_0 / M)
-					$M = 0.0289644; // Molare Masse trockener Luft (J/kgK)
-					$R_0 = 8.314472; // allgemeine Gaskonstante (J/molK)
-					$T_0 = 273.15; // Umrechnung von °C in K
-					$C = 0.11; // DWD-Beiwert für die Berücksichtigung der Luftfeuchte
-					$E_0 = 6.11213; // (hPa)
-					$f_rel = $Humidity / 100; // relative Luftfeuchte (0-1.0)
-					// momentaner Stationsdampfdruck (hPa)
-					$e_d = $f_rel * $E_0 * exp((17.5043 * $Temperature) / (241.2 + $Temperature));
-        				$PressureRel = $Pressure * exp(($g_n * $Altitude) / ($R * ($Temperature + $T_0 + $C * $e_d + (($gam * $Altitude) / 2))));
-					SetValueFloat($this->GetIDForIdent("PressureRel"), round($PressureRel / 100, 2));
-					
-					// Luftdruck Trends
-					If ($this->ReadPropertyBoolean("LoggingPres") == true) {
-						SetValueFloat($this->GetIDForIdent("PressureTrend1h"), $this->PressureTrend(1));
-						SetValueFloat($this->GetIDForIdent("PressureTrend3h"), $this->PressureTrend(3));
-						SetValueFloat($this->GetIDForIdent("PressureTrend12h"), $this->PressureTrend(12));
-						SetValueFloat($this->GetIDForIdent("PressureTrend24h"), $this->PressureTrend(24));
-					}
-					
-					// Look up table for the possible gas range values
-					$lookupTable1 = array(2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 2126008810, 2147483647, 2130303777,2147483647, 
-							      2147483647, 2143188679, 2136746228, 2147483647, 2126008810, 2147483647, 2147483647);
-					// Look up table for the possible gas range values
-					$lookupTable2 = array(4096000000, 2048000000, 1024000000, 512000000, 255744255, 127110228, 64000000, 32258064, 16016016, 
-							      8000000, 4000000, 2000000, 1000000, 500000, 250000, 125000);
-
-					
-					// Gas Widerstand
-					$var1 = ((1340 + (5 * $range_switching_error)) * ($lookupTable1[$gas_range])) / 65536;
-					$var2 = ((($adc_gas_res * 32768) - (16777216)) + $var1);
-					$var3 = (($lookupTable2[$gas_range] * $var1) / 512);
-					$calc_gas_res = (($var3 + ($var2 / 2)) / $var2);
-					SetValueFloat($this->GetIDForIdent("GasResistance"), $calc_gas_res);
-
-				}
-				
-			}
 		}
 	}	
 	
 	private function Setup()
 	{
 		If ($this->ReadPropertyBoolean("Open") == true) {
-			$this->SendDebug("Setup", "Ausfuehrung", 0);
-			$osrs_t = $this->ReadPropertyInteger("OSRS_T"); // Oversampling Measure temperature x1, x2, x4, x8, x16 (dec: 0 (off), 1, 2, 3, 4)
-			$osrs_p = $this->ReadPropertyInteger("OSRS_P"); // Oversampling Measure pressure x1, x2, x4, x8, x16 (dec: 0 (off), 1, 2, 3, 4)
-			$osrs_h = $this->ReadPropertyInteger("OSRS_H"); // Oversampling Measure humidity x1, x2, x4, x8, x16 (dec: 0 (off), 1, 2, 3, 4)
-			$mode = $this->ReadPropertyInteger("Mode"); // 0 = Power Off (Sleep Mode), x01 und x10 Force Mode
-			$filter = $this->ReadPropertyInteger("IIR_Filter"); // IIR-Filter 0-> off - 2, 4, 8, 16 (dec: 0 (off) - 4)
-			$HeaterProfileSetpoint = $this->ReadPropertyInteger("HeaterProfileSetpoint");
-			$run_gas = 1;
-			
-			$spi3w_en = 0;
-			//$ctrl_meas_reg_start = (($osrs_t << 5)|($osrs_p << 2)|0);
-			$ctrl_meas_reg_end = (($osrs_t << 5)|($osrs_p << 2)|$mode);
-			$config_reg = (($filter << 2)|$spi3w_en);
-			$ctrl_hum_reg = $osrs_h;
-			$crtl_gas_0 = hexdec("00"); // Heater enable - Heater disable = hexdec("08")
-			$crtl_gas_1 = ($run_gas << 4)|$HeaterProfileSetpoint;
-			
-			$this->bme680_set_sensor_mode();
-			
-			
-			
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME680_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("72"), "Value" => $ctrl_hum_reg)));
-			If (!$Result) {
-				$this->SendDebug("Setup", "ctrl_hum_reg setzen fehlerhaft!", 0);
-				return;
-			}
-			
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME680_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("75"), "Value" => $config_reg)));
-			If (!$Result) {
-				$this->SendDebug("Setup", "config_reg setzen fehlerhaft!", 0);
-				return;
-			}
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME680_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("70"), "Value" => $crtl_gas_0)));
-			If (!$Result) {
-				$this->SendDebug("Setup", "crtl_gas_0 setzen fehlerhaft!", 0);
-				return;
-			}
-			
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME680_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("71"), "Value" => $crtl_gas_1)));
-			If (!$Result) {
-				$this->SendDebug("Setup", "crtl_gas_1 setzen fehlerhaft!", 0);
-				return;
-			}
-			// ********************************
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME680_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("64"), "Value" => hexdec("59") )));
-			If (!$Result) {
-				$this->SendDebug("Setup", "gas_wait_0 setzen fehlerhaft!", 0);
-				return;
-			}
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME680_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("5A"), "Value" => hexdec("59") )));
-			If (!$Result) {
-				$this->SendDebug("Setup", "res_heat_0 setzen fehlerhaft!", 0);
-				return;
-			}
-			
-			
-			// **********************************
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME680_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("74"), "Value" => $ctrl_meas_reg_end)));
-			If (!$Result) {
-				$this->SendDebug("Setup", "ctrl_meas_reg_end setzen fehlerhaft!", 0);
-				return;
-			}
-			
 			// Lesen der ChipID
 			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BME680_read", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("D0"))));
-				If ($Result < 0) {
-					$this->SendDebug("Setup", "Fehler beim Einlesen der Chip ID", 0);
-					return;
-				}
-				else {
-					SetValueInteger($this->GetIDForIdent("ChipID"), $Result);
-				}
+			If ($Result < 0) {
+				$this->SendDebug("Setup", "Fehler beim Einlesen der Chip ID", 0);
+				return;
+			}
+			else {
+				SetValueInteger($this->GetIDForIdent("ChipID"), $Result);
+			}
 		
 		}
 	}
-
-
 	
 	private function bme680_set_sensor_settings()
 	{
@@ -847,35 +583,8 @@
 		$par_p9 = $this->bin16dec(($CalibrateData[22] << 8) | $CalibrateData[21]);
 		$par_p10 = $CalibrateData[23];
 		$t_fine = intval($this->GetBuffer("t_fine"));
-		$this->SendDebug("calc_pressure", "Werte:".$par_p1.", ".$par_p2.", ".$par_p3.", ".$par_p4.", ".$par_p5.", ".$par_p6.", ".$par_p7.", ".$par_p8.", ".$par_p9.", ".$par_p10, 0);
-		
-		/*
-		$var1 = intval((($t_fine) >> 1) - 64000);
-   		$var2 = intval((((($var1 >> 2) * ($var1 >> 2)) >> 11) * $par_p6) >> 2);
-    		$var2 = intval($var2 + (($var1 * $par_p5) << 1));
-    		$var2 = intval(($var2 >> 2) + ($par_p4 << 16));
-    		$var1 = intval(((((($var1 >> 2) * ($var1 >> 2)) >> 13) * ($par_p3 << 5)) >> 3) + (($par_p2 * $var1) >> 1));
-    		$var1 = intval($var1 >> 18);
-    		$var1 = intval(((32768 + $var1) * $par_p1) >> 15);
-    		$pressure_comp = intval(1048576 - $adc_pres);
-    		$pressure_comp = intval((($pressure_comp - ($var2 >> 12)) * (3125)));
-		$var4 = (1 << 31);
-		If ($pressure_comp >= $var4) {
-			$pressure_comp = (($pressure_comp / $var1) << 1);
-		}
-		else {
-			$pressure_comp = (($pressure_comp << 1) / $var1);
-		}
-    		$var1 = intval(($par_p9 * ((($pressure_comp >> 3) * ($pressure_comp >> 3)) >> 13)) >> 12);
-    		$var2 = intval((($pressure_comp >> 2) * $par_p8) >> 13);
-    		$var3 = intval((($pressure_comp >> 8) * ($pressure_comp >> 8) * ($pressure_comp >> 8) * $par_p10) >> 17);
- 
-    		$pressure_comp = intval(($pressure_comp) + (($var1 + $var2 + $var3 + ($par_p7 << 7)) >> 4));
-		$Pressure = ($pressure_comp >> 1);
-		//$Pressure = $pressure_comp;
-		*/
-		
-	
+
+		// Luftdruck
 		$var1 = (($t_fine) / 2) - 64000;
 		$var2 = (($var1 / 4) * ($var1 / 4)) / 2048;
 		$var2 = (($var2) * $par_p6) / 4;
@@ -892,27 +601,6 @@
 		$var2 = (($calc_pres / 4) * $par_p8) / 8192;
 		$var3 = (($calc_pres / 256) * ($calc_pres / 256) * ($calc_pres / 256) * $par_p10) / 131072;
 		$calc_pres = intval(($calc_pres) + (($var1 + $var2 + $var3 + ($par_p7 * 128)) / 16));
-		
-		/*
-		// Luftdruck
-		$var1 = ($t_fine / 2) - 64000;
-		$var2 = (($var1 / 4) * ($var1 / 4)) / 2048;
-		$var2 = $var2 * $par_p6 / 4;
-		$var2 = $var2 + ($var1 * $par_p5 * 2);
-		$var2 = ($var2 / 4) + ($par_p4 * 65536);
-		$var1 = ($var1 / 4 * $var1 / 4) / 8192;
-		$var1 = ($var1 * $par_p3 * 32 / 8) + ($par_p2 * $var1 / 2);
-		$var1 = $var1 / 262144;
-		$var1 = ((32768 + $var1) * $par_p1) / 32768;
-		$Pressure = (1048576 - $adc_pres);
-		$Pressure = ($Pressure - ($var2 / 4096)) * 3125;
-		$Pressure = $Pressure / $var1 * 2;
-		$var1 = $par_p9 * ($Pressure / 8) * ($Pressure / 8) / 8192 / 4096;
-		$var2 = ($Pressure / 4) * $par_p8 / 8192;
-		$var3 = ($Pressure / 256) * ($Pressure / 256) * ($Pressure / 256) * $par_p10 / 131072;
-		$Pressure = $Pressure + (($var1 + $var2 + $var3 + ($par_p7 * 128)) / 16);
-		*/
-		
 		SetValueFloat($this->GetIDForIdent("Pressure"), round($calc_pres / 100, 2));
 	return $calc_pres;
 	}
@@ -981,7 +669,7 @@
 	
 	private function bin16dec($dec) 
 	{
-	    	// converts 16bit binary number string to integer using two's complement
+	    	// converts 8bit binary number string to integer using two's complement
 	    	$BinString = decbin($dec);
 		$DecNumber = bindec($BinString) & 0xFFFF; // only use bottom 16 bits
 	    	If (0x8000 & $DecNumber) {
