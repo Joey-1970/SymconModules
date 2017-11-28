6d34e2ce9d3e8ca62dae032ae757420357184882
Joey-1970 <paeper@horburg.de>
2017-11-28 08:03:45 +0100

    Update module.php

--- IPS2GPIO_DS2438/module.php
+++ IPS2GPIO_DS2438/module.php
@@ -106,11 +106,12 @@
 		$OWDeviceArray = Array();
 		$this->SetBuffer("OWDeviceArray", serialize($OWDeviceArray));
 		
+		//ReceiveData-Filter setzen
+		$Filter = '(.*"Function":"status".*|.*"InstanceID":'.$this->InstanceID.'.*)';
+		$this->SetReceiveDataFilter($Filter);
+		
 		If ((IPS_GetKernelRunlevel() == 10103) AND ($this->HasActiveParent() == true)) {			
 			If ($this->ReadPropertyBoolean("Open") == true) {	
-				//ReceiveData-Filter setzen
-				$Filter = '(.*"Function":"status".*|.*"InstanceID":'.$this->InstanceID.'.*)';
-				$this->SetReceiveDataFilter($Filter);
 				
 				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_OWDevices", "DeviceSerial" => $this->ReadPropertyString("DeviceAddress"), "InstanceID" => $this->InstanceID)));		
 				If ($Result == true) {
