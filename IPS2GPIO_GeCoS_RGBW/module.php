2c029fd2ac9da99a2d9e0f3b04569c431d614ae5
Joey-1970 <paeper@horburg.de>
2017-12-09 15:57:46 +0100

    Update module.php

--- IPS2GPIO_GeCoS_RGBW/module.php
+++ IPS2GPIO_GeCoS_RGBW/module.php
@@ -397,12 +397,13 @@
 					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_12_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "InstanceID" => $this->InstanceID, "Register" => $StartAddress, 
 								  "Value_1" => 0, "Value_2" => 0, "Value_3" => $L_Bit_R, "Value_4" => $H_Bit_R, "Value_5" => 0, "Value_6" => 0, "Value_7" => $L_Bit_G, "Value_8" => $H_Bit_G, "Value_9" => 0, "Value_10" => 0, "Value_11" => $L_Bit_B, "Value_12" => $H_Bit_B)));
 					If (GetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group)) == false) {
-						//SetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group), true);
+						SetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group), true);
 					}
 				}
 				$Endtime = microtime(true);
 				//$this->SendDebug("RGBFadeIn", "Endzeit: ".$Endtime, 0);
-				$this->SendDebug("RGBFadeIn", "Endzeit - Startzeit: ".($Starttime - $Endtime), 0);
+				$Delay = intval(($Endtime - $Starttime) * 1000);
+				$this->SendDebug("RGBFadeIn", "Delay: ".$Delay, 0);
 				IPS_Sleep(intval(1000 / $FadeScalar));
 			}
 				
@@ -455,12 +456,12 @@
 					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_12_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "InstanceID" => $this->InstanceID, "Register" => $StartAddress, 
 								  "Value_1" => 0, "Value_2" => 0, "Value_3" => $L_Bit_R, "Value_4" => $H_Bit_R, "Value_5" => 0, "Value_6" => 0, "Value_7" => $L_Bit_G, "Value_8" => $H_Bit_G, "Value_9" => 0, "Value_10" => 0, "Value_11" => $L_Bit_B, "Value_12" => $H_Bit_B)));
 					If (GetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group)) == true) {
-						//SetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group), false);
+						SetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group), false);
 					}
 				}
 				$Endtime = microtime(true);
-				//$this->SendDebug("RGBFadeOut", "Endzeit: ".$Endtime, 0);
-				$this->SendDebug("RGBFadeOut", "Endzeit - Startzeit: ".($Starttime - $Endtime), 0);
+				$Delay = intval(($Endtime - $Starttime) * 1000);
+				$this->SendDebug("RGBFadeOut", "Delay: ".$Delay, 0);
 				IPS_Sleep(intval(1000 / $FadeScalar));
 			}
 				
