4f3d29978d7450deef02dd9e5eecc670b6069d45
Joey-1970 <paeper@horburg.de>
2017-12-09 12:46:38 +0100

    Update module.php

--- IPS2GPIO_GeCoS_RGBW/module.php
+++ IPS2GPIO_GeCoS_RGBW/module.php
@@ -397,7 +397,7 @@
 					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_12_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "InstanceID" => $this->InstanceID, "Register" => $StartAddress, 
 								  "Value_1" => 0, "Value_2" => 0, "Value_3" => $L_Bit_R, "Value_4" => $H_Bit_R, "Value_5" => 0, "Value_6" => 0, "Value_7" => $L_Bit_G, "Value_8" => $H_Bit_G, "Value_9" => 0, "Value_10" => 0, "Value_11" => $L_Bit_B, "Value_12" => $H_Bit_B)));
 					If (GetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group)) == false) {
-						SetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group), true);
+						//SetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group), true);
 					}
 				}
 				$Endtime = microtime(true);
@@ -455,7 +455,7 @@
 					$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_write_12_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "InstanceID" => $this->InstanceID, "Register" => $StartAddress, 
 								  "Value_1" => 0, "Value_2" => 0, "Value_3" => $L_Bit_R, "Value_4" => $H_Bit_R, "Value_5" => 0, "Value_6" => 0, "Value_7" => $L_Bit_G, "Value_8" => $H_Bit_G, "Value_9" => 0, "Value_10" => 0, "Value_11" => $L_Bit_B, "Value_12" => $H_Bit_B)));
 					If (GetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group)) == true) {
-						SetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group), false);
+						//SetValueBoolean($this->GetIDForIdent("Status_RGB_".$Group), false);
 					}
 				}
 				$Endtime = microtime(true);
