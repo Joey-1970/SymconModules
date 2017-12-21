765027aa967a15a5a1b14a76edc99eab88c316d7
Joey-1970 <paeper@horburg.de>
2017-12-21 08:38:53 +0100

    Update module.php

--- IPS2GPIO_PTLB10VE/module.php
+++ IPS2GPIO_PTLB10VE/module.php
@@ -81,6 +81,8 @@
 		IPS_SetHidden($this->GetIDForIdent("Status"), false);
 		
 		
+		
+		
         	If ((IPS_GetKernelRunlevel() == 10103) AND ($this->HasActiveParent() == true)) {
 			// den Handle für dieses Gerät ermitteln
 			//$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "get_handle_serial", "Baud" => 9600, "Device" => $this->ReadPropertyString('ConnectionString'), "InstanceID" => $this->InstanceID )));
