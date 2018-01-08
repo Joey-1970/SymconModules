06b5f5019e887a5594708c23d3361e3fc34e1bd6
Joey-1970 <paeper@horburg.de>
2018-01-08 07:49:45 +0100

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -132,13 +132,13 @@ class IPS2GPIO_IO extends IPSModule
 					$arrayValues[] = array("DeviceTyp" => $DeviceArray[$i][0], "DeviceAddress" => $DeviceArray[$i][1], "DeviceBus" => $DeviceArray[$i][2], "InstanceID" => $DeviceArray[$i][3], "DeviceStatus" => $DeviceArray[$i][4], "rowColor" => $DeviceArray[$i][5]);
 				}
 				$arrayElements[] = array("type" => "List", "name" => "I2C_Devices", "caption" => "I²C-Devices", "rowCount" => 5, "add" => false, "delete" => false, "sort" => $arraySort, "columns" => $arrayColumns, "values" => $arrayValues);
+				$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
 			}
 			else {
 				$arrayElements[] = array("type" => "Label", "label" => "Es wurden keine I²C-Devices gefunden.");
 			}
 			
 			If ($this->GetBuffer("OW_Handle") >= 0) {
-				$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
 				// 1-Wire-Devices einlesen und in das Values-Array kopieren
 				$OWDeviceArray = array();
 				$this->OWSearchStart();
@@ -149,18 +149,19 @@ class IPS2GPIO_IO extends IPSModule
 						$arrayOWValues[] = array("DeviceTyp" => $OWDeviceArray[$i][0], "DeviceSerial" => $OWDeviceArray[$i][1], "InstanceID" => $OWDeviceArray[$i][2], "DeviceStatus" => $OWDeviceArray[$i][3], "rowColor" => $OWDeviceArray[$i][4]);
 					}
 					$arrayElements[] = array("type" => "List", "name" => "OW_Devices", "caption" => "1-Wire-Devices", "rowCount" => 5, "add" => false, "delete" => false, "sort" => $arraySort, "columns" => $arrayOWColumns, "values" => $arrayOWValues);
+					$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
 				}
 				else {
 					$arrayElements[] = array("type" => "Label", "label" => "Es wurden keine 1-Wire-Devices gefunden.");
 				}
 			}
+			$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
 		}
 		If (($this->ConnectionTest()) AND ($this->ReadPropertyBoolean("Open") == true)) {
-			$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
 			$arrayElements[] = array("type" => "Label", "label" => "Führt einen Restart des PIGPIO aus:");
 			$arrayElements[] = array("type" => "Button", "label" => "PIGPIO Restart", "onClick" => 'I2G_PIGPIOD_Restart($id);');
+			$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
 		}
-		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
 		$arrayElements[] = array("type" => "Label", "label" => "Wird ein Audio Hat wie z.B. Hifiberry parallel verwendet, muss diese Option gewählt werden.");
 		$arrayElements[] = array("type" => "Label", "label" => "Die Nutzung von PWM (Dimmer, RGB, RGBW usw.) ist dann nicht möglich!");
 		$arrayElements[] = array("type" => "CheckBox", "name" => "AudioDAC", "caption" => "Vorhanden");
