ab15821cd60af05cde978f1111ee057479b981d7
Joey-1970 <paeper@horburg.de>
2018-01-12 07:51:50 +0100

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -419,8 +419,16 @@ class IPS2GPIO_IO extends IPSModule
 		    	// Setzt die RGB-Farben
 		    	If (($data->Pin_R >= 0) AND ($data->Pin_G >= 0) AND ($data->Pin_B >= 0)) {
 		        	//IPS_LogMessage("IPS2GPIO Set Intensity RGB : ",$data->Pin_R." , ".$data->Value_R." ".$data->Pin_G." , ".$data->Value_G." ".$data->Pin_B." , ".$data->Value_B);  
-		        	$Result = $this->CommandClientSocket(pack("LLLL", 5, $data->Pin_R, $data->Value_R, 0).pack("LLLL", 5, $data->Pin_G, $data->Value_G, 0).pack("LLLL", 5, $data->Pin_B, $data->Value_B, 0), 48);
-				$this->SendDebug("set_PWM_dutycycle_RGB", "Ergebnis: ".boolval($Result), 0);
+		        	$Result = $this->CommandClientSocket(pack("L*", 5, $data->Pin_R, $data->Value_R, 0).pack("L*", 5, $data->Pin_G, $data->Value_G, 0).pack("L*", 5, $data->Pin_B, $data->Value_B, 0), 48);
+				//$this->SendDebug("set_PWM_dutycycle_RGB", "Ergebnis: ".boolval($Result), 0);
+		    	}
+		        break;
+		case "set_PWM_dutycycle_RGBW":
+		    	// Setzt die RGBW-Farben
+		    	If (($data->Pin_R >= 0) AND ($data->Pin_G >= 0) AND ($data->Pin_B >= 0) AND ($data->Pin_W >= 0)) {
+		        	//IPS_LogMessage("IPS2GPIO Set Intensity RGB : ",$data->Pin_R." , ".$data->Value_R." ".$data->Pin_G." , ".$data->Value_G." ".$data->Pin_B." , ".$data->Value_B);  
+		        	$Result = $this->CommandClientSocket(pack("L*", 5, $data->Pin_R, $data->Value_R, 0).pack("L*", 5, $data->Pin_G, $data->Value_G, 0).pack("L*", 5, $data->Pin_B, $data->Value_B, 0).pack("L*", 5, $data->Pin_W, $data->Value_W, 0), 64);
+				//$this->SendDebug("set_PWM_dutycycle_RGBW", "Ergebnis: ".boolval($Result), 0);
 		    	}
 		        break;
 		case "get_value":
