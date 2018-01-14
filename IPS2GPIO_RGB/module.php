e5e76dc7c2419c29fc715d7cd14c03dafb502491
Joey-1970 <paeper@horburg.de>
2018-01-14 13:36:05 +0100

    Update module.php

--- IPS2GPIO_RGB/module.php
+++ IPS2GPIO_RGB/module.php
@@ -107,11 +107,11 @@
 	        //Status-Variablen anlegen
 	        $this->RegisterVariableBoolean("Status", "Status", "~Switch", 10);
            	$this->EnableAction("Status");
-           	$this->RegisterVariableInteger("Intensity_R", "Intensity Rot", "~Intensity.255",20);
+           	$this->RegisterVariableInteger("Intensity_R", "Rot", "~Intensity.255",20);
            	$this->EnableAction("Intensity_R");
-           	$this->RegisterVariableInteger("Intensity_G", "Intensity Grün", "~Intensity.255", 30);
+           	$this->RegisterVariableInteger("Intensity_G", "Grün", "~Intensity.255", 30);
            	$this->EnableAction("Intensity_G");
-           	$this->RegisterVariableInteger("Intensity_B", "Intensity Blau", "~Intensity.255", 40);
+           	$this->RegisterVariableInteger("Intensity_B", "Blau", "~Intensity.255", 40);
            	$this->EnableAction("Intensity_B");
            	$this->RegisterVariableInteger("Color", "Farbe", "~HexColor", 50);
            	$this->EnableAction("Color");
