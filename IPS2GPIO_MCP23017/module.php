90011090a6f500b73ddad2114f908ed46a0976de
Joey-1970 <paeper@horburg.de>
2018-01-18 11:02:22 +0100

    Update module.php

--- IPS2GPIO_MCP23017/module.php
+++ IPS2GPIO_MCP23017/module.php
@@ -334,6 +334,15 @@
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			$this->SendDebug("GetOutput", "Ausfuehrung", 0);
 			// Adressen 12 13
+			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_MCP23017_read", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => hexdec("12"), "Count" => 2)));
+			If ($Result < 0) {
+				$this->SendDebug("GetOutput", "Einlesen der Werte fehlerhaft!", 0);
+				return;
+			}
+			else {
+				$this->SendDebug("GetOutput", "Ergbnis: ".$Result, 0);
+				$MeasurementData = unserialize($Result);
+			}
 		}
 	}
 	    
