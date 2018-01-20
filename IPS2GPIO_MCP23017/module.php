e960b3c702c0844ecfd73d6afe26ba1e41e9ee4a
Joey-1970 <paeper@horburg.de>
2018-01-20 11:57:18 +0100

    Update module.php

--- IPS2GPIO_MCP23017/module.php
+++ IPS2GPIO_MCP23017/module.php
@@ -456,8 +456,8 @@
 			// ConfigByte senden!
 			$this->SendDebug("Setup", "Config-Byte: ".$Config, 0);
 			$ConfigArray = array();
-			$ConfigArray[] = $Config;
-			$ConfigArray[] = $Config;
+			$ConfigArray[0] = $Config;
+			$ConfigArray[1] = $Config;
 			// Adressen 0A 0B
 			
 			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_MCP23017_Write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "InstanceID" => $this->InstanceID, "Register" => hexdec("A0"), 
