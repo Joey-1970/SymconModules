f9b612202ceb1feed0955e426296a226997a7278
Joey-1970 <paeper@horburg.de>
2018-01-24 15:37:58 +0100

    Update module.php

--- IPS2GPIO_SSD1306/module.php
+++ IPS2GPIO_SSD1306/module.php
@@ -2,13 +2,6 @@
     // Klassendefinition
     class IPS2GPIO_SSD1306 extends IPSModule 
     {
-	public function Destroy() 
-	{
-		//Never delete this line!
-		parent::Destroy();
-
-	}
-	    
 	// Überschreibt die interne IPS_Create($id) Funktion
         public function Create() 
         {
@@ -18,13 +11,6 @@
 		$this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
  	    	$this->RegisterPropertyInteger("DeviceAddress", 35);
 		$this->RegisterPropertyInteger("DeviceBus", 1);
- 	    	$this->RegisterPropertyInteger("Messzyklus", 60);
-		$this->RegisterPropertyInteger("Resulution", 16);
-		$this->RegisterPropertyInteger("Sensitivity", 69);
- 	    	$this->RegisterPropertyBoolean("Logging", false);
-		$this->RegisterPropertyInteger("HysteresisOn", 100);
-		$this->RegisterPropertyInteger("HysteresisOff", 0);
-            	$this->RegisterTimer("Messzyklus", 0, 'I2GBH_Measurement($_IPS["TARGET"]);');
         }
 	    
 	public function GetConfigurationForm() 
@@ -54,29 +40,6 @@
 		}
 		$arrayElements[] = array("type" => "Select", "name" => "DeviceBus", "caption" => "Device Bus", "options" => $arrayOptions );
 		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________"); 
-		$arrayElements[] = array("type" => "Label", "label" => "Wiederholungszyklus in Sekunden (0 -> aus, 1 sek -> Minimum)");
-		$arrayElements[] = array("type" => "IntervalBox", "name" => "Messzyklus", "caption" => "Sekunden");
-		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________"); 
-		$arrayElements[] = array("type" => "Label", "label" => "An den folgenden Werten muss in der Regel nichts verändert werden");
-		$arrayOptions = array();
-		$arrayOptions[] = array("label" => "L-Resulution Mode", "value" => 19);
-		$arrayOptions[] = array("label" => "H-Resulution Mode", "value" => 16);
-		$arrayOptions[] = array("label" => "H-Resulution Mode 2", "value" => 17);
-		$arrayElements[] = array("type" => "Select", "name" => "Resulution", "caption" => "Auflösung", "options" => $arrayOptions );
-		$arrayElements[] = array("type" => "Label", "label" => "Wert muss zwischen 31 und 254 liegen (Default 69)");
-		$arrayElements[] = array("type" => "NumberSpinner", "name" => "Sensitivity",  "caption" => "Sensivität"); 
-		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
-		$arrayElements[] = array("type" => "CheckBox", "name" => "Logging", "caption" => "Logging aktivieren"); 
-		$arrayElements[] = array("type" => "Label", "label" => "Optional: Definition einer Hysterese Variablen");
-		$arrayElements[] = array("type" => "NumberSpinner", "name" => "HysteresisOn",  "caption" => "Ein (lx)"); 
-		$arrayElements[] = array("type" => "NumberSpinner", "name" => "HysteresisOff",  "caption" => "Aus (lx)"); 
-		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
-		$arrayElements[] = array("type" => "Label", "label" => "Hinweise:");
-		$arrayElements[] = array("type" => "Label", "label" => "- die Device Adresse lautet 35 dez (0x23h) bei ADDR an GND");
-		$arrayElements[] = array("type" => "Label", "label" => "- die Device Adresse lautet 92 dez (0x5Ch) bei ADDR an 5V");
-		$arrayElements[] = array("type" => "Label", "label" => "- die I2C-Nutzung muss in der Raspberry Pi-Konfiguration freigegeben werden (sudo raspi-config -> Advanced Options -> I2C Enable = true)");
-		$arrayElements[] = array("type" => "Label", "label" => "- die korrekte Nutzung der GPIO ist zwingend erforderlich (GPIO-Nr. 0/1 nur beim Raspberry Pi Model B Revision 1, alle anderen GPIO-Nr. 2/3)");
-		$arrayElements[] = array("type" => "Label", "label" => "- auf den korrekten Anschluss von SDA/SCL achten");
 		
 		$arrayActions = array();
 		$arrayActions[] = array("type" => "Label", "label" => "Diese Funktionen stehen erst nach Eingabe und Übernahme der erforderlichen Daten zur Verfügung!");
@@ -90,25 +53,14 @@
             	// Diese Zeile nicht löschen
             	parent::ApplyChanges();
    
-		// Device Adresse prüfen
-	    	If (($this->ReadPropertyInteger("DeviceAddress") < 0) OR ($this->ReadPropertyInteger("DeviceAddress") > 128)) {
-	    		IPS_LogMessage("IPS2GPIO BH1750","I2C-Device Adresse in einem nicht definierten Bereich!");  
-	    	}
-	    	// Profil anlegen
-	    	$this->RegisterProfileFloat("IPS2GPIO.lx", "Bulb", "", " lx", 0, 1000000, 0.1, 2);
-		//Status-Variablen anlegen
-		$this->RegisterVariableFloat("Illuminance", "Illuminance", "IPS2GPIO.lx", 10);
-		$this->DisableAction("Illuminance");
-		IPS_SetHidden($this->GetIDForIdent("Illuminance"), false);
 		
-		$this->RegisterVariableBoolean("Hysteresis", "Hysteresis", "~Switch", 20);
-		$this->DisableAction("Hysteresis");
-		IPS_SetHidden($this->GetIDForIdent("Hysteresis"), false);
+	    	// Profil anlegen
+	    	
+		//Status-Variablen anlegen
+		
 		
 		If ((IPS_GetKernelRunlevel() == 10103) AND ($this->HasActiveParent() == true)) {
-			// Logging setzen
-			AC_SetLoggingStatus(IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}")[0], $this->GetIDForIdent("Illuminance"), $this->ReadPropertyBoolean("Logging"));
-			IPS_ApplyChanges(IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}")[0]);
+			
 			//ReceiveData-Filter setzen
 			$this->SetBuffer("DeviceIdent", (($this->ReadPropertyInteger("DeviceBus") << 7) + $this->ReadPropertyInteger("DeviceAddress")));
 			$Filter = '((.*"Function":"get_used_i2c".*|.*"DeviceIdent":'.$this->GetBuffer("DeviceIdent").'.*)|.*"Function":"status".*)';
@@ -118,21 +70,20 @@
 			If ($this->ReadPropertyBoolean("Open") == true) {
 				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
 				If ($Result == true) {
-					$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
+					
 					// Setup
 					$this->Setup();
-					// Erste Messdaten einlesen
-					$this->Measurement();
+					
 					$this->SetStatus(102);
 				}
 			}
 			else {
-				$this->SetTimerInterval("Messzyklus", 0);
+				
 				$this->SetStatus(104);
 			}	
 		}
 		else {
-			$this->SetTimerInterval("Messzyklus", 0);
+			//
 		}
 	}
 	
@@ -162,105 +113,14 @@
  	}
 	// Beginn der Funktionen
 	// Führt eine Messung aus
-	public function Measurement()
-	{
-		If ($this->ReadPropertyBoolean("Open") == true) {
-			$this->SendDebug("Measurement", "Ausfuehrung", 0);
-			// Messwerterfassung setzen
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BH1750_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Value" => $this->ReadPropertyInteger("Resulution") )));
-			If (!$Result) {
-				$this->SendDebug("Measurement", "Fehler beim Schreiben der Aufloesung!", 0);
-				$this->SetStatus(202);
-				return;
-			}
-			IPS_Sleep(180);
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BH1750_read", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"))));
-			$this->SendDebug("Measurement", "Wert: ".$Result, 0);
-			If ($Result < 0) {
-				$this->SendDebug("Measurement", "Fehler beim Lesen des Wertes!", 0);
-				$this->SetStatus(202);
-				return;
-			}
-			else {
-				$this->SetStatus(102);
-				If ($this->ReadPropertyInteger("Resulution") == 19) {
-					// LR
-					$Lux = (($Result & 0xff00)>>8) | (($Result & 0x00ff)<<8);
-				}
-				elseif ($this->ReadPropertyInteger("Resulution") == 16) {
-					// HR
-					$Lux = (($Result & 0xff00)>>8) | (($Result & 0x00ff)<<8);
-				}
-				elseif ($this->ReadPropertyInteger("Resulution") == 17) {
-					// HR 2
-					$Lux = (($Result & 0xff00)>>8) | (($Result & 0x00ff)<<8);
-					$Lux = (($Lux & 1) * 0.5) + ($Lux >> 1);
-				}
-				SetValueFloat($this->GetIDForIdent("Illuminance"), max(0, $Lux / 1.2));
-				// Hysteres Variablen setzen
-				If ($Lux >= $this->ReadPropertyInteger("HysteresisOn")) {
-					SetValueBoolean($this->GetIDForIdent("Hysteresis"), true);
-				}
-				elseif ($Lux <= $this->ReadPropertyInteger("HysteresisOff")) {
-					SetValueBoolean($this->GetIDForIdent("Hysteresis"), false);
-				}
-			}
-		}
-	}	
+	
 	    
-	private function RegisterProfileFloat($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits)
-	{
-	        if (!IPS_VariableProfileExists($Name))
-	        {
-	            IPS_CreateVariableProfile($Name, 2);
-	        }
-	        else
-	        {
-	            $profile = IPS_GetVariableProfile($Name);
-	            if ($profile['ProfileType'] != 2)
-	                throw new Exception("Variable profile type does not match for profile " . $Name);
-	        }
-	        IPS_SetVariableProfileIcon($Name, $Icon);
-	        IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
-	        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
-	        IPS_SetVariableProfileDigits($Name, $Digits);
-	}
+
 	private function Setup()
 	{
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			$this->SendDebug("Setup", "Ausfuehrung", 0);
-			// Einschalten
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BH1750_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Value" => hexdec("01"))));
-			If (!$Result) {
-				$this->SendDebug("Setup", "Fehler beim Einschalten!", 0);
-				$this->SetStatus(202);
-				return;
-			}
-			IPS_Sleep(100);
-			// Sensivität setzen
-			$MTreg = max(31, min($this->ReadPropertyInteger("Resulution"), 254));
-			// High bit MTreg 01000 + Bit 6,7,8
-			$HighMTreg =  (8 << 3) | ($MTreg >> 5);
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BH1750_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Value" => $HighMTreg)));
-			If (!$Result) {
-				$this->SendDebug("Setup", "Fehler beim Schreiben des HighMTreg!", 0);
-				$this->SetStatus(202);
-				return;
-			}
-			// Low bit MTreg 011 + Bit 1, 2, 3 ,4, 5
-			$LowMTreg =  ($MTreg & 31) | 96;
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BH1750_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Value" => $LowMTreg)));
-			If (!$Result) {
-				$this->SendDebug("Setup", "Fehler beim Schreiben des LowMTreg!", 0);
-				$this->SetStatus(202);
-				return;
-			}
-			// Messwerterfassung setzen
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_BH1750_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Value" => $this->ReadPropertyInteger("Resulution") )));
-			If (!$Result) {
-				$this->SendDebug("Setup", "Fehler beim Schreiben des Auflösung!", 0);
-				$this->SetStatus(202);
-				return;
+			
 			}
 		}
 	}
