1a1e1535f9d84c72d17bde09a5494ee397a89f48
Joey-1970 <paeper@horburg.de>
2018-01-30 21:58:44 +0100

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -1963,7 +1963,7 @@ class IPS2GPIO_IO extends IPSModule
 					}
 					elseif ($response[2] == $this->GetBuffer("Serial_SDS011_RxD")) {
 						//$this->SendDataToChildren(json_encode(Array("DataID" => "{8D44CA24-3B35-4918-9CBD-85A28C0C8917}", "Function"=>"set_serial_SDS011_data", "Value"=> utf8_encode($Result) )));
-						$Result = utf8_encode($Result);
+						$Result = $Message;
 					}
 					//$this->SendDebug("Serielle Daten", "Text: ".$Result, 0);
 				}
