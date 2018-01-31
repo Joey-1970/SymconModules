d1f454ed26f591d204bd24f6c6e58298f8cd8efc
Joey-1970 <paeper@horburg.de>
2018-01-31 08:58:40 +0100

    Update module.php

--- IPS2GPIO_SDS011/module.php
+++ IPS2GPIO_SDS011/module.php
@@ -180,6 +180,15 @@
 			$this->GetData();
 		}
 	}    
+	
+	private function QueryData()
+	{
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$Message = pack("C*", 0xAA, 0xB4, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x02, 0xAB);
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "write_bb_bytes_serial", "Baud" => 9600, "Pin_TxD" => $this->ReadPropertyInteger("Pin_TxD"), "Command" => $Message)));
+			$this->GetData();
+		}
+	}       
 	    
 	private function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
 	{
