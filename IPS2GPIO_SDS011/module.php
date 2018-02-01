25ac24d13ed39730cf053e3bceb26b8a787f946d
Joey-1970 <paeper@horburg.de>
2018-02-01 16:03:25 +0100

    Update module.php

--- IPS2GPIO_SDS011/module.php
+++ IPS2GPIO_SDS011/module.php
@@ -304,22 +304,6 @@
 			$this->GetData();
 		}
 	} 
-	
-	public function SetWorkingPeriod(Int $Time)
-	{
-		If ($this->ReadPropertyBoolean("Open") == true) {
-			$this->SendDebug("SetWorkingPeriod", "Ausfuehrung", 0);
-			// $Time begrenzen auf 1 -30
-			$Checksum = (0x08 + 0x01 + $Time + 0xFF + 0xFF) & 0xFF;
-			//$Message = pack("C*", 0xAA, 0xB4, 0x02, 0x01, $Time, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, $Checksum, 0xAB);
-			//$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "write_bb_bytes_serial", "Baud" => 9600, "Pin_TxD" => $this->ReadPropertyInteger("Pin_TxD"), "Command" => $Message)));
-			
-			$Message = array(0xAA, 0xB4, 0x02, 0x01, $Time, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, $Checksum, 0xAB);
-			$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "write_bb_bytesarray_serial", "Baud" => 9600, "Pin_TxD" => $this->ReadPropertyInteger("Pin_TxD"), "Command" => serialize($Message) )));
-			
-			$this->GetData();
-		}
-	}     
 	    
 	private function GetFirmware()
 	{
