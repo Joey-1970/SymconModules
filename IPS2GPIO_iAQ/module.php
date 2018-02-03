2c5db449d890011e1889c62f8fd0e713d1402d45
Joey-1970 <paeper@horburg.de>
2018-02-03 20:04:21 +0100

    Update module.php

--- IPS2GPIO_iAQ/module.php
+++ IPS2GPIO_iAQ/module.php
@@ -118,7 +118,6 @@
 					$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
 					// Erste Messdaten einlesen
 					$this->Measurement();
-					$this->SetStatus(102);
 				}
 			}
 			else {
