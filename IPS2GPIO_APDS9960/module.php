dbb22e4f1dafd66204453d10227aa428e673011a
Joey-1970 <paeper@horburg.de>
2018-02-20 17:45:23 +0100

    Update module.php

--- IPS2GPIO_APDS9960/module.php
+++ IPS2GPIO_APDS9960/module.php
@@ -455,20 +455,22 @@
 				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_APDS9960_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $Register, "Value" => $Value)));
 				If (!$Result) {
 					$this->SendDebug("Setup", "Setzen ".$RegisterName." fehlerhaft!", 0);
-					$this->SetStatus(202);
-					$this->SetTimerInterval("Messzyklus", 0);
-					$Response = false;
 				}
 				else {
 					$this->SetStatus(102);
 					$Response = true;
-					return true;
 					break;
 				} 
 			$tries--;
 			} while ($tries); 
-			return $Response;
+			
+			If (!$tries) {
+				$this->SetStatus(202);
+				$this->SetTimerInterval("Messzyklus", 0);
+				$Response = false;
+			}
 		}
+	return $Response;
 	}
 	    
 	private function GetMode()
