1f74c4ce502a786e7e3d2e77db9dec68d6f2c056
Joey-1970 <paeper@horburg.de>
2018-02-20 21:05:22 +0100

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -1003,7 +1003,12 @@ class IPS2GPIO_IO extends IPSModule
 		   		$Result = $this->CommandClientSocket(pack("L*", 62, $this->GetI2C_DeviceHandle(intval($data->DeviceIdent)), $data->Register, 4, $data->Value), 16);
 		   	}
 		   	break;
-
+		case "i2c_APDS9960_read_byte":
+		   	If ($this->GetI2C_DeviceHandle(intval($data->DeviceIdent)) >= 0) {
+				$this->SetI2CBus(intval($data->DeviceIdent));
+		   		$Result = $this->CommandClientSocket(pack("L*", 61, $this->GetI2C_DeviceHandle(intval($data->DeviceIdent)), $data->Register, 0), 16);
+		   	}
+		   	break;
 		// Serielle Kommunikation
 		case "get_handle_serial":
 	   		If ($this->GetBuffer("ModuleReady") == 1) {
