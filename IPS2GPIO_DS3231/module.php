ff9de99ead76e71d75aca0764e21946ca10de769
Joey-1970 <paeper@horburg.de>
2018-03-25 17:51:04 +0200

    Update module.php

--- IPS2GPIO_DS3231/module.php
+++ IPS2GPIO_DS3231/module.php
@@ -129,6 +129,11 @@
 				$this->SetStatus(104);
 			}	
 		}
+		else {
+			$this->SetTimerInterval("Messzyklus", 0);
+			$this->SetTimerInterval("NTPUpdate", 0);
+			$this->SetStatus(104);
+		}	
 	}
 	
 	public function ReceiveData($JSONString) 
