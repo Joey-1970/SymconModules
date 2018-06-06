96d9a27d9df3a3875a01595e0d8a40ba1d222960
Joey-1970 <paeper@horburg.de>
2018-06-06 21:38:46 +0200

    Update module.php

--- IPS2GPIO_SUSV/module.php
+++ IPS2GPIO_SUSV/module.php
@@ -241,7 +241,7 @@
 							switch ($DataArray[1]) {
 								case 208:
 									// Externe Spannung
-									$Voltage = (($DataArray[2] << 8) | $DataArray[3]);
+									$Voltage = (($DataArray[2] << 8) | $DataArray[3]) / 1000;
 									SetValueFloat($this->GetIDForIdent("Voltage"), $Voltage);
 									break;    
 								case 209:
@@ -256,7 +256,7 @@
 									break;
 								case 211:
 									// Batterie Spannung
-									$BatteryVoltage = (($DataArray[2] << 8) | $DataArray[3]);
+									$BatteryVoltage = (($DataArray[2] << 8) | $DataArray[3] / 1000);
 									SetValueFloat($this->GetIDForIdent("BatteryVoltage"), $BatteryVoltage);
 									break;
 								case 53:
