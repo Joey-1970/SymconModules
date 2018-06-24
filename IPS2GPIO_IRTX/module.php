0d06209cd30793d262e9ac446377e7c340f557d3
Joey-1970 <paeper@horburg.de>
2018-06-24 19:37:09 +0200

    Update module.php

--- IPS2GPIO_IRTX/module.php
+++ IPS2GPIO_IRTX/module.php
@@ -102,12 +102,14 @@
 	// Beginn der Funktionen
 	public function RC5(Int $Address, Int $Command, Int $Repeats)
 	{
+		$Result = false;
 		$GPIO = $this->ReadPropertyInteger("Pin");
 		$Address = $Address & 31;
 		$Command = $Command & 63;
 		$Repeats = min(10, max(1, $Repeats));
 		
-		$this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "IR_Remote_RC5", "GPIO" => $GPIO, "Address" => $Address, "Command" => $Command, "Repeats" => 5 )));
+		$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "IR_Remote_RC5", "GPIO" => $GPIO, "Address" => $Address, "Command" => $Command, "Repeats" => $Repeats )));
+	Return $Result;
 	}
      
      
