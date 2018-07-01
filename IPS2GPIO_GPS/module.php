1ba246b755aeca70f1f7a17bfb73dab060fa6295
Joey-1970 <paeper@horburg.de>
2018-07-01 08:03:02 +0200

    Update module.php

--- IPS2GPIO_GPS/module.php
+++ IPS2GPIO_GPS/module.php
@@ -117,6 +117,9 @@
 			$this->SendDebug("ApplyChanges", "Pin-Wechsel TxD - Vorheriger Pin: ".$this->GetBuffer("PreviousPin_TxD")." Jetziger Pin: ".$this->ReadPropertyInteger("Pin_TxD"), 0);
 		}
 		
+		// Summary setzen
+		$this->SetSummary("GPIO RxD: ".$this->ReadPropertyInteger("Pin_RxD")." GPIO TxD: ".$this->ReadPropertyInteger("Pin_TxD"));
+		
 		$this->SetBuffer("Serial_GPS_Data", "");
 
 		//ReceiveData-Filter setzen 		    
