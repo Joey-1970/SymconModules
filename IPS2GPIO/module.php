03b4bf037dad990e1b43c613daac9703a192ed96
Joey-1970 <paeper@horburg.de>
2018-08-14 22:06:19 +0200

    Update module.php

--- IPS2GPIO/module.php
+++ IPS2GPIO/module.php
@@ -107,7 +107,7 @@ class IPS2GPIO_IO extends IPSModule
 		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
 		$arrayElements[] = array("type" => "Label", "label" => "Analyse der Raspberry Pi Konfiguration:");
 		$arraySort = array();
-		$arraySort[] = array("column" => "Typ", "direction" => "ascending");
+		$arraySort[] = array("column" => "Service", "direction" => "ascending");
 		$arrayColumns = array();
 		$arrayColumns[] = array("label" => "Service", "name" => "ServiceTyp", "width" => "200px", "add" => "");
 		$arrayColumns[] = array("label" => "Status", "name" => "ServiceStatus", "width" => "auto", "add" => "");
@@ -121,6 +121,8 @@ class IPS2GPIO_IO extends IPSModule
 		$arrayElements[] = array("type" => "List", "name" => "Raspi_Config", "caption" => "Konfiguration", "rowCount" => 5, "add" => false, "delete" => false, "sort" => $arraySort, "columns" => $arrayColumns, "values" => $arrayValues);
 		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");		
 		// Tabelle für die gefundenen I²C-Devices
+		$arraySort = array();
+		$arraySort[] = array("column" => "Typ", "direction" => "ascending");
 		$arrayColumns = array();
 		$arrayColumns[] = array("label" => "Typ", "name" => "DeviceTyp", "width" => "120px", "add" => "");
 		$arrayColumns[] = array("label" => "Adresse", "name" => "DeviceAddress", "width" => "60px", "add" => "");
