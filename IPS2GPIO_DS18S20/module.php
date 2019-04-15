bf441625902aa99c2fecf62466d4304ac67f1712
Joey-1970 <paeper@horburg.de>
2019-04-15 15:38:28 +0200

    Update module.php

--- IPS2GPIO_DS18S20/module.php
+++ IPS2GPIO_DS18S20/module.php
@@ -159,7 +159,7 @@
 		}
 	}
 	    
-	private function HasActiveParent()
+	protected function HasActiveParent()
     	{
 		$this->SendDebug("HasActiveParent", "Ausfuehrung", 0);
 		$Instance = @IPS_GetInstance($this->InstanceID);
