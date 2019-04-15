749d161d3226c0dc024a3637758dbfbae3524c19
Joey-1970 <paeper@horburg.de>
2019-04-15 15:43:59 +0200

    Update module.php

--- IPS2GPIO_PCF8574/module.php
+++ IPS2GPIO_PCF8574/module.php
@@ -383,7 +383,7 @@
 	return $GPIO;
 	}
 	    
-	private function HasActiveParent()
+	protected function HasActiveParent()
     	{
 		$Instance = @IPS_GetInstance($this->InstanceID);
 		if ($Instance['ConnectionID'] > 0)
