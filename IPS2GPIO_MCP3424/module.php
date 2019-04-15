3264ab643e4b845de4f1eec12ece82edbebe07c1
Joey-1970 <paeper@horburg.de>
2019-04-15 15:43:20 +0200

    Update module.php

--- IPS2GPIO_MCP3424/module.php
+++ IPS2GPIO_MCP3424/module.php
@@ -325,7 +325,7 @@
 	return $I2C_Ports;
 	}    
 	
-	private function HasActiveParent()
+	protected function HasActiveParent()
     	{
 		$Instance = @IPS_GetInstance($this->InstanceID);
 		if ($Instance['ConnectionID'] > 0)
