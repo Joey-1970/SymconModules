8e083206f8943ff9722c7d12d76216473ccda7fd
Joey-1970 <paeper@horburg.de>
2019-04-15 15:37:24 +0200

    Update module.php

--- IPS2GPIO_BT/module.php
+++ IPS2GPIO_BT/module.php
@@ -186,7 +186,7 @@
 		}
 	}
 	
-	private function HasActiveParent()
+	protected function HasActiveParent()
     	{
 		$Instance = @IPS_GetInstance($this->InstanceID);
 		if ($Instance['ConnectionID'] > 0)
