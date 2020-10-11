46c744caa221862aa71504b2b35c7f160576b866
Joey-1970 <paeper@horburg.de>
2020-10-11 22:49:54 +0200

    Update module.php

--- IPS2GPIO_PCF8583/module.php
+++ IPS2GPIO_PCF8583/module.php
@@ -368,6 +368,21 @@
 			$tries = 5;
 			do {
 				$this->SendDebug("GetCounter", "Ausfuehrung", 0);
+				
+				$Bitmask = 0xE0;
+				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_PCF8583_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => 0x00, "Value" => $Bitmask)));
+				If (!$Result) {
+					$this->SendDebug("Setup", "Setzen der Config fehlerhaft!", 0);
+					$this->SetStatus(202);
+					$this->SetTimerInterval("Messzyklus", 0);
+					return;
+				}
+				else {
+					$this->SetStatus(102);
+				}				
+				
+				
+				
 				$CounterValue =  0;
 				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_PCF8583_read", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => 0x01, "Count" => 3)));
 				If ($Result < 0) {
