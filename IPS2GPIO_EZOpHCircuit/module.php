73d7e4ae5c91091a4f81ccccf16f4fbe5dcecc52
Joey-1970 <paeper@horburg.de>
2020-12-25 21:16:11 +0100

    Update module.php

--- IPS2GPIO_EZOpHCircuit/module.php
+++ IPS2GPIO_EZOpHCircuit/module.php
@@ -138,9 +138,7 @@
 			$this->SendDebug("SetLEDState", "Ausfuehrung", 0);
 			$Message = "L,".intval($State);
 			$MessageArray = (unpack("C*", $Message));
-			
-			
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_EZOphCircuit_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Value" => $Message)));
+			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "i2c_EZOphCircuit_write", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Parameter" => serialize($MessageArray) )));
 			$this->SendDebug("SetLEDState", "Ergebnis: ".$Result, 0);
 		}
 	}	
