2ec0995211f7c425f59f2c1fd9c40f6ac909d4f9
Joey-1970 <paeper@horburg.de>
2020-12-26 19:57:06 +0100

    Update module.php

--- IPS2GPIO_EZOpHCircuit/module.php
+++ IPS2GPIO_EZOpHCircuit/module.php
@@ -9,7 +9,7 @@
             	parent::Create();
  	    	$this->RegisterPropertyBoolean("Open", false);
 		$this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
- 	    	$this->RegisterPropertyInteger("DeviceAddress", 35);
+ 	    	$this->RegisterPropertyInteger("DeviceAddress", 99);
 		$this->RegisterPropertyInteger("DeviceBus", 1);
  	    	$this->RegisterPropertyInteger("Messzyklus", 60);
             	$this->RegisterTimer("Messzyklus", 0, 'EZOpHCircuit_Measurement($_IPS["TARGET"]);');
