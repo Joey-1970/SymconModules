8b8c0716b4ac5c4d6afa776fda479f4dfecb6954
Joey-1970 <paeper@horburg.de>
2021-01-11 15:53:23 +0100

    Update module.php

--- IPS2GPIO_EZOpHCircuit/module.php
+++ IPS2GPIO_EZOpHCircuit/module.php
@@ -20,7 +20,9 @@
 	    			
 		
 		//Status-Variablen anlegen
-		$this->RegisterVariableString("Firmware", "Firmware", "", 10);
+		$this->RegisterVariableString("DeviceType", "Device Typ", "", 10);
+		$this->RegisterVariableString("Firmware", "Firmware", "", 20);
+		
 		
 		$this->RegisterVariableBoolean("LED", "LED", "~Switch", 20);
 		$this->EnableAction("LED");
@@ -86,6 +88,9 @@
 				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
 				If ($Result == true) {
 					$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
+					// Firmware und Device-Typ einlesen
+					$this->GetFirmware();
+					
 					// Erste Messdaten einlesen
 					$this->Measurement();
 				}
@@ -283,6 +288,7 @@
 
 			case "?I":
 				$this->SendDebug("ReadResult", "Device Information", 0);
+				$this->SetValue("DeviceType", $ResultParts[1]);
 				$this->SetValue("Firmware", $ResultParts[2]);
 				break;
 			/*
