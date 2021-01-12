bf362969e5636d99d607138e397fb49c24b05e83
Joey-1970 <paeper@horburg.de>
2021-01-12 20:24:45 +0100

    Update module.php

--- IPS2GPIO_Configurator_I2C/module.php
+++ IPS2GPIO_Configurator_I2C/module.php
@@ -120,7 +120,7 @@
 				     "EZO ORP" => "{51401510-EBA1-2C99-5B39-3C0C9C9758B6}", "EZO PH" => "{4D846905-0066-AB5D-F997-DC01CB1D975E}", "MCP3424" => "{0EBA825C-47AD-4BC6-AC0D-1ADF9CD55AB2}",
 				     "BME280" => "{64E6464A-664C-46DE-B49F-8629497ED56F}", "BME680" => "{54EBA6FB-A557-4CB9-B384-933D6F5155B6}", "BMP180" => "{9D970308-36E7-428D-8AC0-D8C1496DDCCA}");
 		$BusArray = array("I²C-Bus 0", "I²C-Bus 0", "MUX I²C-Bus 0", "MUX I²C-Bus 1", "MUX I²C-Bus 2", "MUX I²C-Bus 3", "MUX I²C-Bus 4", "MUX I²C-Bus 5", "MUX I²C-Bus 6", "MUX I²C-Bus 7",);
-		
+		$this->SendDebug("GetDeviceInstanceID", "Bus: ".$Bus." Suche: ".intval(in_array($Bus, $BusArray)), 0);
 		// Bausteine fehlen noch
 		if ((array_key_exists($Type, $TypeArray)) AND (in_array($Bus, $BusArray))) {
 			$guid = $DeviceArray[$Type];
