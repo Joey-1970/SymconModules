cc045469eb6a11d3e7d969a96ec141e5d7692563
Joey-1970 <paeper@horburg.de>
2021-01-13 07:18:04 +0100

    Update module.php

--- IPS2GPIO_MCP3424/module.php
+++ IPS2GPIO_MCP3424/module.php
@@ -16,7 +16,6 @@
 			$this->RegisterPropertyInteger("Resolution_".$i, 0);
 			$this->RegisterPropertyInteger("Amplifier_".$i, 0);
 			$this->RegisterPropertyBoolean("Active_".$i, true);
-			$this->RegisterPropertyBoolean("Logging_".$i, false);
 		}
             	$this->RegisterTimer("Messzyklus", 0, 'I2GAD2_Measurement($_IPS["TARGET"]);');
 		
@@ -121,11 +120,6 @@
 		$this->SetBuffer("MeasurementData", serialize($MeasurementData));
 		
 		If ((IPS_GetKernelRunlevel() == 10103) AND ($this->HasActiveParent() == true)) {
-			for ($i = 0; $i <= 3; $i++) {
-				AC_SetLoggingStatus(IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}")[0], $this->GetIDForIdent("Channel_".($i + 1)), $this->ReadPropertyBoolean("Logging_".$i));
-			}
-			IPS_ApplyChanges(IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}")[0]);
-			
 			If ($this->ReadPropertyBoolean("Open") == true) {
 				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
 				If ($Result == true) {
