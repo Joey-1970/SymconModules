fb147d57e4a9e89d3ef3b7cbc4006bcda6fb350f
Joey-1970 <paeper@horburg.de>
2021-06-02 16:29:43 +0200

    Update module.php

--- IPS2GPIO_EZOORPCircuit/module.php
+++ IPS2GPIO_EZOORPCircuit/module.php
@@ -77,7 +77,7 @@
 		$arrayActions = array(); 
 		$arrayActions[] = array("type" => "Label", "caption" => "Wichtiger Hinweis: Bitte dazu die Bedienungsanleitung beachten!"); 
 		$ArrayRowLayout = array();
-		$ArrayRowLayout[] = array("type" => "Button", "caption" => "Kalibrierung", "onClick" => 'EZOOPRCircuit_Calibration($id, $Value);'); 
+		$ArrayRowLayout[] = array("type" => "Button", "caption" => "Kalibrierung", "onClick" => 'EZOOPRCircuit_Calibration($id, 475);'); 
 		$ArrayRowLayout[] = array("type" => "NumberSpinner", "name" => "Value", "caption" => "Wert");
 		$arrayActions[] = array("type" => "RowLayout", "items" => $ArrayRowLayout);
 		$arrayActions[] = array("type" => "Button", "caption" => "Kalibrierung löschen", "onClick" => 'EZOOPRCircuit_CalibrationClear($id);'); 
