20ce121d347a8d6c1888bc71c138748a0886305f
Joey-1970 <paeper@horburg.de>
2025-09-12 20:18:09 +0200

    Update module.php

--- ShotGlassFillingMachine/module.php
+++ ShotGlassFillingMachine/module.php
@@ -22,12 +22,25 @@
 			$this->RegisterPropertyInteger("midpoint", 1500);
 			$this->RegisterPropertyInteger("most_clockwise", 2500);
 			$this->RegisterPropertyInteger("Shutdown", 500);
+			for ($i = 1; $i <= 5; $i++) {
+				$this->RegisterPropertyInteger("Position_".$i, $i * 20);
+			}
+			$this->RegisterPropertyInteger("Position_1", 50);
 			$this->ConnectParent("{ED89906D-5B78-4D47-AB62-0BDCEB9AD330}");
 			$this->RegisterTimer("Shutdown", 0, 'ShotGlassFillingMachine_Shutdown($_IPS["TARGET"]);');
+
+			// Profile erstellen
+			$this->RegisterProfileInteger("ShotGlassFillingMachine.Position", "Information", "", "", 0, 3, 1);
+			IPS_SetVariableProfileAssociation("ShotGlassFillingMachine.Position", 0, "Ruheposition", "TurnLeft", 0x000000);
+			for ($i = 1; $i <= 5; $i++) {
+				IPS_SetVariableProfileAssociation("ShotGlassFillingMachine.Position", $i, "Postion ".$i, "TurnRight", 0x000000);
+			}
 			
 			// Status-Variablen anlegen
 			$this->RegisterVariableInteger("Output", "Ausgang", "~Intensity.100", 10);
 			$this->EnableAction("Output");
+			$this->RegisterVariableInteger("Position", "Position", "ShotGlassFillingMachine.Position", 20);
+			$this->EnableAction("Position");
         }
 	
 	public function GetConfigurationForm() 
@@ -63,7 +76,11 @@
 		$arrayElements[] = array("type" => "Label", "caption" => "Zeit bis zur Abschaltung in Microsekunden (0 = keine automatische Abschaltung)"); 
 		$arrayElements[] = array("type" => "NumberSpinner", "name" => "Shutdown", "caption" => "Abschaltung (ms)", "minimum" => 0); 
 		$arrayElements[] = array("type" => "Label", "caption" => "ACHTUNG: Falsche Werte können zur Beschädigung des Servo führen!");
-		
+		$arrayElements[] = array("type" => "Label", "caption" => "_____________________________________________________________________________________________________"); 
+		$arrayElements[] = array("type" => "Label", "caption" => "Angabe der Postitionen in Prozent"); 
+		for ($i = 1; $i <= 5; $i++) {
+			$arrayElements[] = array("type" => "NumberSpinner", "name" => "Position_".$i, "caption" => "Position ".$i, "minimum" => 0, "maximum" => 100); 
+		}
 		
 		$arrayActions = array();
 		If (($this->ReadPropertyInteger("Pin") >= 0) AND ($this->ReadPropertyBoolean("Open") == true)) {
@@ -128,6 +145,11 @@
 		    		$this->SetOutput($Value);
 		    	}
 	            break;
+			case "Position":
+	            If ($this->ReadPropertyBoolean("Open") == true) {
+		    		$this->SetPosition($Value);
+		    	}
+	            break;
 	        default:
 	            throw new Exception("Invalid Ident");
 	    	}
@@ -161,6 +183,41 @@
 	{
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			$this->SendDebug("SetOutput", "Ausfuehrung", 0);
+			$Left = $this->ReadPropertyInteger("most_anti_clockwise");
+			$Right = $this->ReadPropertyInteger("most_clockwise");
+			$Shutdown = $this->ReadPropertyInteger("Shutdown");
+			
+			$Value = min(100, max(0, $Value));
+			
+			$Value = intval(($Value * ($Right - $Left) / 100) + $Left);
+			$this->SendDebug("SetOutput", "Errechneter Zielwert: ".$Value, 0);
+			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_servo", "Pin" => $this->ReadPropertyInteger("Pin"), "Value" => $Value)));
+			If (!$Result) {
+				$this->SendDebug("SetOutput", "Fehler beim Positionieren!", 0);
+				If ($this->GetStatus() <> 202) {
+					$this->SetStatus(202);
+				}
+			}
+			else {
+				If ($this->GetStatus() <> 102) {
+					$this->SetStatus(102);
+				}
+				//$Output = ($Value / ($Right - $Left)) * 100;
+				$Output = (($Value - $Left)/ ($Right - $Left)) * 100;
+				SetValue("Output", $Output);
+				$this->GetOutput();
+			}
+			
+			If ($Shutdown > 0) {
+				$this->SetTimerInterval("Shutdown", $Shutdown);
+			}
+		}
+	}
+
+	public function SetPosition(Int $Value)
+	{
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$this->SendDebug("SetPosition", "Ausfuehrung", 0);
 			$Left = $this->ReadPropertyInteger("most_anti_clockwise");
 			$Right = $this->ReadPropertyInteger("most_clockwise");
 			$Shutdown = $this->ReadPropertyInteger("Shutdown");
@@ -265,7 +322,24 @@
 		}
 	return $GPIO;
 	}    
-	    
+
+	private function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
+	{
+	        if (!IPS_VariableProfileExists($Name))
+	        {
+	            IPS_CreateVariableProfile($Name, 1);
+	        }
+	        else
+	        {
+	            $profile = IPS_GetVariableProfile($Name);
+	            if ($profile['ProfileType'] != 1)
+	                throw new Exception("Variable profile type does not match for profile " . $Name);
+	        }
+	        IPS_SetVariableProfileIcon($Name, $Icon);
+	        IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+	        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);        
+	}
+		
 	protected function HasActiveParent()
     	{
 		$Instance = @IPS_GetInstance($this->InstanceID);
