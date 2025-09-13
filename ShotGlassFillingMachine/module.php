f500dfceb2a7671dd4f3342822303add9255e906
Joey-1970 <paeper@horburg.de>
2025-09-13 08:30:01 +0200

    Update module.php

--- ShotGlassFillingMachine/module.php
+++ ShotGlassFillingMachine/module.php
@@ -344,7 +344,9 @@
 		$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{A0DAAF26-4A2D-4350-963E-CC02E74BD414}", "Function" => "set_servo", "Pin" => $this->ReadPropertyInteger("Pin_Servo"), "Value" => 0)));
 			If (!$Result) {
 				$this->SendDebug("Shutdown", "Fehler beim Ausschalten!", 0);
-				$this->SetStatus(202);
+				If ($this->GetStatus() <> 202) {
+					$this->SetStatus(202);
+				}
 			}
 		$this->SetTimerInterval("Shutdown", 0);
 	}
