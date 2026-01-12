4fe5abc2c22873599fcb0d2b2afcf7d484e6d501
Joey-1970 <paeper@horburg.de>
2026-01-12 13:00:37 +0100

    Add files via upload

---
+++ libs/Math/Common/FiniteField.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Finite Fields Base Class
+ *
+ * PHP version 5 and 7
+ *
+ * @author    Jim Wigginton <terrafrost@php.net>
+ * @copyright 2017 Jim Wigginton
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+
+namespace phpseclib4\Math\Common;
+
+/**
+ * Finite Fields
+ *
+ * @author  Jim Wigginton <terrafrost@php.net>
+ */
+abstract class FiniteField
+{
+}
