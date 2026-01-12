988d1aaad535eb27870d59c38ffa9c9d9084843a
Joey-1970 <paeper@horburg.de>
2026-01-12 12:59:37 +0100

    Add files via upload

---
+++ libs/Math/Common/FiniteField/Integer.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Finite Field Integer Base Class
+ *
+ * PHP version 5 and 7
+ *
+ * @author    Jim Wigginton <terrafrost@php.net>
+ * @copyright 2017 Jim Wigginton
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+
+namespace phpseclib4\Math\Common\FiniteField;
+
+/**
+ * Finite Field Integer
+ *
+ * @author  Jim Wigginton <terrafrost@php.net>
+ */
+abstract class Integer implements \JsonSerializable
+{
+    /**
+     * JSON Serialize
+     *
+     * Will be called, automatically, when json_encode() is called on a BigInteger object.
+     *
+     * PHP Serialize isn't supported because unserializing would require the factory be
+     * serialized as well and that just sounds like too much
+     *
+     * @return array{hex: string}
+     */
+    #[\ReturnTypeWillChange]
+    public function jsonSerialize(): array
+    {
+        return ['hex' => $this->toHex(true)];
+    }
+
+    /**
+     * Converts an Integer to a hex string (eg. base-16).
+     */
+    abstract public function toHex(): string;
+}
