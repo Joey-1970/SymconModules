2cf34693b369748c0ff44932bd582b2356cbd0e8
Joey-1970 <paeper@horburg.de>
2026-01-12 12:55:30 +0100

    Add files via upload

---
+++ libs/Math/BigInteger/Engines/PHP/Reductions/PowerOfTwo.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * PHP Power of Two Modular Exponentiation Engine
+ *
+ * PHP version 5 and 7
+ *
+ * @author    Jim Wigginton <terrafrost@php.net>
+ * @copyright 2017 Jim Wigginton
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @link      http://pear.php.net/package/Math_BigInteger
+ */
+
+declare(strict_types=1);
+
+namespace phpseclib4\Math\BigInteger\Engines\PHP\Reductions;
+
+use phpseclib4\Math\BigInteger\Engines\PHP\Base;
+
+/**
+ * PHP Power Of Two Modular Exponentiation Engine
+ *
+ * @author  Jim Wigginton <terrafrost@php.net>
+ */
+abstract class PowerOfTwo extends Base
+{
+    /**
+     * Prepare a number for use in Montgomery Modular Reductions
+     */
+    protected static function prepareReduce(array $x, array $n, string $class): array
+    {
+        return self::reduce($x, $n, $class);
+    }
+
+    /**
+     * Power Of Two Reduction
+     */
+    protected static function reduce(array $x, array $n, string $class): array
+    {
+        $lhs = new $class();
+        $lhs->value = $x;
+        $rhs = new $class();
+        $rhs->value = $n;
+
+        $temp = new $class();
+        $temp->value = [1];
+
+        $result = $lhs->bitwise_and($rhs->subtract($temp));
+        return $result->value;
+    }
+}
