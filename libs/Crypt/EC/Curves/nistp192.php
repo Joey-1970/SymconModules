646ccdf5a8a468b78c37d956d944ae2c09431aab
Joey-1970 <paeper@horburg.de>
2026-01-12 12:11:17 +0100

    Add files via upload

---
+++ libs/Crypt/EC/Curves/nistp192.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * nistp192
+ *
+ * PHP version 5 and 7
+ *
+ * @author    Jim Wigginton <terrafrost@php.net>
+ * @copyright 2017 Jim Wigginton
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @link      http://pear.php.net/package/Math_BigInteger
+ */
+
+declare(strict_types=1);
+
+namespace phpseclib4\Crypt\EC\Curves;
+
+final class nistp192 extends secp192r1
+{
+}
---
+++ libs/Crypt/EC/Curves/nistp384.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * nistp384
+ *
+ * PHP version 5 and 7
+ *
+ * @author    Jim Wigginton <terrafrost@php.net>
+ * @copyright 2017 Jim Wigginton
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @link      http://pear.php.net/package/Math_BigInteger
+ */
+
+declare(strict_types=1);
+
+namespace phpseclib4\Crypt\EC\Curves;
+
+final class nistp384 extends secp384r1
+{
+}
